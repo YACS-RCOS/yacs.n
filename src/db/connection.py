bfbc39ee7ad297d282fe2e503c57308b520badb9
Jacob Shomstein <jacob@shomstein.com>
2020-02-29 15:42:22 -0500

    added env vars to parametrize db connection details

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,11 +1,18 @@
 import psycopg2
 import psycopg2.extras
+import os
+
+# connection details
+DB_NAME = os.environ.get('DB_NAME', 'yacs')
+DB_USER = os.environ.get('DB_USER', '')
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_PASS = os.environ.get('DB_PASS', '')
 
 
 class database():
     def __init__(self):
         self.connect_str = "dbname='{}' user='{}' host='{}' password='{}'".format(
-            "yacs", "", "localhost", "")
+            DB_NAME, DB_USER, DB_HOST, DB_PASS)
 
     def connect(self):
         try:
