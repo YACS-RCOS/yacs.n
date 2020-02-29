2ac8e7438e5ce7b484a9409da7b83e44ba635591
corey buckley <coreybuckley13@gmail.com>
2020-02-29 14:31:39 -0500

    Hook up CSV to DB operations module to /api/upload endpoint

--- src/app.py
+++ src/app.py
@@ -2,8 +2,14 @@
 from flask import Flask
 from flask import send_from_directory
 from flask import jsonify
+from flask import Response
+from flask import request
 import db.connection as connection
 import db.classinfo as ClassInfo
+
+from CsvToInsert import CsvToInsert
+
+from io import StringIO
 
 # - init interfaces to db
 db_conn = connection.db
@@ -43,6 +49,25 @@ def js(file):
 def get_classes():
     return jsonify(class_info.get_classes_full())
 
+@app.route('/api/upload', methods=['POST'])
+def uploadHandler():
+    if len(request.files) > 0:
+        files = []
+        for _, file in request.files.items():
+            if ('.' in file.filename and file.filename.rsplit('.')[1] == 'csv'):
+                # Flask opens passed files from POST in binary mode by default
+                content = file.read().decode()
+                # https://stackoverflow.com/questions/3305926/python-csv-string-to-array
+                tempFile = StringIO(content)
+                files.append(tempFile)
+            else:
+                print("not csv file")
+        print(files)
+        csvToDBInsertJob = CsvToInsert(connection.db)
+        csvToDBInsertJob.populateDBFromCSVDataSourceDirectoryPath(files)
+    # for key, value in request.form.items():
+    #     print(key, value)
+    return Response("Received file(s)", 200)
 
 if __name__ == '__main__':
     app.run()
