d4358d23f7fab1fcadbdad98e3c3bd4b682d323c
123joshuawu <joshuawu3@gmail.com>
2020-03-01 23:03:38 -0500

    Start yacs web in Vue. Added listing courses with filtering and display of time blocks on schedule grid.

---
+++ src/yacs-web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <Header></Header> -->
+    <MainPage/>
+  </div>
+</template>
+
+<script>
+import MainPage from './pages/Main';
+
+// import Header from './components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    MainPage,
+    // Header
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+    overflow-x: hidden;
+  }
+</style>
---
+++ src/yacs-web/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <b-navbar variant="info">
+        <b-navbar-brand href="#">YACS</b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
