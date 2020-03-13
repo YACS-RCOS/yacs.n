3dd0c40c983883a590ab83ec36f533d229f598b9
123joshuawu <joshuawu3@gmail.com>
2020-03-13 00:19:10 -0400

    Changed to singleton

--- src/yacs-web/src/services/ColorService.js
+++ src/yacs-web/src/services/ColorService.js
@@ -1,39 +1,43 @@
-// export class ColorAssignment {
-//     primary
-// 	constructor (
-// 		public readonly primary: string,
-// 		public readonly text: string,
-// 		public readonly border: string
-// 	) { }
-// }
 
-export default class ColorService {
-    static COLORS = ['#ffd4df', '#ceeffc', '#fff4d0', '#dcf7da', '#f7e2f7', '#ede6df', '#ffe9cf'];
-    static TEXT_COLORS = ['#d1265d', '#1577aa', '#bf8a2e', '#008a2e', '#853d80', '#9d5733', '#d9652b'];
-    static BORDER_COLORS = ['#ff2066', '#00aff2', '#ffcb45', '#48da58', '#d373da', '#a48363', '#ff9332'];
-    static NUM_COLORS = 7;
+const COLORS = ['#ffd4df', '#ceeffc', '#fff4d0', '#dcf7da', '#f7e2f7', '#ede6df', '#ffe9cf'];
+const TEXT_COLORS = ['#d1265d', '#1577aa', '#bf8a2e', '#008a2e', '#853d80', '#9d5733', '#d9652b'];
+const BORDER_COLORS = ['#ff2066', '#00aff2', '#ffcb45', '#48da58', '#d373da', '#a48363', '#ff9332'];
+const NUM_COLORS = 7;
 
-    // TODO: use something persistent
-    assignmentIncr = 0;
-    colorAssignments = new Map();
+// TODO: use something persistent
+var assignmentIncr = 0;
+var colorAssignments = new Map();
 
-    getColor (id) {
-        const assignment = this.assign(id);
+/**
+ * @module ColorService
+ * Ported over from old YACS
+ * Provides colors for schedule blocks
+ */
+export default {
+    _getColor (id) {
+        const assignment = this._assign(id);
         return {
-            primary: ColorService.COLORS[assignment],
-            text: ColorService.TEXT_COLORS[assignment],
-            border: ColorService.BORDER_COLORS[assignment]
+            primary: COLORS[assignment],
+            text: TEXT_COLORS[assignment],
+            border: BORDER_COLORS[assignment]
         };
-    }
-
-    assign (id) {
-        if (this.colorAssignments.has(id))
-            return this.colorAssignments.get(id);
+    },
+    _assign (id) {
+        if (colorAssignments.has(id))
+            return colorAssignments.get(id);
         else
-            return this.colorAssignments.set(id, this.nextAssignment()).get(id);
-    }
-
-    nextAssignment () {
-        return (++this.assignmentIncr) % ColorService.NUM_COLORS;
-    }
+            return colorAssignments.set(id, this._nextAssignment()).get(id);
+    },
+    _nextAssignment () {
+        return (++assignmentIncr) % NUM_COLORS;
+    },
+    getBackgroundColor (courseSession) {
+        return this._getColor(courseSession.crn).primary;
+    },
+    getBorderColor (courseSession) {
+        return this._getColor(courseSession.crn).border;
+    },
+    getTextColor (courseSession) {
+        return this._getColor(courseSession.crn).text;
+    },
 }
