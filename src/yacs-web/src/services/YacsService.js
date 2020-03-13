38b20ea95ffc5613a51b267f4b49e35d22dffea7
123joshuawu <joshuawu3@gmail.com>
2020-03-13 00:11:03 -0400

    Moved data parsing and 'enrichment' to YacsService

--- src/yacs-web/src/services/YacsService.js
+++ src/yacs-web/src/services/YacsService.js
@@ -1,10 +1,41 @@
 import axios from 'axios';
 
+import { readableDate } from '@/utils';
+
 const client = axios.create({
     baseURL: 'http://localhost:5000/api'
 });
 
 
-export const getCourses = () => client.get('/class');
-export const getDepartments = () => client.get('/department');
-export const getSubSemesters = () => client.get('/subsemester');
+const _getCourseIdentifier = (courseObj) => {
+    return `${courseObj.department}${courseObj.level}${courseObj.date_start.getMonth() + 1}${courseObj.date_start.getDay() + 1}${courseObj.date_start.getFullYear()}${courseObj.date_end.getMonth() + 1}${courseObj.date_end.getDay() + 1}${courseObj.date_end.getFullYear()}`;
+}
+
+export const getCourses = () => client.get('/class').then(({ data }) => {
+    return data.map(c => {
+        c.date_start = new Date(c.date_start);
+        c.date_end = new Date(c.date_end);
+  
+        c.sections = c.sections.filter(s => !!s);
+        c.sections.forEach(s => {if (s) s.selected = false;});
+        c.selected = false;
+        c.id = _getCourseIdentifier(c);
+        return c;
+    });
+});
+export const getDepartments = () => client.get('/department').then(({ data }) => {
+    return data;
+});
+export const getSubSemesters = () => client.get('/subsemester').then(({ data }) => {
+    return data.map(subsemester => {
+        subsemester.date_start = new Date(subsemester.date_start)
+        subsemester.date_end = new Date(subsemester.date_end)
+        subsemester.date_start_display = readableDate(subsemester.date_start);
+        subsemester.date_end_display = readableDate(subsemester.date_end);
+
+        subsemester.text = `${subsemester.date_start_display} - ${subsemester.date_end_display}`;
+
+
+        return subsemester;
+    });
+});
