980f974a8c9062659928729d734fed0a267306e9
123joshuawu <joshuawu3@gmail.com>
2020-03-14 18:45:16 -0400

    Add prettier code formatter to start to enforce code style

--- src/yacs-web/.eslintrc.js
+++ src/yacs-web/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    'prettier'
   ],
   parserOptions: {
     parser: 'babel-eslint'
