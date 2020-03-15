eaa22e0f6a2d4bb8edc0d9de3d8921d85581e060
123joshuawu <joshuawu3@gmail.com>
2020-03-14 21:54:07 -0400

    Update config files to enable @ webpack import alias for src/ directory

--- src/yacs-web/vue.config.js
+++ src/yacs-web/vue.config.js
@@ -1,18 +1,22 @@
+const path = require('path');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
-                changeOrigin: true
-            }
-        },
-    },
-    configureWebpack: {
-        plugins: [
-            new MomentLocalesPlugin()
-        ],
-        devtool: process.env.VUE_APP_ENVIRONMENT == 'development' ? 'source-map' : 'none'
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
-}
+  },
+  configureWebpack: {
+    plugins: [new MomentLocalesPlugin()],
+    devtool: process.env.VUE_APP_ENVIRONMENT == 'development' ? 'source-map' : 'none',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    }
+  }
+};
