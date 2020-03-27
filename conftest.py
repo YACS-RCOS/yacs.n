0c9d11cb593f736e5d8e43300eef486f57f5af95
123joshuawu <joshuawu3@gmail.com>
2020-03-27 16:10:32 -0700

    Add load test csv to conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,9 @@ import pytest
 
 from src.db.connection import db
 from src.db.classinfo import ClassInfo
+from src.db.courses import Courses
+
+TEST_CSV = os.environ.get('TEST_CSV', None)
 
 @pytest.fixture(scope="session")
 def db_conn():
@@ -12,3 +15,7 @@ def db_conn():
 @pytest.fixture(scope="session")
 def class_info(db_conn):
     return ClassInfo(db_conn)
+
+if TEST_CSV is not None:
+    with open(TEST_CSV) as csvfile:
+        Courses(db).populate_from_csv(csvfile)
