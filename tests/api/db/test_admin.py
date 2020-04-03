e013c85a7ecd1133cca9c374ecd0e89faeebcb2a
Daniel Ackermans <ackerd2@rpi.edu>
2020-04-03 01:58:48 -0400

    Testing fix

--- tests/api/db/test_admin.py
+++ tests/api/db/test_admin.py
@@ -1,4 +1,4 @@
 def test_get_default_semester(admin_settings):
-    default_semester = admin_settings.get_semester_default()
+    default_semester, error = admin_settings.get_semester_default()
     print(default_semester)
     assert(isinstance(default_semester, str) or default_semester is None)
