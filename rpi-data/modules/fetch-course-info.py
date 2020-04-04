a076f0bee66808c1bfabd71e5a4acbddbe424b2c
corey buckley <coreybuckley13@gmail.com>
2020-04-04 02:31:04 -0400

    Merge XML chunks into one string while maintaining the being xml well-formed. Then beautiful soup can parse only one string. Also use soup strainer to filter out not-needed data. More naive approach (last commit): ~60secs. This commit: ~30secs. 50% improvement.

--- rpi-data/modules/fetch-course-info.py
+++ rpi-data/modules/fetch-course-info.py
@@ -1,62 +1,95 @@
 import requests as req
-import asyncio
+import threading
 import unicodedata
+import re
 from io import StringIO
 from bs4 import BeautifulSoup, SoupStrainer
 
-acalog_api_key = "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
-api_response_format = "xml"
 catalog_id = 20 # 2019-2020 course catalog
 chunk_size = 200 # max number of course ids per GET request
+acalog_api_key = "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
 
-acalog_search_endpoint = "http://rpi.apis.acalog.com/v2/search/courses"
-acalog_course_detail_endpoint = "http://rpi.apis.acalog.com/v2/content?options[full]=1&method=getItems&type=courses"
+ending_catalog_regex = re.compile("</catalog>$")
+allow_for_extension_regex = re.compile("(<catalog.*?>)|(<\/catalog>)|(<\?xml.*?\?>)")
+closed_for_extension_regex = re.compile("(<catalog.*?>)|(<\?xml.*?\?>)")
 
-def clean_utf(string):
-    return unicodedata.normalize("NFKD", string)
+def dwrite_obj(obj):
+    with open("temp-xml.xml", "w") as file:
+        file.write(obj.__str__())
 
-async def request_all_course_ids():
-    return req.get(f"{acalog_search_endpoint}?key={acalog_api_key}&format={api_response_format}&method=listing&catalog={catalog_id}&options[limit]=0")
+class acalog_client():
+    def __init__(self, api_key):
+        self.search_endpoint = "http://rpi.apis.acalog.com/v2/search/courses"
+        self.course_detail_endpoint = "http://rpi.apis.acalog.com/v2/content?options[full]=1&method=getItems&type=courses"
+        self.api_key = api_key
+        self.api_response_format = "xml"
 
-async def request_course_details(id_params):
-    return req.get(f"{acalog_course_detail_endpoint}&key={acalog_api_key}&format={api_response_format}&catalog=20&{id_params}")
+    def _clean_utf(self, string):
+        return unicodedata.normalize("NFKD", string)
 
-async def get_all_course_ids_as_url_params():
-    only_ids = SoupStrainer("id")
-    all_ids_xml_resp = await request_all_course_ids()
-    id_tags = BeautifulSoup(all_ids_xml_resp.content, "xml", parse_only=only_ids).find_all("id")
-    return [f"ids[]={id_xml.text}" for id_xml in id_tags]
+    def _get_course_ids(self):
+        return req.get(f"{self.search_endpoint}?key={self.api_key}&format={self.api_response_format}&method=listing&catalog={catalog_id}&options[limit]=0")
 
-async def get_all_courses_xml(course_ids_url_params):
-    course_count = len(course_ids_url_params)
-    id_chunks = [course_ids_url_params[i:i+chunk_size] for i in range(0, course_count, chunk_size)]
-    courses_xml_chunks = []
-    for id_chunk in id_chunks:
-        query_param_chunk = "&".join(id_chunk)
-        resp = await request_course_details(query_param_chunk)
-        courses_xml_chunks.append(resp.content)
-    return courses_xml_chunks
+    def _get_course_details(self, id_params):
+        return req.get(f"{self.course_detail_endpoint}&key={self.api_key}&format={self.api_response_format}&catalog=20&{id_params}")
 
-async def get_all_courses(courses_xml_chunks):
-    courses = []
-    for course_chunk in courses_xml_chunks:
-        bs = BeautifulSoup(course_chunk, "xml")
-        root = bs.find("catalog")
-        raw_courses_xml = root.find_all("courses")
+    def _course_xml_ids_to_url_params(self, ids):
+        only_ids = SoupStrainer("id")
+        all_ids_xml_resp = ids
+        id_tags = BeautifulSoup(all_ids_xml_resp.content, "xml", parse_only=only_ids).find_all("id")
+        return [f"ids[]={id_xml.text}" for id_xml in id_tags]
 
+    def get_all_courses_xml(self, course_ids_url_params):
+        course_count = len(course_ids_url_params)
+        id_chunks = [course_ids_url_params[i:i+chunk_size] for i in range(0, course_count, chunk_size)]
+        chunk_count = len(id_chunks)
+        courses_xml_chunks = []
+        for i, id_chunk in enumerate(id_chunks):
+            query_param_chunk = "&".join(id_chunk)
+            resp = self._get_course_details(query_param_chunk)
+            # if first get rid of ending </catalog>
+            # else get rid of prolog and <catalog> and </catalog>
+            if (i != 0 and i != chunk_count - 1):
+                # https://stackoverflow.com/questions/39119165/xml-what-does-that-question-mark-mean
+                # Can only have one prolog per XML document in order for it to be well-formed.
+                courses_xml_chunks.append(allow_for_extension_regex.sub("", resp.content.decode("utf8")))
+            elif (i == chunk_count - 1):
+                courses_xml_chunks.append(closed_for_extension_regex.sub("", resp.content.decode("utf8")))
+            else:
+                # Don't see a reason to keep it as a bytestring, beautifulsoup
+                # is eventually just going to convert it to utf8 anyway
+                courses_xml_chunks.append(ending_catalog_regex.sub("", resp.content.decode("utf8")))
+        return courses_xml_chunks
+
+    def _get_all_courses(self, courses_xml_chunks):
+        courses = []
+        only_courses = SoupStrainer("courses")
+        merged = "".join(courses_xml_chunks)
+        # dwrite_obj(merged)
+        bs = BeautifulSoup(merged, "xml", parse_only=only_courses)
+        raw_courses_xml = bs.find_all("course")
         for raw_course in raw_courses_xml:
             content = raw_course.find("content")
-            department = content.find("prefix").text
-            level = content.find("code").text
-            title = content.find("name").text
-            # https://stackoverflow.com/a/34669482/8088388
-            courses.append({"description": clean_utf(content.find(type="acalog-field-471").text), "full_name": clean_utf(content.find("name").text)})
-    return courses
+            description = content.find(type="acalog-field-471")
+            # If it has the description, it will be the correct schema of a course object.
+            # For some reason the course tag is used for not actual courses. Fun.
+            if (description):
+                # https://stackoverflow.com/a/34669482/8088388
+                courses.append({"description": self._clean_utf(content.find(type="acalog-field-471").text), "full_name": self._clean_utf(content.find("name").text)})
+        return courses
 
-async def main():
-    course_id_params = await get_all_course_ids_as_url_params()
-    courses_xml = await get_all_courses_xml(course_id_params)
-    courses = await get_all_courses(courses_xml)
-    print(courses[0])
+    def get_all_courses(self):
+        ids_xml = self._get_course_ids()
+        ids = self._course_xml_ids_to_url_params(ids_xml)
+        course_xml_chunks = self.get_all_courses_xml(ids)
+        # Should be 10 <catalog>..</catalog> tags where each has <= chunk (200) courses.
+        # dwrite_obj(course_xml_chunks)
+        return self._get_all_courses(course_xml_chunks)
 
-asyncio.run(main())
+def main():
+    c = acalog_client(acalog_api_key)
+    courses = c.get_all_courses()
+    print(len(courses))
+
+if __name__ == "__main__":
+    main()
