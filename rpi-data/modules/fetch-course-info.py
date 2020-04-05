e90edac8371be9741bfe2670adfc7ea13003e5a5
corey buckley <coreybuckley13@gmail.com>
2020-04-05 02:22:33 -0400

    Improve to ~5s. Threaded api calls and use XML tree instead of beautifulsoup when working on large XML files (~23MB).

--- rpi-data/modules/fetch-course-info.py
+++ rpi-data/modules/fetch-course-info.py
@@ -2,20 +2,31 @@ import requests as req
 import threading
 import unicodedata
 import re
-from io import StringIO
+from threading import Lock
+from time import time
+from io import StringIO, BytesIO
 from bs4 import BeautifulSoup, SoupStrainer
+from lxml import etree
 
 catalog_id = 20 # 2019-2020 course catalog
 chunk_size = 200 # max number of course ids per GET request
 acalog_api_key = "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
 
+COURSE_DETAIL_TIMEOUT = 120.00 # seconds
+
 ending_catalog_regex = re.compile("</catalog>$")
 allow_for_extension_regex = re.compile("(<catalog.*?>)|(<\/catalog>)|(<\?xml.*?\?>)")
-closed_for_extension_regex = re.compile("(<catalog.*?>)|(<\?xml.*?\?>)")
+prolog_and_root_ele_regex = re.compile("^(?P<prolog><\?xml.*?\?>)\s*(?P<root><catalog.*?>)")
 
 def dwrite_obj(obj):
     with open("temp-xml.xml", "w") as file:
         file.write(obj.__str__())
+
+# todo: try to use <fields>...</fields> to get all possible fields and map <course> to these.
+#       they'll just be that internal name like 'acalog-field-814'. Not sure if there's a good
+#       and easy way to have a dynamic key name to properly represent the data..
+#
+#       threads? haha
 
 class acalog_client():
     def __init__(self, api_key):
@@ -23,73 +34,95 @@ class acalog_client():
         self.course_detail_endpoint = "http://rpi.apis.acalog.com/v2/content?options[full]=1&method=getItems&type=courses"
         self.api_key = api_key
         self.api_response_format = "xml"
+        self._lock = Lock()
+        self._course_details_xml_strs = []
+        self._xml_prolog = ""
+        self._catalog_root = ""
 
     def _clean_utf(self, string):
         return unicodedata.normalize("NFKD", string)
 
-    def _get_course_ids(self):
-        return req.get(f"{self.search_endpoint}?key={self.api_key}&format={self.api_response_format}&method=listing&catalog={catalog_id}&options[limit]=0")
+    def _all_threads_joined(self, threads):
+        for thread in threads:
+            thread.join(COURSE_DETAIL_TIMEOUT)
+            if (thread.is_alive()):
+                return False
+        return True
+
+    def get_course_ids_xml(self):
+        return req.get(f"{self.search_endpoint}?key={self.api_key}&format={self.api_response_format}&method=listing&catalog={catalog_id}&options[limit]=0").content
 
     def _get_course_details(self, id_params):
-        return req.get(f"{self.course_detail_endpoint}&key={self.api_key}&format={self.api_response_format}&catalog=20&{id_params}")
+        course_details_xml_str = req.get(f"{self.course_detail_endpoint}&key={self.api_key}&format={self.api_response_format}&catalog=20&{id_params}").content.decode("utf8")
+        with self._lock:
+            # https://stackoverflow.com/questions/39119165/xml-what-does-that-question-mark-mean
+            # Can only have one prolog per XML document in order for it to be well-formed.
+            # Can also only have one root.
+            # Don't see a reason to keep it as a bytestring, beautifulsoup
+            # is eventually just going to convert it to utf8 anyway
+            match = prolog_and_root_ele_regex.match(course_details_xml_str)
+            self._xml_prolog = match.group("prolog")
+            self._catalog_root = match.group("root")
+            self._course_details_xml_strs.append(allow_for_extension_regex.sub("", course_details_xml_str))
 
-    def _course_xml_ids_to_url_params(self, ids):
+
+    def _course_xml_ids_to_url_params(self, all_ids_xml):
         only_ids = SoupStrainer("id")
-        all_ids_xml_resp = ids
-        id_tags = BeautifulSoup(all_ids_xml_resp.content, "xml", parse_only=only_ids).find_all("id")
+        id_tags = BeautifulSoup(all_ids_xml, "xml", parse_only=only_ids).find_all("id")
         return [f"ids[]={id_xml.text}" for id_xml in id_tags]
 
     def get_all_courses_xml(self, course_ids_url_params):
         course_count = len(course_ids_url_params)
         id_chunks = [course_ids_url_params[i:i+chunk_size] for i in range(0, course_count, chunk_size)]
         chunk_count = len(id_chunks)
-        courses_xml_chunks = []
-        for i, id_chunk in enumerate(id_chunks):
+        courses_xml = ""
+        thread_jobs = []
+        for id_chunk in id_chunks:
             query_param_chunk = "&".join(id_chunk)
-            resp = self._get_course_details(query_param_chunk)
-            # if first get rid of ending </catalog>
-            # else get rid of prolog and <catalog> and </catalog>
-            if (i != 0 and i != chunk_count - 1):
-                # https://stackoverflow.com/questions/39119165/xml-what-does-that-question-mark-mean
-                # Can only have one prolog per XML document in order for it to be well-formed.
-                courses_xml_chunks.append(allow_for_extension_regex.sub("", resp.content.decode("utf8")))
-            elif (i == chunk_count - 1):
-                courses_xml_chunks.append(closed_for_extension_regex.sub("", resp.content.decode("utf8")))
-            else:
-                # Don't see a reason to keep it as a bytestring, beautifulsoup
-                # is eventually just going to convert it to utf8 anyway
-                courses_xml_chunks.append(ending_catalog_regex.sub("", resp.content.decode("utf8")))
-        return courses_xml_chunks
+            fetch_course_details_job = threading.Thread(target=self._get_course_details, args=[query_param_chunk], daemon=False)
+            thread_jobs.append(fetch_course_details_job)
+            fetch_course_details_job.start()
+        # Wait until all jobs are finished
+        while True:
+            if self._all_threads_joined(thread_jobs):
+                break
+        courses_xml = self._xml_prolog + self._catalog_root + "".join(self._course_details_xml_strs) + "</catalog>"
+        return courses_xml
 
-    def _get_all_courses(self, courses_xml_chunks):
+    def _get_all_courses(self, courses_xml_str):
+        tree = etree.parse(StringIO(courses_xml_str))
+        # root = tree.getroot()
+        # namespace = etree.QName(root).namespace
+        # raw_courses_xml = BeautifulSoup(courses_xml_str, "xml", parse_only=only_courses).find_all("course")
+        # raw_courses_xml = root.findall(f"{{{namespace}}}courses//{{{namespace}}}course")
+        # https://stackoverflow.com/a/4256011/8088388
+        course_content_xml = tree.getroot().xpath("//*[local-name() = 'course']/*[local-name() = 'content']")
         courses = []
-        only_courses = SoupStrainer("courses")
-        merged = "".join(courses_xml_chunks)
-        # dwrite_obj(merged)
-        bs = BeautifulSoup(merged, "xml", parse_only=only_courses)
-        raw_courses_xml = bs.find_all("course")
-        for raw_course in raw_courses_xml:
-            content = raw_course.find("content")
-            description = content.find(type="acalog-field-471")
+        for raw_course in course_content_xml:
+            # The description <field> can have multiple children tags, so get all text nodes.
+            # One example of this is ARCH 4870 - Sonics Research Lab 1, catalog 20, courseid 38592
+            description_xml = raw_course.xpath("*[local-name() = 'field'][@type='acalog-field-471']")
+            name_xml = raw_course.xpath("*[local-name() = 'name']")
             # If it has the description, it will be the correct schema of a course object.
             # For some reason the course tag is used for not actual courses. Fun.
-            if (description):
+            if (description_xml and name_xml):
+                description = " ".join(description_xml[0].xpath(".//text()"))
+                name = name_xml[0].text
                 # https://stackoverflow.com/a/34669482/8088388
-                courses.append({"description": self._clean_utf(content.find(type="acalog-field-471").text), "full_name": self._clean_utf(content.find("name").text)})
+                courses.append({"description": self._clean_utf(description), "full_name": self._clean_utf(name)})
         return courses
 
     def get_all_courses(self):
-        ids_xml = self._get_course_ids()
+        ids_xml = self.get_course_ids_xml()
         ids = self._course_xml_ids_to_url_params(ids_xml)
-        course_xml_chunks = self.get_all_courses_xml(ids)
-        # Should be 10 <catalog>..</catalog> tags where each has <= chunk (200) courses.
-        # dwrite_obj(course_xml_chunks)
-        return self._get_all_courses(course_xml_chunks)
+        courses_xml_str = self.get_all_courses_xml(ids)
+        return self._get_all_courses(courses_xml_str)
 
 def main():
     c = acalog_client(acalog_api_key)
     courses = c.get_all_courses()
-    print(len(courses))
+    # print(len(courses))
+    print(courses[0])
 
 if __name__ == "__main__":
     main()
