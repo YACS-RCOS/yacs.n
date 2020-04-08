bc240e30116aeb69ab0dcf9dd8dfa2aed5fb1bdd
Jacob Shomstein <jacob@shomstein.com>
2020-04-08 01:05:20 -0400

    allowing to create more than one server now

--- ops/provision.js
+++ ops/provision.js
@@ -6,14 +6,15 @@ const path = require('path')
 SETTINGS = {
   SSH_PUB_KEY: process.env.SSH_PUB_KEY,
   SERVER_ROOT_PASS: process.env.SERVER_ROOT_PASS || "some_pass",
-  BRANCH: process.env.BRANCH || "master"
+  BRANCH: process.env.BRANCH || "master",
+  ACTIVE_SERVER_COUNT: process.env.ACTIVE_SERVER_COUNT || 3
 }
 
 const infraSync = async () => {
 
   console.info(`Deploying Branch: ${SETTINGS.BRANCH}`)
   console.info('--- manage script ---')
-  console.info('[INFO] updating #yacs-rcos scripts')
+  console.info('[INFO] updating #yacs-rcos script')
 
   script = await Linode.script.update(639784, {
     images: ["linode/ubuntu18.04"],
@@ -25,13 +26,15 @@ const infraSync = async () => {
 
   // ------------ VMS
 
-  console.info('--- manage vm ---')
+  console.info('--- manage vm(s) ---')
   console.info('[INFO] fetching all #yacs-rcos vms')
   vms = await Linode.node.list()
+  console.log(`[INFO] currently, ${vms.length} vms are running/active`)
 
   // fetch current to delete later
   rcosVmIds = vms
     .filter(vm => vm.tags.includes('yacs-rcos'))
+    .sort((a,b) => Date(a.created) - Date(b.created)) // sort by date created, earliest created first
     .map(vm => vm.id)
 
   // create a new vm
@@ -50,13 +53,16 @@ const infraSync = async () => {
   })
 
   // delete existing vm(s) after creation
-  console.info('[INFO] deleting old vm(s) by tag #yacs-rcos')
-  for (linodeId of rcosVmIds) {
-    console.info(`[INFO] deleting vm: ${linodeId}`)
-    await Linode.node.remove(linodeId)
+  const amountToRemove = (rcosVmIds.length + 1) - SETTINGS.ACTIVE_SERVER_COUNT // plus one since we just added one
+  console.info(`[INFO] deleting old vm(s) by tag #yacs-rcos that surpass server allotment of ${SETTINGS.ACTIVE_SERVER_COUNT}`)
+  console.info(`[INFO] amount to remove: ${amountToRemove}`)
+  // rcosVmIds is sorted, this allows us to remove the oldest server(s)
+  for (let i = 0; i < amountToRemove; i++) {
+    console.info(`[INFO] deleting vm: ${rcosVmIds[i]}`)
+    await Linode.node.remove(rcosVmIds[i])
   }
 
-  // print ip for use.
+  // print ip
   // USED IN GITHUB ACTIONS PIPELINE TO SHOW MESSAGE IN PR
   // DO NOT REFORMAT
   console.log(`http://${info.ipv4[0]}/`)
