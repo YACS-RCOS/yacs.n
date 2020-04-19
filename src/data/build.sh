eef4e5e8f49ab4fa85ff6ff12804f20b4e17f8d7
corey buckley <coreybuckley13@gmail.com>
2020-04-19 19:10:27 -0400

    rename schemas to fix docker evaluating schemas alphabetically. Fix from #109

--- src/data/build.sh
+++ src/data/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 SCRIPTDIR=$(dirname "$BASH_SOURCE")
-psql -d yacs < $SCRIPTDIR/schema/course.sql
-psql -d yacs < $SCRIPTDIR/schema/course_session.sql
-psql -d yacs < $SCRIPTDIR/schema/admin_setting.sql
-psql -d yacs < $SCRIPTDIR/schema/semester_date_range.sql
-psql -d yacs < $SCRIPTDIR/schema/semester_info.sql
-psql -d yacs < $SCRIPTDIR/schema/course_corequisite.sql
-psql -d yacs < $SCRIPTDIR/schema/course_prerequisite.sql
-psql -d yacs < $SCRIPTDIR/schema/user.sql
-psql -d yacs < $SCRIPTDIR/schema/user_session.sql
-psql -d yacs < $SCRIPTDIR/schema/user_event.sql
+psql -d yacs < $SCRIPTDIR/schema/01_course.sql
+psql -d yacs < $SCRIPTDIR/schema/02_course_session.sql
+psql -d yacs < $SCRIPTDIR/schema/03_admin_setting.sql
+psql -d yacs < $SCRIPTDIR/schema/04_semester_date_range.sql
+psql -d yacs < $SCRIPTDIR/schema/05_course_corequisite.sql
+psql -d yacs < $SCRIPTDIR/schema/06_course_prerequisite.sql
+psql -d yacs < $SCRIPTDIR/schema/07_user.sql
+psql -d yacs < $SCRIPTDIR/schema/08_user_session.sql
+psql -d yacs < $SCRIPTDIR/schema/09_user_event.sql
+psql -d yacs < $SCRIPTDIR/schema/11_semester_info.sql
