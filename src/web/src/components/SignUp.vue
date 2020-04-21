16f283e51c683af1b51677a81b9e1adc8b201c40
Daniel Ackermans <ackerd2@rpi.edu>
2020-04-21 19:54:27 -0400

    Removed Clear button and Renamed Your name to Full name

--- src/web/src/components/SignUp.vue
+++ src/web/src/components/SignUp.vue
@@ -15,7 +15,7 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Full Name:" label-for="input-2">
         <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
       </b-form-group>
 
@@ -41,7 +41,6 @@
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
   </div>
 </template>
