037f58c3b2ff75eb030021ac7fea1d83370026d8
Daniel Ackermans <ackerd2@rpi.edu>
2020-04-21 22:25:43 -0400

    Added userID cookie storage back

--- src/web/src/components/SignUp.vue
+++ src/web/src/components/SignUp.vue
@@ -74,6 +74,7 @@ export default {
             console.log(response);
             this.$cookies.set('sessionID', response.data.content['sessionID']);
             this.$cookies.set('userName', response.data.content['userName']);
+            this.$cookies.set('userID', response.data.content['uid']);
             this.$router.go()
           })
         } else {
