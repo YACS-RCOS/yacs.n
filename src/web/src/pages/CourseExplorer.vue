44c36781d93ba7b116c31fdb08549b30c13eea23
123joshuawu <joshuawu3@gmail.com>
2020-06-19 16:14:07 -0700

    Add course load to courseexplorer

--- src/web/src/pages/CourseExplorer.vue
+++ src/web/src/pages/CourseExplorer.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    Hi im the course explorer
-  </div>
+  <div>Hi im the course explorer with {{ this.courses.length }} courses</div>
 </template>
 
 <script>
+import { getCourses } from "../services/YacsService";
 export default {
   name: "CourseExplorer",
   props: {
     selectedSemester: String,
+  },
+  data() {
+    return {
+      courses: [],
+    };
+  },
+  async created() {
+    this.courses = await getCourses(this.selectedSemester);
   },
 };
 </script>
