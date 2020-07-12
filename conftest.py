db9b6db289b3ec449d16496fdecd64932068a95b
Jacob Shomstein <jacob@shomstein.com>
2020-07-12 15:25:27 -0400

    added cache mock for testing

--- conftest.py
+++ conftest.py
@@ -9,6 +9,22 @@ from src.api.db.admin import Admin
 # from src.api.db.student_course_selection import student_course_selection
 # from src.api.db.user import User
 
+
+class MockCache:
+    """simple cache mock"""
+    def __init__(self):
+        self.__reset()
+
+    def clear(self):
+        self.cache_cleared = True
+
+    def __is_cleared(self):
+        return self.cache_cleared
+
+    def __reset(self):
+        self.cache_cleared = True
+
+
 from rpi_data.modules.fetch_catalog_course_info import acalog_client as AcalogClient
 
 TEST_CSV = os.environ.get('TEST_CSV', None)
@@ -17,9 +33,11 @@ TEST_CSV = os.environ.get('TEST_CSV', None)
 def db_conn():
     return db
 
+
 @pytest.fixture(scope="session")
 def class_info(db_conn):
-    return ClassInfo(db_conn)
+    mock_cache = MockCache()
+    return ClassInfo(db_conn, mock_cache)
 
 @pytest.fixture(scope="session")
 def admin_settings(db_conn):
@@ -42,4 +60,6 @@ def acalog_client():
 
 if TEST_CSV is not None:
     with open(TEST_CSV) as csvfile:
-        Courses(db).populate_from_csv(csvfile)
+        mock_cache = MockCache()
+        Courses(db, mock_cache).populate_from_csv(csvfile)
+        assert(mock_cache.is_cleared())
