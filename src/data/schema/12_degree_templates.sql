e3a12c742834db7992c65da31f40da5945eb6828
tant2 <tant2@rpi.edu>
2020-08-05 14:40:45 -0400

    Update 12_degree_templates.sql

--- src/data/schema/12_degree_templates.sql
+++ src/data/schema/12_degree_templates.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS degree_templates;
+
 CREATE TABLE degree_templates (
-    id          varchar(9),
+    id          varchar(255),
     degree      varchar(255),
-    major       varchar(4),
+    major       varchar(255),
     year        int,
-    semesters1   varchar(255),
+    semesters1   varchar(255),/*ITWS_2018_sm1 */
     semesters2   varchar(255),
     semesters3   varchar(255),
     semesters4   varchar(255),
@@ -16,10 +17,6 @@ CREATE TABLE degree_templates (
 );
 
 CREATE TABLE degree_templates_semester (
-    id          varchar(9),
-    degree      varchar(255),
-    major       varchar(4),
-    year        int,
     course1 varchar(255),
     course2 varchar(255),
     course3 varchar(255),
@@ -30,21 +27,6 @@ CREATE TABLE degree_templates_semester (
 );
 
 CREATE TABLE degree_option_course (
-    id          varchar(9),
-    degree      varchar(255),
-    major       varchar(4),
-    year        int,
-    name varchar(255),
-    description varchar(255),
-    id varchar(255),
-    PRIMARY KEY (name)
-);
-
-CREATE TABLE degree_option_department (
-    id          varchar(9),
-    degree      varchar(255),
-    major       varchar(4),
-    year        int,
     name varchar(255),
     description varchar(255),
     id varchar(255),
