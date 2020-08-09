d7fdf0028c0cbafa7b4c31f87f3893abc17a5440
123joshuawu <joshuawu3@gmail.com>
2020-08-08 18:44:58 -0700

    more tests

--- tests/api/db/test_classinfo.py
+++ tests/api/db/test_classinfo.py
@@ -34,19 +34,47 @@ def test_get_classes_full_with_semester_unique_crns(class_info, test_data):
 
 def test_get_departments(class_info, test_data):
     departments, err = class_info.get_departments()
+
     assert err is None
     assert len(test_data.departments - set(row['department'] for row in departments)) == 0
 
-# def test_get_subsemesters(class_info, test_data):
+def test_get_subsemesters(class_info, test_data):
+    subsemesters, err = class_info.get_subsemesters()
+    assert err is None
+    assert len(test_data.subsemesters) == len(subsemesters)
     
-def test_get_semesters(class_info, test_data):
+    expected_semester = None
+    expected_subsemesters = []
+    for semester in test_data.semesters:
+        expected_subsemesters = [s for s in test_data.subsemesters if s.parent_semester == semester]
+
+        if len(expected_subsemesters) > 0:
+            expected_semester = semester
+            break
+
+    subsemesters, err = class_info.get_subsemesters(expected_semester)
+    assert err is None
+    assert len(expected_subsemesters) == len(subsemesters)
+    for subsemester in subsemesters:
+        assert expected_semester == subsemester['parent_semester_name']
+    
+def test_get_semesters(class_info, test_data, semester_info):
     semesters, err = class_info.get_semesters(True)
     assert err is None
     assert len(test_data.semesters - set(row['semester'] for row in semesters)) == 0
 
-# todo test hidden semesters
+    semesters, err = class_info.get_semesters()
+    assert err is None
+    assert len(test_data.semesters - set(row['semester'] for row in semesters)) == 0
 
-# def test_get_all_semester_info(class_info, test_data):
+def test_get_all_semester_info(class_info, test_data):
+    semesters_info, err = class_info.get_all_semester_info()
+
+    assert err is None
+    assert len(test_data.semesters) == len(semesters_info)
+
+    for semester in semesters_info:
+        assert True == semester['public']
 
 def test_get_classes_by_search_by_department(class_info, test_data):
     expected = next(test_data.course_sessions_iter)
