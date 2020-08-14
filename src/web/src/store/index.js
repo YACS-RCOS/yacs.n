c93232231ed1bf46cb5100b57be893df7afbb84e
I'm a Github Action <beepboop@github.com>
2020-08-14 19:23:01 +0000

    beep boop Github Action Master Branch CI Prettier hard at work

--- src/web/src/store/index.js
+++ src/web/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
 
-export const TOGGLE_DARK_MODE = 'TOGGLE_DARK_MODE';
+export const TOGGLE_DARK_MODE = "TOGGLE_DARK_MODE";
 
 const store = new Vuex.Store({
   state: {
