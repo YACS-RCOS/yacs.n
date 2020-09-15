a5ef388f846baffc84d4188a849d2173ea6c5f01
I'm a Github Action <beepboop@github.com>
2020-09-15 21:36:59 +0000

    beep boop Github Action Master Branch CI Prettier hard at work

--- src/web/src/pages/CourseScheduler.vue
+++ src/web/src/pages/CourseScheduler.vue
@@ -353,7 +353,6 @@ export default {
       course.selected = false;
       this.scheduler.removeAllCourseSections(course);
 
-
       if (this.userID) {
         const info = {
           name: course.name,
