5290b05b49e7ab5ffee8ea757a755f34411f9739
Ike <Lyonsw@rpi.edu>
2020-10-04 17:55:09 -0400

    Get courseObj corresponding to course name

--- src/web/src/pages/CoursePage.vue
+++ src/web/src/pages/CoursePage.vue
@@ -3,12 +3,33 @@
 </template>
 
 <script>
+import { getCourses } from "../services/YacsService";
+import { getDefaultSemester } from "@/services/AdminService";
+
 export default {
   name: "CoursePage",
   data() {
     return {
       courseName: this.$route.params.course,
+      courseObj: {},
+      selectedSemester: String,
     };
+  },
+  async created() {
+    const querySemester = this.$route.query.semester;
+    this.selectedSemester =
+      querySemester && querySemester != "null"
+        ? querySemester
+        : await getDefaultSemester();
+    getCourses(this.selectedSemester).then((courses) => {
+      for (const c of courses) {
+        if (c.name === this.courseName) {
+          this.courseObj = c;
+          console.log(this.courseObj);
+        }
+      }
+      this.ready = true;
+    });
   },
 };
 </script>
