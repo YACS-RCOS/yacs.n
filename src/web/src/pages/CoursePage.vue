b6be962db1864fc922ba6d356ce3ba7ab0b665eb
Ike <Lyonsw@rpi.edu>
2020-10-09 14:58:36 -0400

    Center things

--- src/web/src/pages/CoursePage.vue
+++ src/web/src/pages/CoursePage.vue
@@ -1,19 +1,51 @@
 <template>
-  <h1>{{ courseName }}</h1>
+  <div v-if="ready" class="container w-100 mb-4">
+    <b-row>
+      <b-col>
+        <h1>{{ courseName }}</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        {{
+          generateRequirementsText(
+            courseObj.prerequisites,
+            courseObj.corequisites,
+            courseObj.raw_precoreqs
+          )
+        }}
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <br />
+        {{ courseObj.description }}
+      </b-col>
+    </b-row>
+  </div>
+  <div v-else>
+    <b-spinner></b-spinner>
+    <strong class="m-2">Loading courses...</strong>
+  </div>
 </template>
 
 <script>
 import { getCourses } from "../services/YacsService";
 import { getDefaultSemester } from "@/services/AdminService";
+import { generateRequirementsText } from "@/utils";
 
 export default {
   name: "CoursePage",
   data() {
     return {
+      ready: false,
       courseName: this.$route.params.course,
       courseObj: {},
       selectedSemester: String,
     };
+  },
+  methods: {
+    generateRequirementsText,
   },
   async created() {
     const querySemester = this.$route.query.semester;
@@ -33,3 +65,10 @@ export default {
   },
 };
 </script>
+
+<style>
+.container {
+  text-align: center;
+  margin: ;
+}
+</style>
