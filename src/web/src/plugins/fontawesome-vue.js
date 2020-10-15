b4c1071624c4c9a1bf798e0303f9547d4f0b77db
Joshua Wu <joshuawu3@gmail.com>
2020-10-14 22:33:59 -0400

    Responsive header component (#229)
    * Extract login form to separate component
    
    * configure header menu items to collapse on md breakpoint
    
    * remove comments
    
    * add boostrap mixins, variables, and functions
    
    * cleaning up old code
    
    * fix darkmode align
    
    * fix navbar toggle color in dark mode
    
    * fix icon usage
    
    * add more context to comment
    
    * cleaning up login component

--- src/web/src/plugins/fontawesome-vue.js
+++ src/web/src/plugins/fontawesome-vue.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPaperPlane);
+// This allows for tree-shaking to minimize bundle size
+import {
+  faPaperPlane,
+  faMoon,
+  faCalendar,
+  faList,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPaperPlane, faMoon, faCalendar, faList, faBars);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
