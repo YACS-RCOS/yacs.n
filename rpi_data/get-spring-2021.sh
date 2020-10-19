f2c3b5468c7c18e96aed8d457ebfcf3f726e72e5
Richard Tsai <RichtXO@gmail.com>
2020-10-18 23:47:56 -0400

    small change for the bash script

--- rpi_data/get-spring-2021.sh
+++ rpi_data/get-spring-2021.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 export SEMESTER="SPRING 2021"
 SOURCE_URL=https://sis.rpi.edu/reg/zfs202101.htm DEST=p0.csv HEADER=True python modules/rpi-parse.py
 cat p* > spring-2021.csv
