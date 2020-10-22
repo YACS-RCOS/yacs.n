b048d62cc14ac2fe434da6124a6a6f11768f0eb8
Ike <Lyonsw@rpi.edu>
2020-10-22 18:09:26 -0400

    made subjectBox respect darkmode

--- src/web/src/pages/SubjectExplorer.vue
+++ src/web/src/pages/SubjectExplorer.vue
@@ -142,7 +142,6 @@ export default {
 
 .subjectBox {
   border: 3px solid black;
-  background: rgb(255, 255, 255);
   height: 3rem;
   width: 15rem;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
