0d5f53764db2fc1bfba4e640bfc6e834e11e76b9
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-10-30 13:11:05 -0400

    Initial Fix For Subject Display List Order In Department Major Page.

--- src/web/src/pages/SubjectExplorer.vue
+++ src/web/src/pages/SubjectExplorer.vue
@@ -121,6 +121,16 @@ export default {
     );
     this.rightColumnCourses.splice(0, 0);
 
+    const allTempData = this.leftColumnCourses.concat(this.rightColumnCourses);
+    //Reset All Column Data.
+    this.leftColumnCourses = [];
+    this.rightColumnCourses = [];
+    //Loop Through All Temp Data:
+    for(var k=0; k<allTempData.length-1; k+=1){
+      this.leftColumnCourses.push(allTempData[k]);
+      this.rightColumnCourses.push(allTempData[k+1]);
+    }
+
     this.ready = true;
   },
   methods: {},
