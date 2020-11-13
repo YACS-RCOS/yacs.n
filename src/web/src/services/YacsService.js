c6078d429d29f0cdc3a7fe23e94f0d81166d2055
Venkat Srinivas <30516782+venkatrsrinivas@users.noreply.github.com>
2020-11-13 15:42:14 -0500

    Fix To Random Course Listing Spaces + Missing "Hidden" Course Listings (#322)
    * s21 up
    
    * Removed Use Of Un-Utilized Full Title.
    
    * Major Update To Course Identifier To Allow For Courses Like RCOS w/ Varying Credits To Be Distinguished + Displayed Properly.
    
    * Added CRN + Minimum + Maximum Credits To Prevent Random Spaces Caused By Perceived Identical Courses.
    
    * Added Some Minor Comments To Easily In Worst-Case That Future => Similar Problems.
    
    Co-authored-by: Jacob Shomstein <jacob@shomstein.com>

--- src/web/src/services/YacsService.js
+++ src/web/src/services/YacsService.js
@@ -18,9 +18,17 @@ const client = axios.create({
  * a course.
  * @param {Course} courseObj an object that is a subclass of course
  * @returns {string} the unique identifier of a course
+ * NOTE: In The Case Course Listings Have Random Spaces In The Future,
+ * This Is Likely Due To Two Course Objects Having The Same Identifier. 
+ * Thus, Should Revise The Following Code, If Necessary.
  */
 const _getCourseIdentifier = (courseObj) => {
   return `
+    ${courseObj.crn}
+    ${courseObj.name}
+    ${courseObj.title}
+    ${courseObj.min_credits}
+    #{courseObj.max_credits}
     ${courseObj.department}
     ${courseObj.level}
     ${courseObj.date_start.getMonth() + 1}
