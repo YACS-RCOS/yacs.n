b7e0e4fba259367969cc3a0a5c46f8f209183365
IkeLyons <59896953+IkeLyons@users.noreply.github.com>
2020-11-16 12:18:59 -0500

    Prereq links (#323)
    * Deleting unused code
    
    * Add links to precoreq text
    
    * Check if there are no prereqs
    
    * remove logging
    
    * cn -> course_name
    
    * Better default text
    
    * Update CoursePage.vue
    
    Co-authored-by: Jacob Shomstein <jacob@shomstein.com>

--- src/web/src/pages/CoursePage.vue
+++ src/web/src/pages/CoursePage.vue
@@ -1,36 +1,28 @@
 <template>
-  <b-container fluid>
-    <div v-if="ready" class="w-90 ml-4 mb-4">
-      <b-row>
-        <b-col>
-          <h1 class="mt-4">{{ courseTitle }}</h1>
-          <h4 class="mb-1">{{ courseName }}</h4>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          {{
-            generateRequirementsText(
-              courseObj.prerequisites,
-              courseObj.corequisites,
-              courseObj.raw_precoreqs
-            )
-          }}
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col class="mb-4">
-          <br />
-          {{ courseObj.description }}
-        </b-col>
-      </b-row>
-      <b-button :to="backRoute">Back</b-button>
-    </div>
-    <div v-else>
-      <b-spinner></b-spinner>
-      <strong class="m-2">Loading Course...</strong>
-    </div>
-  </b-container>
+  <div v-if="ready" class="w-90 ml-4 mb-4">
+    <b-row>
+      <b-col>
+        <h1 class="mt-4">{{ courseTitle }}</h1>
+        <h4 class="mb-1">{{ courseName }}</h4>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <p v-html="transformed" />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="mb-4">
+        <br />
+        {{ courseObj.description }}
+      </b-col>
+    </b-row>
+    <b-button :to="backRoute">Back</b-button>
+  </div>
+  <div v-else>
+    <b-spinner></b-spinner>
+    <strong class="m-2">Loading Course...</strong>
+  </div>
 </template>
 
 <script>
@@ -46,7 +38,6 @@ export default {
       courseName: this.$route.params.course,
       courseTitle: String,
       courseObj: {},
-      allCoursePreReqs: {},
       selectedSemester: String,
       backRoute: String,
     };
@@ -62,7 +53,6 @@ export default {
         : await getDefaultSemester();
     const courses = await getCourses(this.selectedSemester);
     for (const c of courses) {
-      this.allCoursePreReqs[c.name] = c.prerequisites;
       if (c.name === this.courseName) {
         this.courseTitle = c.title;
         this.courseObj = c;
@@ -72,10 +62,32 @@ export default {
     this.ready = true;
   },
   computed: {
-    prereqTree() {
-      let treeData = {};
-      treeData.name = this.courseName;
-      return treeData;
+    transformed() {
+      let precoreqtext = this.courseObj.raw_precoreqs;
+      if (precoreqtext === null) {
+        return "No information on pre/corequisites";
+      }
+      const regex = /([A-Z]){4}( )([0-9]){4}/g;
+      while (precoreqtext.search(regex) != -1) {
+        let index = precoreqtext.search(regex);
+        let beforetext = precoreqtext.slice(0, index);
+        let dept = precoreqtext.slice(index, index + 4);
+        let course_name = precoreqtext
+          .slice(index, index + 9)
+          .split(" ")
+          .join("-");
+        let link = '<a href="/explore/'.concat(
+          dept,
+          "/",
+          course_name,
+          '">',
+          course_name,
+          "</a>"
+        );
+        let aftertext = precoreqtext.slice(index + 9);
+        precoreqtext = beforetext.concat(link, aftertext);
+      }
+      return precoreqtext;
     },
   },
 };
