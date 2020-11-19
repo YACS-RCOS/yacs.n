a7788aad8eff231f6bbab2e4b1e9f8f429a5810d
nick-lennox <44211696+nick-lennox@users.noreply.github.com>
2020-11-18 20:01:31 -0500

    294 - breadcrumb nav on explore pages (#328)
    * removed horizontal scroll on individual course pages
    
    * rows now in fluid b-container (as opposed to setting 0px margin class)
    
    * added breadcrumb nav to explore pages
    
    * changed hrefs to to
    
    * breadcrumb nav 'home' -> 'YACS'
    
    * spacing issues
    
    Co-authored-by: nick-lennox <lennon@rpi.edu>

--- src/web/src/pages/CoursePage.vue
+++ src/web/src/pages/CoursePage.vue
@@ -1,34 +1,38 @@
 <template>
-  <div v-if="ready" class="w-90 ml-4 mb-4">
-    <b-row>
-      <b-col>
-        <h1 class="mt-4">{{ courseTitle }}</h1>
-        <h4 class="mb-1">{{ courseName }}</h4>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <p v-html="transformed" />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col class="mb-4">
-        <br />
-        {{ courseObj.description }}
-      </b-col>
-    </b-row>
-    <b-button :to="backRoute">Back</b-button>
-  </div>
-  <div v-else>
-    <b-spinner></b-spinner>
-    <strong class="m-2">Loading Course...</strong>
-  </div>
+  <b-container fluid>
+    <b-breadcrumb :items="breadcrumbNav"></b-breadcrumb>
+    <div v-if="ready" class="w-90 ml-4 mb-4">
+      <b-row>
+        <b-col>
+          <h1 class="mt-4">{{ courseTitle }}</h1>
+          <h4 class="mb-1">{{ courseName }}</h4>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <p v-html="transformed" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="mb-4">
+          <br />
+          {{ courseObj.description }}
+        </b-col>
+      </b-row>
+      <b-button :to="backRoute">Back</b-button>
+    </div>
+    <div v-else>
+      <b-spinner></b-spinner>
+      <strong class="m-2">Loading Course...</strong>
+    </div>
+  </b-container>
 </template>
 
 <script>
 import { getCourses } from "../services/YacsService";
 import { getDefaultSemester } from "@/services/AdminService";
 import { generateRequirementsText } from "@/utils";
+
 
 export default {
   name: "CoursePage",
@@ -40,6 +44,21 @@ export default {
       courseObj: {},
       selectedSemester: String,
       backRoute: String,
+      breadcrumbNav: [{
+        text: 'YACS',
+        to: '/'
+      },
+      {
+        text: 'Explore',
+        to: '/explore'
+      },
+      {
+        text: this.$route.params.subject,
+        to: '/explore/' + this.$route.params.subject
+      },
+      {
+        text: this.$route.params.course,
+      }]
     };
   },
   methods: {
@@ -47,6 +66,7 @@ export default {
   },
   async created() {
     const querySemester = this.$route.query.semester;
+    console.log(this.$route)
     this.selectedSemester =
       querySemester && querySemester != "null"
         ? querySemester
