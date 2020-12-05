241db13e071e399c8b094b72b4ffb11332f1f326
123joshuawu <joshuawu3@gmail.com>
2020-12-04 23:56:29 -0500

    move migration to separate step in ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python src/api/database_session.py && 
-PYTHONPATH=src/api alembic upgrade head && 
 pytest --cov=. --cov-config=.coveragec -s --ignore=tests/test_data.py --ignore=tests/integration tests/
