65b111c4220d58495feef706330882f957c7c659
123joshuawu <joshuawu3@gmail.com>
2020-12-04 23:51:59 -0500

    fix directory

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python database_session.py
-&& PYTHONPATH=. alembic upgrade head
-&& pytest --cov=. --cov-config=.coveragec -s --ignore=tests/test_data.py --ignore=tests/integration tests/
+python src/api/database_session.py && 
+PYTHONPATH=src/api alembic upgrade head && 
+pytest --cov=. --cov-config=.coveragec -s --ignore=tests/test_data.py --ignore=tests/integration tests/
