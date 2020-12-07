988f433948948132c3de8ef8a0bfbf23eae88d37
123joshuawu <joshuawu3@gmail.com>
2020-12-07 00:25:41 -0500

    add pythonpath for alembic

--- scripts/dev-create-migration.sh
+++ scripts/dev-create-migration.sh
@@ -3,6 +3,6 @@
 docker-compose \
   -f docker-compose.yml \
   -f docker-compose.development.yml \
-  run --rm \
+  run --rm -e PYTHONPATH=/usr/src\
   yacs_api \
   alembic revision --autogenerate -m $1
