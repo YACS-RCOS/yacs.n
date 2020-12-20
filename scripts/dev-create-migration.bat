e882916092a63f01dfa106b4502b26f23634d769
123joshuawu <joshuawu3@gmail.com>
2020-12-19 23:06:39 -0500

    fix quotes for migration message

--- scripts/dev-create-migration.bat
+++ scripts/dev-create-migration.bat
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.development.yml run --rm -e PYTHONPATH=/usr/src yacs_api alembic revision --autogenerate -m %1
+docker-compose -f docker-compose.yml -f docker-compose.development.yml run --rm -e PYTHONPATH=/usr/src yacs_api alembic revision --autogenerate -m "%~1"
