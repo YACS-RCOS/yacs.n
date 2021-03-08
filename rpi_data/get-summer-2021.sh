8d1960188611693c9e9ba8c9c398215455bb2fe8
Daniel Ackermans <ackerd2@rpi.edu>
2021-03-08 08:51:49 -0500

    Updated summer2021 to account for partial semesters and update summer2021.csv

--- rpi_data/get-summer-2021.sh
+++ rpi_data/get-summer-2021.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 export SEMESTER="SUMMER 2021"
-SOURCE_URL=https://sis.rpi.edu/reg/zfs202105.htm DEST=p0.csv HEADERS=True python ./modules/rpi-parse.py
+SOURCE_URL=https://sis.rpi.edu/reg/zs202105.htm DEST=p0.csv HEADERS=True python ./modules/rpi-parse.py
+SOURCE_URL=https://sis.rpi.edu/reg/zs20210502.htm DEST=p1.csv HEADERS=False python ./modules/rpi-parse.py
+SOURCE_URL=https://sis.rpi.edu/reg/zs20210503.htm DEST=p2.csv HEADERS=False python ./modules/rpi-parse.py
 cat p* > summer-2021.csv
 rm -rf p*.csv
