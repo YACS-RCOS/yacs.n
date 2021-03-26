820b37bac3820f79d5390108d99aa56bd9739fd2
Richard Tsai <RichtXO@gmail.com>
2021-03-26 18:50:37 -0400

    fixed dark mode

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -12,33 +12,15 @@ import {
   LOAD_SEMESTERS,
   LOAD_SUBSEMESTERS,
   SELECT_SEMESTER,
+  TOGGLE_DARK_MODE,
 } from "@/store";
 export default {
   name: "App",
-  computed: {
-    darkMode() {
-      return this.$store.state.darkMode;
-    },
-  },
-  watch: {
-    darkMode(newState, oldState) {
-      if (newState === oldState) {
-        return;
-      }
-
-      const bodyClassList = document.getElementsByTagName("body")[0].classList;
-      if (newState) {
-        bodyClassList.add("dark");
-      } else {
-        bodyClassList.remove("dark");
-      }
-    },
-  },
   created() {
-    const bodyClassList = document.getElementsByTagName("body")[0].classList;
     if (this.$cookies.get("darkMode") == "true") {
-      bodyClassList.add("dark");
+      this.$store.commit(TOGGLE_DARK_MODE, true);
     }
+
     this.$store.dispatch(SELECT_SEMESTER);
     this.$store.dispatch(LOAD_DEPARTMENTS);
     this.$store.dispatch(LOAD_SEMESTERS);
