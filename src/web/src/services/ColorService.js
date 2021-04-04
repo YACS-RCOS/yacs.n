59f6cf76cf72bc2574ef27416a6678f9862c7d87
NickKing24 <78986301+NickKing24@users.noreply.github.com>
2021-04-03 21:44:05 -0400

    Added color blind friendly colors

--- src/web/src/services/ColorService.js
+++ src/web/src/services/ColorService.js
@@ -1,4 +1,5 @@
 import "@/typedef";
+import store from "@/store";
 
 /**
  * Ported over from old YACS
@@ -6,6 +7,7 @@ import "@/typedef";
  * @module ColorService
  */
 
+//Default Colors
 const COLORS = [
   "#ffd4df",
   "#ceeffc",
@@ -14,6 +16,11 @@ const COLORS = [
   "#f7e2f7",
   "#ede6df",
   "#ffe9cf",
+  "#ffd1fc",
+  "#d9fdfc",
+  "#e2caca",
+  "#A3CFE1",
+  "#ACDBA9",
 ];
 const TEXT_COLORS = [
   "#d1265d",
@@ -23,6 +30,11 @@ const TEXT_COLORS = [
   "#853d80",
   "#9d5733",
   "#d9652b",
+  "#ff00ef",
+  "#00a499",
+  "#ab1111",
+  "#014779",
+  "#116940",
 ];
 const BORDER_COLORS = [
   "#ff2066",
@@ -32,8 +44,66 @@ const BORDER_COLORS = [
   "#d373da",
   "#a48363",
   "#ff9332",
+  "#ff00ef",
+  "#00fff9",
+  "#ab0000",
+  "#015FA2",
+  "#298E33",
 ];
-const NUM_COLORS = 7;
+/** 
+ * Color Blind Friendly Colors
+ * 
+ * Adapted from Paul Tot's discrete rainbow scheme
+ * https://personal.sron.nl/~pault/#fig:scheme_rainbow_discrete
+ * 
+ * Colors adjusted using https://davidmathlogic.com/colorblind/
+ * 
+ * Color palette tested using 
+ * https://www.color-blindness.com/coblis-color-blindness-simulator/
+ */
+const COLOR_BLIND_COLORS = [
+  "#FB373E", //red
+  "#8CBBE3", //blue
+  "#F7F056", //yellow
+  "#CAE0AB", //green
+  "#DCCCE1", //purple
+  "#ede6df", //brown
+  "#F6C141", //orange
+  "#FFEBFE", //pink
+  "#6596CD", //aquamarine
+  "#e2caca", //maroon
+  "#2286AA", //dark blue
+  "#90C987", //dark green
+];
+const COLOR_BLIND_TEXT_COLORS = [
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+  "#000000",
+];
+const COLOR_BLIND_BORDER_COLORS = [
+  "#A30034",
+  "#00aff2",
+  "#ffcb45",
+  "#97C25B",
+  "#d373da",
+  "#a48363",
+  "#ff9332",
+  "#FFADFA",
+  "#32639A",
+  "#ab0000",
+  "#015FA2",
+  "#298E33",
+];
+const NUM_COLORS = 12;
 
 // TODO: use something persistent
 var assignmentIncr = 0;
@@ -41,11 +111,19 @@ var colorAssignments = new Map();
 
 const _getColor = (id) => {
   const assignment = _assign(id);
-  return {
-    primary: COLORS[assignment],
-    text: TEXT_COLORS[assignment],
-    border: BORDER_COLORS[assignment],
-  };
+  if (store.getters.colorBlindAssistState) {
+    return {
+      primary: COLOR_BLIND_COLORS[assignment],
+      text: COLOR_BLIND_TEXT_COLORS[assignment],
+      border: COLOR_BLIND_BORDER_COLORS[assignment],
+    };
+  } else {
+    return {
+      primary: COLORS[assignment],
+      text: TEXT_COLORS[assignment],
+      border: BORDER_COLORS[assignment],
+    };
+  }
 };
 
 const _assign = (id) => {
