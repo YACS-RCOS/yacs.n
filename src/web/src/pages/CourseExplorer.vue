1926e4e3e520042fc4ef31e85f6fd129f0b4c902
keerti-sundaram <70778626+keerti-sundaram@users.noreply.github.com>
2021-09-28 23:15:57 -0400

    Breadcrumb darkmode (#485)
    * Update dark.scss
    
    Added additional color (dark-tertiary) for the breadcrumb, need to determine HEX code, also added another category for breadcrumb.
    
    * Update CourseExplorer.vue
    
    * Update dark.scss
    
    Color update

--- src/web/src/pages/CourseExplorer.vue
+++ src/web/src/pages/CourseExplorer.vue
@@ -88,6 +88,7 @@ export default {
         },
         {
           text: "Explore",
+          
         },
       ],
     };
@@ -140,6 +141,7 @@ export default {
   },
   metaInfo() {
     return {
+     
       title: "Explore",
       titleTemplate: "%s | YACS",
       meta: [
