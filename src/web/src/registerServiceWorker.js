c8580403e893397cf6faa3300831a50d2ddedccf
Samuel Mei <Samoooomei@gmail.com>
2021-10-15 16:52:55 -0400

    Yoinked

--- src/web/src/registerServiceWorker.js
+++ src/web/src/registerServiceWorker.js
@@ -3,6 +3,21 @@
 import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
+  if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+
+    for(let registration of registrations) {
+
+            registration.unregister()
+
+    }}).catch(function(err) {
+
+        console.log('Service Worker registration failed: ', err);
+  
+    });
+  }
+  
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
