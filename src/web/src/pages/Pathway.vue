7f8aa4afa84b047ebc277161568a873aaf6418ba
tant2 <tant2@rpi.edu>
2021-10-19 19:57:05 -0400

    some UI change

--- src/web/src/pages/Pathway.vue
+++ src/web/src/pages/Pathway.vue
@@ -3,20 +3,21 @@
   <div fluid class="py-3 h-100">
     <h1>Intergrated Pathways</h1>
 
-    <b-modal ref="my-modal">
-      <div class="block text-left" v-if="showPath != null">
-        <h3 class="text-center">{{ showPath.Name[0] }}</h3>
+    <b-modal ref="my-modal" >
+      <div class="block text-left" v-if="showPath != null" md="10">
+        <h3 class="text-center" style="color:#d6001c">{{ showPath.Name[0] }}</h3>
         <div v-for="(item, category) in showPath" :key="category"> 
-            <h4>{{ category + ": " }}</h4>
-            <p v-for="course in item" :key="course" v-on:click="goPage(course)">{{ course }}</p>
+            <h4 style="color:#c35442">{{ category + ": " }}</h4>
+            <li v-for="course in item" :key="course" v-on:click="goPage(course)">{{ course }}</li>
+            <br>
         </div>
       </div>
     </b-modal>
 
     <b-row>
-      <b-col class = "pathBox" md="3" v-for="pathway in pathways" :key="pathway['Name']" v-on:click="ShowPath(pathway)">
+      <b-col class = "pathBox" md="4" v-for="pathway in pathways" :key="pathway['Name']" v-on:click="ShowPath(pathway)">
         <div calss = "roundBox">
-            <p class = "pathwayName"> {{ pathway["Name"][0] }}</p>
+            <p class = "pathwayName text-center" > {{ pathway["Name"][0] }}</p>
            
         </div>
       </b-col>
@@ -67,10 +68,8 @@ body {
 }
 
 .pathwayName{
-    border: 1px dashed #0088f8;
+    background: #ab2328;
+    height: 45px;
 }
 
-.roundBox{
-    background: red;
-}
 </style>
