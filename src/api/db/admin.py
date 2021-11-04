55f6c07d361124dcc1e972026c11358104f22188
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2021-11-04 17:33:14 -0400

    Resolved Default Semester Bug (#493)
    * fix: removes defaultsemester API cache
    
    * feat: delete old defaultsemester before insert
    
    Merged by @marchdan

--- src/api/db/admin.py
+++ src/api/db/admin.py
@@ -9,7 +9,7 @@ class Admin:
 		result, error = self.db_conn.execute("""
 			SELECT admin.semester FROM admin_settings admin
 			UNION ALL
-			SELECT si.semester FROM semester_info si WHERE si.public=true::boolean 
+			SELECT si.semester FROM semester_info si WHERE si.public=true::boolean
 			LIMIT 1
 		""", None, True)
 
@@ -18,7 +18,7 @@ class Admin:
 		if len(result) == 1:
 			# parse row
 			default_semester = result[0]['semester'] ## Only one record in table for admin_settings
-		
+
 		if error:
 			return (None, error)
 		else:
@@ -27,6 +27,7 @@ class Admin:
 	def set_semester_default(self, semester):
 		try:
 			cmd = """
+				WITH _ AS (DELETE FROM admin_settings)
 				INSERT INTO admin_settings(semester)
 				VALUES(%s)
 				ON CONFLICT (semester) DO UPDATE SET semester = %s
