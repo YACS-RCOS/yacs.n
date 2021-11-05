c012ad3fa3f3743987ef72feb8d25f211e35efca
NickKing24 <78986301+NickKing24@users.noreply.github.com>
2021-11-05 17:34:41 -0400

    Revert "Selected index cookie"
    This reverts commit 5187a4fb3ddb2bf049aed7675fa3962e507a6f53.

--- src/web/src/services/ColorService.js
+++ src/web/src/services/ColorService.js
@@ -129,6 +129,7 @@ const _getColor = (id) => {
 };
 
 const _assign = (id) => {
+  console.log(id)
   if (colorAssignments.has(id)) return colorAssignments.get(id);
   else return colorAssignments.set(id, _nextAssignment()).get(id);
 };
