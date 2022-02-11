41aad2c77dede29fd94ac442b7f04bdc54e4cada
hhdtc <49419262+hhdtc@users.noreply.github.com>
2022-02-11 17:59:46 -0500

    test user-get success

---
+++ src/api/tests/test_FastAPI-user-get.py
@@ -0,0 +1,47 @@
+from urllib import response
+import pytest
+import requests
+from .util import Client
+from .fixtures import *
+
+
+TEST_USER = { 'email': TEST_USER_SIGNUP['email'],
+              'password': TEST_USER_SIGNUP['password'] }
+
+
+'''
+TEST_USER_SIGNUP = { 'email': 'test@email.com',
+                     'name': 'TestName',
+                     'phone': '',
+                     'password': '123456',
+                     'degree': 'Undergraduate',
+                     'major': 'CSCI' }
+'''
+
+def test_get_user_success(client: Client):
+
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    headersa = r.headers
+    print (headersa['set-cookie'])
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    '''
+    {degree: "Undergraduate", email: "test@email.com", major: "CSCI", name: "TestName", phone: "", uid: 1}
+    '''
+    r = client.get("/api/user/"+data['content']['sessionID'],headers = {'Cookie': headersa['set-cookie']})
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content']['degree'] == TEST_USER_SIGNUP['degree']
+    assert data['content']['email'] == TEST_USER_SIGNUP['email']
+    assert data['content']['major'] == TEST_USER_SIGNUP['major']
+    assert data['content']['name'] == TEST_USER_SIGNUP['name']
+    assert data['content']['phone'] == TEST_USER_SIGNUP['phone']
+    assert data['content']['uid'] is not None
+    #assert(r.text == "YACS API is Up!")
+
+# def test_api(upload, client: Client):
+#     r = client.get("/api")
+#     assert(r.text == "wow")
--- src/api/tests/util.py
+++ src/api/tests/util.py
@@ -4,8 +4,8 @@ class Client():
     def __init__(self):
         self.URL = "http://0.0.0.0:5000"
 
-    def get(self, endpoint, params=None, json=None, data=None):
-        return requests.get(self.URL + endpoint, params=params, json=json, data=data)
+    def get(self, endpoint, params=None, json=None, data=None, headers=None):
+        return requests.get(self.URL + endpoint, params=params, json=json, data=data, headers = headers)
 
     def post(self, endpoint, params=None, json=None, data=None, files=None):
         return requests.post(self.URL + endpoint, params=params, json=json, files=files, data=data)
