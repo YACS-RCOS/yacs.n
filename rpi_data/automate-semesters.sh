537c695e56939254affcc83313b44b385b99d3a4
jaw12346 <jaw12346@gmail.com>
2022-02-11 17:59:51 -0500

    Fixed minor syntax and style issues

--- rpi_data/automate-semesters.sh
+++ rpi_data/automate-semesters.sh
@@ -1,59 +1,59 @@
 #!/usr/bin/env bash
 
 spring() {
-	local YEAR=${1}
-	SOURCE_URL=https://sis.rpi.edu/reg/zfs${YEAR}01.htm
-	if !(! curl -s --head  --request GET ${SOURCE_URL} | grep "404 Not Found" > /dev/null)
+	local year=${1}
+	source_url=https://sis.rpi.edu/reg/zfs${year}01.htm
+	if !(! curl -s --head  --request GET ${source_url} | grep "404 Not Found" > /dev/null)
     then
-			export SEMESTER="SPRING $YEAR"
+			export SEMESTER="SPRING ${year}"
 			DEST=p0.csv HEADERS=True python3 modules/rpi-parse.py
-			cat p* > spring-$YEAR.csv
+			cat p* > spring-${year}.csv
 			rm -rf p*.csv
 }
 
 summer() {
-	local YEAR=${1}
-	SOURCE_URL_A=https://sis.rpi.edu/reg/zfs${YEAR}05.htm
-	SOURCE_URL_B=https://sis.rpi.edu/reg/zfs${YEAR}0502.htm
-	SOURCE_URL_C=https://sis.rpi.edu/reg/zfs${YEAR}0503.htm
-	if !((! curl -s --head  --request GET ${SOURCE_URL_A} | grep "404 Not Found" > /dev/null)
-				&& (! curl -s --head  --request GET ${SOURCE_URL_B} | grep "404 Not Found" > /dev/null)
-				&& (! curl -s --head  --request GET ${SOURCE_URL_C} | grep "404 Not Found" > /dev/null)
+	local year=${1}
+	source_url_a=https://sis.rpi.edu/reg/zfs${year}05.htm
+	source_url_b=https://sis.rpi.edu/reg/zfs${year}0502.htm
+	source_url_c=https://sis.rpi.edu/reg/zfs${year}0503.htm
+	if !((! curl -s --head  --request GET ${source_url_a} | grep "404 Not Found" > /dev/null)
+				&& (! curl -s --head  --request GET ${source_url_b} | grep "404 Not Found" > /dev/null)
+				&& (! curl -s --head  --request GET ${source_url_c} | grep "404 Not Found" > /dev/null)
 			)
     then
-			export SEMESTER="SUMMER $YEAR"
-			SOURCE_URL=$SOURCE_URL_A HEADERS=True python3 modules/rpi-parse.py
-			SOURCE_URL=$SOURCE_URL_B HEADERS=False python ./modules/rpi-parse.py
-			SOURCE_URL=$SOURCE_URL_C HEADERS=False python ./modules/rpi-parse.py
-			cat p* > summer-$YEAR.csv
+			export SEMESTER="SUMMER ${year}"
+			SOURCE_URL=${source_url_a} HEADERS=True python3 modules/rpi-parse.py
+			SOURCE_URL=${source_url_b} HEADERS=False python3 ./modules/rpi-parse.py
+			SOURCE_URL=${source_url_c} HEADERS=False python3 ./modules/rpi-parse.py
+			cat p* > summer-${year}.csv
 			rm -rf p*.csv
 }
 
 fall() {
-	local YEAR=${1}
-	SOURCE_URL=https://sis.rpi.edu/reg/zfs${YEAR}09.htm
-	if !(! curl -s --head  --request GET ${SOURCE_URL} | grep "404 Not Found" > /dev/null)
+	local year=${1}
+	source_url=https://sis.rpi.edu/reg/zfs${year}09.htm
+	if !(! curl -s --head  --request GET ${source_url} | grep "404 Not Found" > /dev/null)
     then
-			export SEMESTER="SPRING $YEAR"
-			DEST=p0.csv HEADERS=True python modules/rpi-parse.py
-			cat p* > fall-$YEAR.csv
+			export SEMESTER="SPRING ${year}"
+			DEST=p0.csv HEADERS=True python3 modules/rpi-parse.py
+			cat p* > fall-${year}.csv
 			rm -rf p*.csv
 }
 
 
 MONTH=$(date +%m)
 YEAR=$(date +%y)
-if [[$MONTH -ge 01 ]] && [[$MONTH -le 05]]
+if [[${MONTH} -ge 01 ]] && [[${MONTH} -le 05]]
 then
-	spring(YEAR)
-	summer(YEAR)
+  spring(${YEAR})
+	summer(${YEAR})
 
-if [[$MONTH -ge 05 ]] && [[$MONTH -le 09]]
+if [[${MONTH} -ge 05 ]] && [[${MONTH} -le 09]]
 then
-	summer(YEAR)
-	fall(YEAR)
+	summer(${YEAR})
+	fall(${YEAR})
 
-if [[$MONTH -ge 09 ]] || [[$MONTH -eq 01]]
+if [[${MONTH} -ge 09 ]] || [[${MONTH} -eq 01]]
 then
-	fall(YEAR)
-	spring(YEAR)
+	fall(${YEAR})
+	spring(${YEAR})
