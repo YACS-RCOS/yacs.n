e6412e4cb0de05d4680740a14e4a7f9d6fbd924e
YuetingLiao <46305076+YuetingLiao@users.noreply.github.com>
2022-02-16 12:48:25 -0500

    Passed post success and failure for user/course

--- src/api/tests/test_user_course.py
+++ src/api/tests/test_user_course.py
@@ -2,57 +2,40 @@ from .util import Client
 
 TEST_USER = { 'email': 'test@email.com',
               'password': '123456' }
+
 TEST_USER_COURSE = {'name': 'ADMN-1824',
                     'semester': 'SUMMER 2020',
                     'cid': '-1'
 }
-# TEST_USER_SIGNUP = { 'email': 'test@email.com',
-#                      'name': 'TestName',
-#                      'phone': '',
-#                      'password': '123456',
-#                      'degree': 'Undergraduate',
-#                      'major': 'CSCI' }
+# pytest -s tests/test_user_course.py
 def test_user_course_post_success(post_user, client: Client):
     '''
     Test user course post by comparing it to user course get
     '''
     # Test if user can login
-    s = client.post("/api/session", json=TEST_USER)
-    assert s.status_code == 200
+    sess = client.post("/api/session", json=TEST_USER)
+    assert sess.status_code == 200
     # Test if the user can post course
     r = client.post("/api/user/course", json=TEST_USER_COURSE)
     assert r.status_code == 200
-    print(r.text)
-    data = r.json()
-    assert data['content'] is not None
-    assert data['content']['name'] is not None
-    assert data['content']['semester'] is not None
-    assert data['content']['cid'] is not None
-    # Test if the user can get from posted course data
     g = client.get("/api/user/course", json=TEST_USER_COURSE)
-    get_data = g.json()
-    #print(get_data)
-    assert data['content']['name'] == get_data['content']['name']
-    assert data['content']['semester'] == get_data['content']['semester']
-    assert data['content']['cid'] == get_data['content']['cid']
-
+    get_data = g.json()[0]
+    assert get_data['course_name'] == TEST_USER_COURSE['name']
+    assert get_data['semester'] == TEST_USER_COURSE['semester']
+    assert get_data['crn'] == TEST_USER_COURSE['cid']
+    # clear the session login
+    d = client.delete("/api/session", json={"sessionID": sess.json()['content']['sessionID']})
+    assert d.status_code == 200
 def test_user_course_post_failure(client: Client):
     '''
     Test user course post with invalid parameter
     '''
-    MISMATCH_TEST_USER_COURSE = {
-    "name": "AAAAAA",
-    "semester": "BBBB",
-    "cid": "9999"
-    }
-    s = client.post("/api/session", json=TEST_USER)
-    assert s.status_code == 200
-    r = client.post("api/user/course", json=TEST_USER_COURSE)
-    assert r.status_code == 200
-    data = r.json()
-    g = client.get("/api/user/course", json=MISMATCH_TEST_USER_COURSE)
-    get_data = g.json()
-
-    assert data['content']['name'] != get_data['content']['name']
-    assert data['content']['semester'] != get_data['content']['semester']
-    assert data['content']['cid'] != get_data['content']['cid']
+    TEST_INVALID_USER_COURSE = {}
+    sess = client.post("/api/session", json=TEST_USER).json()
+    sessID = sess['content']['sessionID']
+    # Test the post failure with invalid json
+    r = client.post("/api/user/course", json=TEST_INVALID_USER_COURSE)
+    assert r.status_code == 500
+    # clear the session login
+    d = client.delete('/api/session', json={'sessionID': sessID})
+    assert d.status_code == 200
