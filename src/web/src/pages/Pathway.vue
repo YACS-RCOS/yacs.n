47d05fec92e03bc215a36b6a8d81e760a5240ccd
katherineweng <kaiyaoweng@gmail.com>
2022-02-25 17:44:31 -0500

    moved everything into one vue file; deleted pathway.json and pathwaycomponent.vue (which is combined into pathway.vue)

--- src/web/src/pages/Pathway.vue
+++ src/web/src/pages/Pathway.vue
@@ -3,6 +3,20 @@
   <b-container fluid>
     <b-breadcrumb :items="breadcrumbNav"></b-breadcrumb>
     <div v-if="categories.length > 0" class="mx-auto w-75">
+      
+      <!-- pop-up window -->
+      <b-modal ref="my-modal" >
+        <div class="block text-left" v-if="showPath != null" md="10">
+            <h3 class="text-center" style="color:#007bff">{{ showPath.Name[0] }}</h3>
+            <br>
+            <div v-for="(item, itemName) in showPath" :key="itemName">
+                <h4 style="color:#3395ff">{{ itemName + ": " }}</h4>
+                <li v-for="course in item" :key="course" v-on:click="goPage(course)" class="courseInPath">{{ course }}</li>
+                <br>
+            </div>
+          </div>
+      </b-modal>
+
       <b-row>
         <!-- splited categories into 2 arrays, so we can have 2 columns -->
         <b-col
@@ -22,11 +36,26 @@
                   {{ categoryObj["Category Name"][0] }}
                 </h3>
               </b-row>
-              <!-- Pathway Title  -->
+              <!-- Pathway Names  -->
               <b-row>
-                <PathwayCategoriesList
-                  :pathways="categoryObj['Pathways']"
-                ></PathwayCategoriesList>
+
+                <div class="d-flex flex-column flex-grow-1">
+                  <!-- LOOP Through the Pathway Categories list -->
+                  <div v-for="pathway in categoryObj['Pathways']" :key="pathway['Name'][0]" role="tablist">
+                      <div class="mt-1 mb-1 w-100">
+                        <!-- pathway button -->
+                        <b-button
+                          @click="ShowPath(pathway)"
+                          squared
+                          variant="light"
+                          class="pathway-button m-0 ml-1"
+                        >
+                          {{ pathway["Name"][0] }}
+                        </b-button>
+                      </div>
+                  </div>
+                </div>
+
               </b-row>
             </b-col>
           </b-row>
@@ -40,6 +69,16 @@
       loadingMessage="Pathways"
       :topSpacing="30"
     />
+
+    <b-row id = "Note" class="categoryBox">
+      <b-col>
+        <li>You can explore different pathway by clicking the pathway boxes. </li>
+        <li>You can also check out the courses by clicking the listed courses. </li>
+        <li>You will be directed to the department page if the course is not specified.</li>
+        <li>However, the course may show up as "Course not found" if the course is not being offer this semester. </li>
+      </b-col>
+    </b-row>
+
   </b-container>
 </template>
 
@@ -47,13 +86,11 @@
 
 <script>
 import json from './pathwayV2.json'
-import PathwayCategoriesListComponenet from "@/components/PathwayCategoriesList";
 import CenterSpinnerComponent from "../components/CenterSpinner";
 
 export default {
   name: "Pathway",
   components: {
-    PathwayCategoriesList: PathwayCategoriesListComponenet,
     CenterSpinner: CenterSpinnerComponent,
   },
   data() {
@@ -72,6 +109,7 @@ export default {
     };
   },
   computed:{
+    // splited categories into 2 arrays, one array = one column
     categoryCols() {
       let ret = [];
       let col1 = [];
@@ -87,7 +125,31 @@ export default {
       ret.push(col2);
       return ret;
     }
-  }
+  },
+  methods: {
+    // Display a pop-up window when a pathway is clicked
+    ShowPath(pathway){
+        console.log(this.$refs["my-modal"]);
+        console.log(pathway);
+        this.showPath = pathway;
+        this.$refs["my-modal"].show();
+    },
+    // Go to the course page when a course inside the pop-up window is clicked
+    goPage(course){
+        var subject = "" + course[0] + course[1] + course[2] + course[3];
+        var courseID = "" + course[5] + course[6] + course[7] + course[8];
+        if(course[4] != ' '){
+            return;
+        }
+        if(course[8] == "X")
+        {
+            this.$router.push("/explore/" + subject);
+        }
+        else{
+            this.$router.push("/explore/" + subject +"/" + subject + "-" + courseID);
+        }
+    }
+  },
 };
 </script>
 
@@ -106,5 +168,25 @@ export default {
 .category-title {
   background: rgba(108, 90, 90, 0.15);
   border-bottom: rgba(108, 90, 90, 0.1), solid, 1px;
+}
+
+.pathway-button {
+  display: inline-block;
+  background: white;
+  border-style: none;
+  text-align: justify;
+  width: 95%;
+}
+
+.pathway-button:hover {
+  background: rgba(108, 90, 90, 0.15) !important;
+}
+
+.courseInPath{
+    cursor: pointer;
+}
+
+.courseInPath:hover {
+    background-color: rgba(39, 130, 230, 0.5);
 }
 </style>
