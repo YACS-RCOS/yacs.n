c2fafe2fbea3253dc8c6b8dc3176b3a64f793b16
Xies3 <79339550+Xies3@users.noreply.github.com>
2022-03-04 17:46:37 -0500

    add failure cases for get and delete

--- src/api/tests/test_user_course_get.py
+++ src/api/tests/test_user_course_get.py
@@ -10,10 +10,6 @@ TEST_COURSE = {'name': 'ARCH-4770',
                'semester': 'SUMMER2020'}
 
 
-# TEST_USER_COURSE = {'name': 'ADMN-1824',
-#                     'semester': 'SUMMER 2020',
-#                     'cid': '-1'
-# }
 
 
 def test_user_course_get_success(post_user, client: Client):
@@ -33,6 +29,3 @@ def test_user_course_get_failure(client: Client):
     r = client.post("/api/user/course", json = TEST_COURSE)
     assert r.status_code == 403
 
-
-
-# [{'course_name': 'ADMN-1824', 'crn': '-1', 'semester': 'SUMMER 2020'}, {'course_name': 'ADMN-1824', 'crn': '15486', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4770', 'crn': '-1', 'semester': 'SUMMER2020'}, {'course_name': 'ARCH-4770', 'crn': '-1', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4770', 'crn': '15413', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4770', 'crn': '15769', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4770', 'crn': '15815', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4780', 'crn': '-1', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4780', 'crn': '15771', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4780', 'crn': '15772', 'semester': 'SUMMER 2020'}, {'course_name': 'ARCH-4780', 'crn': '15816', 'semester': 'SUMMER 2020'}, {'course_name': 'ARTS-1380', 'crn': '-1', 'semester': 'SUMMER 2020'}, {'course_name': 'ARTS-1380', 'crn': '15710', 'semester': 'SUMMER 2020'}]
