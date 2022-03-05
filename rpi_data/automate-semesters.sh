6b5520b7a199acaeab88e9c5258d9e6113309ba1
Richard Tsai <richtxo@gmail.com>
2022-03-04 23:20:11 -0500

    Curl updated csv to endpoint, API doesn't work?

--- rpi_data/automate-semesters.sh
+++ rpi_data/automate-semesters.sh
@@ -8,12 +8,13 @@ spring() {
 	exitCode=$?
 	if [ ${exitCode} == 0 ]
 	then
-			export SEMESTER="SPRING ${year}"
-			SOURCE_URL=${source_url} DEST=p0.csv HEADERS=True python3 ./modules/rpi-parse.py
-			cat p* > spring-${year}.csv
-			rm -rf p*.csv
+		export SEMESTER="SPRING ${year}"
+		SOURCE_URL=${source_url} DEST=p0.csv HEADERS=True python3 ./modules/rpi-parse.py
+		cat p* > spring-${year}.csv
+		rm -rf p*.csv
+		curl -X POST -H "Content-Type: multipart/form-data" -F "isPubliclyVisible=on" -F "file=spring-${year}" -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0" $API_ENDPOINT
 	else
-			echo "The requested Spring SIS page returned error 404." >&2
+		echo "The requested Spring SIS page returned error 404." >&2
 	fi
 }
 
@@ -26,13 +27,14 @@ summer() {
 	python3 ./website_checker.py ${source_url_a} ${source_url_b} ${source_url}
 	exitCode=$?
 	if [ ${exitCode} == 0 ]
-  then
+    then
 		export SEMESTER="SUMMER ${year}"
 		SOURCE_URL=${source_url_a} HEADERS=True DEST=p0.csv python3 ./modules/rpi-parse.py
 		SOURCE_URL=${source_url_b} HEADERS=False DEST=p1.csv python3 ./modules/rpi-parse.py
 		SOURCE_URL=${source_url_c} HEADERS=False DEST=p2.csv python3 ./modules/rpi-parse.py
 		cat p* > summer-${year}.csv
 		rm -rf p*.csv
+		curl -X POST -H "Content-Type: multipart/form-data" -F "isPubliclyVisible=on" -F "file=summer-${year}" -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0" $API_ENDPOINT
 	else
 		echo "A requested Summer SIS page returned error 404." >&2
 	fi
@@ -46,11 +48,11 @@ fall() {
 	exitCode=$?
 	if [ ${exitCode} == 0 ]
 	then
-		echo "Fall"
 		export SEMESTER="FALL ${year}"
 		SOURCE_URL=${source_url} DEST=p0.csv HEADERS=True python3 ./modules/rpi-parse.py
 		cat p* > fall-${year}.csv
 		rm -rf p*.csv
+		curl -X POST -H "Content-Type: multipart/form-data" -F "isPubliclyVisible=on" -F "file=fall-${year}" -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0" $API_ENDPOINT
 	else
 		echo "A requested Fall SIS page returned error 404." >&2
 	fi
@@ -59,6 +61,7 @@ fall() {
 
 MONTH=$(date +%m)
 YEAR=$(date +%Y)
+API_ENDPOINT=https://yacs_web/api/bulkCourseUpload
 if [[ ${MONTH} -ge 1 && ${MONTH} -lt 5 ]]
 then
 	spring ${YEAR}
