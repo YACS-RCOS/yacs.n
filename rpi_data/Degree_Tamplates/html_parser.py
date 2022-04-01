f8c78bd8ed50ca5994e044a2e4f0e700ea691702
tant2 <tant2@rpi.edu>
2022-04-01 16:29:40 -0400

    delete things

--- rpi_data/Degree_Tamplates/html_parser.py
+++
@@ -1,7 +0,0 @@
-filename = input("filename:")
-fin = open(filename, "r")
-fout = open("majorURLlist.txt", "a")
-fout.truncate(0)
-for line in fin:
-    if(line[0:2] == "<a"):
-        fout.write("http://catalog.rpi.edu/" + line[9:61]+"\n")
