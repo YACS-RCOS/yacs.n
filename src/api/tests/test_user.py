f0c4a9eaaa971903fb7a480791a7ab43d08013c9
Liam Haining <ljhaining@gmail.com>
2022-04-02 00:25:44 -0400

    merged logically similar test files

--- src/api/tests/test_user.py
+++ src/api/tests/test_user.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 import pytest
+
 TEST_USER = { 'email': 'test@email.com',
               'password': '123456' }
 
@@ -10,7 +11,25 @@ TEST_USER_SIGNUP = { 'email': 'test@email.com',
                      'degree': 'Undergraduate',
                      'major': 'CSCI' }
 
-# pytest -s tests/test_user.py
+TEST_USER_SIGNUP_REVERT = { 'email': 'test@email.com',
+                     'name': 'TestName',
+                     'phone': '',
+                     'newPassword': '123456',
+                     'degree': 'Undergraduate',
+                     'major': 'CSCI' }
+
+TEST_USER2 = { 'email': "test2@email.com",
+              'password': "1234567" }
+
+
+TEST_USER_SIGNUP2 = { 'email': 'test2@email.com',
+                     'name': 'TestName2',
+                     'phone': '',
+                     'newPassword': '1234567',
+                     'degree': 'Graduate',
+                     'major': 'ECON' }
+
+
 @pytest.mark.testclient
 def test_user_post_success(post_user, client: TestClient):
     '''
@@ -40,7 +59,7 @@ def test_user_delete_success(post_user, client: TestClient):
     '''
     delete a valid user in the session
     '''
-    r1 = client.post("/api/session", json=TEST_USER) # {'email':'test12@gmail.com', 'password': 'test123'})
+    r1 = client.post("/api/session", json=TEST_USER)
     data = r1.json()
     sessionid = data['content']['sessionID']
     r2 = client.delete("/api/user", json= {"sessionID": sessionid, "password": "123456"})
@@ -48,15 +67,13 @@ def test_user_delete_success(post_user, client: TestClient):
     r=client.post('/api/user', json=TEST_USER_SIGNUP)
     assert r.status_code == 200
     data = r.json()
-    # assert data['content'] is not None
-    # assert data['content']['msg'] == "User added successfully."
 
 @pytest.mark.testclient
 def test_user_delete_failure(post_user, client: TestClient):
     '''
     delete a not exist user in the session
     '''
-    r1 = client.post("/api/session", json=TEST_USER) # {'email':'test12@gmail.com', 'password': 'test123'})
+    r1 = client.post("/api/session", json=TEST_USER)
     data = r1.json()
     sessionid = data['content']['sessionID']
     r2 = client.delete("/api/user", json= {"sessionID": sessionid, "password": "12345"})
@@ -75,3 +92,124 @@ def test_user_delete_failure2(post_user, client: TestClient):
     assert r.status_code == 200
     r1 = client.delete("/api/user", json= {"sessionID": sessID, "password": "12345"})
     assert r1.status_code == 403
+
+@pytest.mark.testclient
+def test_get_user_success(client: TestClient, post_user):
+    '''
+    Test user get by using /api/session and TEST_USER_SIGNUP.
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    sessionid = data['content']['sessionID']
+    r = client.get("/api/user/"+data['content']['sessionID'])
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content']['degree'] == TEST_USER_SIGNUP['degree']
+    assert data['content']['email'] == TEST_USER_SIGNUP['email']
+    assert data['content']['major'] == TEST_USER_SIGNUP['major']
+    assert data['content']['name'] == TEST_USER_SIGNUP['name']
+    assert data['content']['phone'] == TEST_USER_SIGNUP['phone']
+    assert data['content']['uid'] is not None
+    client.delete("/api/session", json={'sessionID': sessionid})
+
+@pytest.mark.testclient
+def test_get_user_failed(client: TestClient,post_user):
+    '''
+    Test user get with invalid sessionID
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    sessionid = data['content']['sessionID']
+
+    r = client.get("/api/user/"+"00000000")
+    assert r.status_code == 200
+    data = r.json()
+    assert data['errMsg'] == "Unable to find the session."
+    client.delete("/api/session", json={'sessionID': sessionid})
+
+@pytest.mark.testclient
+def test_get_user_after_session_closed(client: TestClient,post_user):
+    '''
+    Test user get by using /api/session and TEST_USER_SIGNUP after session is closed.
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    sessionid = data['content']['sessionID']
+    r=client.delete("/api/session", json={'sessionID': sessionid})
+    assert r.status_code==200
+    r = client.get("/api/user/"+sessionid)
+    assert r.status_code == 403
+
+
+@pytest.mark.testclient
+def test_put_user_success(client:TestClient,post_user):
+    '''
+    Test user put by changing TEST_USER_SIGNUP to TEST_USER_SIGNUP2
+    compare the user information with TEST_USER_SIGNUP2
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    sessionid = data['content']['sessionID']
+    TEST_USER_SIGNUP2['sessionID'] = data['content']['sessionID']
+    r = client.put("/api/user",json = TEST_USER_SIGNUP2)
+    assert r.status_code == 200
+
+    r=client.delete("/api/session", json={'sessionID': sessionid})
+    r = client.post("/api/session", json=TEST_USER2)
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP2['name']
+    sessionid = data['content']['sessionID']
+
+    r = client.get("/api/user/"+data['content']['sessionID'])
+    assert r.status_code == 200
+    data = r.json()
+    assert data['content']['degree'] == TEST_USER_SIGNUP2['degree']
+    assert data['content']['email'] == TEST_USER_SIGNUP2['email']
+    assert data['content']['major'] == TEST_USER_SIGNUP2['major']
+    assert data['content']['name'] == TEST_USER_SIGNUP2['name']
+    assert data['content']['phone'] == TEST_USER_SIGNUP2['phone']
+    assert data['content']['uid'] is not None
+    TEST_USER_SIGNUP_REVERT['sessionID'] = sessionid
+    r = client.put("/api/user",json = TEST_USER_SIGNUP_REVERT)
+
+    r=client.delete("/api/session", json={'sessionID': sessionid})
+
+
+@pytest.mark.testclient
+def test_put_user_after_session_closed(client:TestClient,post_user):
+    '''
+    Test user put by changing TEST_USER_SIGNUP to TEST_USER_SIGNUP2
+    after session is closed
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    data = r.json()
+
+    assert data['content'] is not None
+    assert data['content']['sessionID'] is not None
+    assert data['content']['userName'] == TEST_USER_SIGNUP['name']
+    sessionid = data['content']['sessionID']
+
+    TEST_USER_SIGNUP2['sessionID'] = data['content']['sessionID']
+    r=client.delete("/api/session", json={'sessionID': sessionid})
+    r = client.put("/api/user",json = TEST_USER_SIGNUP2)
+    assert r.status_code == 403
--- src/api/tests/test_user_course.py
+++ src/api/tests/test_user_course.py
@@ -6,15 +6,18 @@ TEST_USER = { 'email': 'test@email.com',
 
 TEST_USER_COURSE = {'name': 'ADMN-1824',
                     'semester': 'SUMMER 2020',
-                    'cid': '-1'
-}
+                    'cid': '-1'}
 
-'''
-Test this api endpoint/file locally with the following command line:
-src/api/tests/test.sh
-'''
+TEST_COURSE = {'name': 'ARCH-4770',
+               'cid': '-1',
+               'semester': 'SUMMER2020'}
+
+TEST_COURSE_2 = {'name': 'ARCH-4770',
+               'cid': None,
+               'semester': 'SUMMER2020'}
+
+
 @pytest.mark.testclient
-@pytest.mark.incompletedependency
 def test_user_course_post_success(post_user, client: TestClient):
     '''
     Test user course post by comparing it to user get course
@@ -52,4 +55,63 @@ def test_course_post_not_authorized(client: TestClient):
     sess = client.post("/api/session", json=TEST_USER).json()
     client.delete("/api/session", json={'sessionID': sess['content']['sessionID']})
     r = client.post("/api/user/course", json=TEST_USER_COURSE)
+    assert r.status_code == 403
+
+@pytest.mark.testclient
+def test_user_course_get_success(post_user, client: TestClient):
+    '''
+    Test user course get success
+    '''
+    r = client.post("/api/session", json=TEST_USER)
+    assert r.status_code == 200
+    course = client.post("/api/user/course", json=TEST_USER_COURSE)
+    assert course.status_code == 200
+    d = client.get("/api/user/course", json=TEST_USER_COURSE)
+    data = d.json()[0]
+    assert data['course_name'] == TEST_USER_COURSE['name']
+    assert data['crn'] == TEST_USER_COURSE['cid']
+    assert data['semester'] == TEST_USER_COURSE['semester']
+    client.delete("/api/session", json = {"sessionID": r.json()["content"]["sessionID"]})
+
+@pytest.mark.testclient
+def test_user_course_get_failure(client: TestClient):
+    '''
+    Test user course get without user session/login
+    '''
+    r = client.get("/api/user/course", json = TEST_USER_COURSE)
+    assert r.status_code == 403
+
+@pytest.mark.testclient
+def test_user_course_delete_success(post_user, client: TestClient):
+    '''
+    Test user/course delete success
+    '''
+    l = client.post("/api/session", json=TEST_USER)
+    assert l.status_code == 200
+    r = client.post("/api/user/course", json=TEST_COURSE)
+    assert r.status_code == 200
+    r = client.get("/api/user/course")
+    assert r.status_code == 200
+    data = r.json()
+    db_course = {'course_name': TEST_COURSE['name'], 'crn': TEST_COURSE['cid'], 'semester': TEST_COURSE['semester']}
+    assert db_course in data
+
+    x = client.delete("/api/user/course", json = TEST_COURSE)
+    assert x.status_code == 200
+    r = client.get("/api/user/course", json=TEST_COURSE)
+    data = r.json()
+    assert db_course not in data
+    client.delete("/api/session", json = {"sessionID": l.json()["content"]["sessionID"]})
+
+@pytest.mark.testclient
+def test_user_course_delete_failure(client: TestClient):
+    '''
+    Test user course delete with wrong type
+    Test user course delete without session/login
+    '''
+    l = client.post("/api/session", json=TEST_USER)
+    r = client.delete("/api/user/course", json={})
+    assert r.status_code == 422
+    client.delete("/api/session", json = {"sessionID": l.json()["content"]["sessionID"]})
+    r = client.delete("/api/user/course", json=TEST_COURSE)
     assert r.status_code == 403
