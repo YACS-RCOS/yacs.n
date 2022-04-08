3089193ed859be2490e4169560012a5212ce237c
franchen28 <90725345+franchen28@users.noreply.github.com>
2022-04-08 17:30:19 -0400

    Prerequisite model

---
+++ src/api/models/course_prerequisite.py
@@ -0,0 +1,10 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class CoursePrerequisite(Model):
+    department = fields.CharField(max_length=255)
+    level = fields.IntField()
+    prerequisite = fields.CharField(max_length=255)
+    
+    class Meta:
+        table = "course_prerequisite"
