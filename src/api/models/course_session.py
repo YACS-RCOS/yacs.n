4276a9397e2be30615f34e192bb9d6b622b332c5
Liam Haining <ljhaining@gmail.com>
2022-04-08 17:34:56 -0400

    fixed fields

--- src/api/models/course_session.py
+++ src/api/models/course_session.py
@@ -3,15 +3,15 @@ from tortoise.models import Model
 
 
 class CourseSession(Model):
-    crn = fields.VarcharField(max_length=255)
-    section = fields.VarcharField(max_length=255)
-    semester = fields.VarcharField(max_length=255)
+    crn = fields.CharField(max_length=255)
+    section = fields.CharField(max_length=255)
+    semester = fields.CharField(max_length=255)
     time_start = fields.TimeField()
     time_end = fields.TimeField()
-    day_of_week = fields.IntegerField()
-    location = fields.VarcharField(max_length=255)
-    session_type = fields.VarcharField(max_length=255)
-    instructor = fields.VarcharField(max_length=255)
+    day_of_week = fields.IntField()
+    location = fields.CharField(max_length=255)
+    session_type = fields.CharField(max_length=255)
+    instructor = fields.CharField(max_length=255)
 
     class Meta:
         table = "course_session"
