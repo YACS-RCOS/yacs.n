a1c8c39b8ab493957e41af2c27fa56cb05661dc2
pyalla26 <90935615+pyalla26@users.noreply.github.com>
2022-04-08 17:03:02 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
         collect: {
             url: ['https://localhost'],
             settings: {
-                chromeFlags: ['--ignore-certificate-errors', '--show-paint-rects', '--headless'],
+                chromeFlags: ['--ignore-certificate-errors', '--headless'],
                 // --- comment the formFactor and screenEmulation to test for mobile ---
                 formFactor: 'desktop',      
                 screenEmulation: {
