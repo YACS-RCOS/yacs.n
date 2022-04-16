6f63030a2f3213075358869fb7b99c5201a680ca
Liam Haining <ljhaining@gmail.com>
2022-04-15 21:07:18 -0400

    Added checks for coreqs

--- src/api/tests/test_bulk_upload.py
+++ src/api/tests/test_bulk_upload.py
@@ -29,6 +29,7 @@ def test_bulk_upload_success(upload, client: TestClient, event_loop: asyncio.Abs
     assert course1.semester == "SUMMER 2020"
 
     # For course 2 we will check all fields that should be populated
+    # Because of lack of TSVector support, we need to use only to select all but column tsv
     course2 = event_loop.run_until_complete(
         Course.get(crn="95659").only("crn", "section", "semester", "min_credits", "max_credits",
                                      "description", "frequency", "full_title", "date_start", "date_end",
@@ -50,9 +51,7 @@ def test_bulk_upload_success(upload, client: TestClient, event_loop: asyncio.Abs
 
     # Need to check all tables, not just Course
     session = event_loop.run_until_complete(
-        CourseSession.get(crn="95659", section="01", semester="SPRING 2020", day_of_week=1).only(
-            "crn", "section", "semester", "time_start", "time_end",
-            "day_of_week", "location", "session_type", "instructor"))
+        CourseSession.get(crn="95659", section="01", semester="SPRING 2020", day_of_week=1))
     assert session.crn == "95659" and session.section == "01"
     assert session.semester == "SPRING 2020"
     assert session.time_start == time(10, 0, tzinfo=pytz.timezone("UTC"))
@@ -62,11 +61,18 @@ def test_bulk_upload_success(upload, client: TestClient, event_loop: asyncio.Abs
     assert session.session_type == "LEC"
     assert session.instructor == "Lewis"
 
-# BIOMEDICAL ENGINEERING LAB,LEC,4,W,9:00AM,11:00AM,Agarwal,,12,9,3,BMED,2020-05-26,2020-08-21,SUMMER 2020,15297,4010,01,BMED-4010,Biomedical Engineering Laboratory,"Theory and practice of biomedical measurements. An introduction to instrumentation and procedures for measurement of membrane transport, bioelectrical potentials, cell counting, biomechanical and biomaterial properties using invasive and noninvasive techniques. Transducers studied include strain gauge, differential transformer, spectrophometer, bipotential electrodes, microscope with camera, mechanical testing machine, piezoelectric transducer (or sensor). Also studied are instruments for determination of material properties.","Corequisite:  BMED 4200 or permission of instructor. Prerequisites: BMED 2100, BMED 2300, and BMED 2540",Fall and spring terms annually.,"['BMED-2100', 'BMED-2300', 'BMED-2540']",['BMED-4200'],Engineering
     # Course 2 does not have pre/corequisites, so we will use Biomedical Engineering Lab to verify those tables
     prereqs = event_loop.run_until_complete(
         CoursePrerequisite.filter(department="BMED", level=4010))
     assert prereqs[0].department == "BMED" and prereqs[0].level == 4010
+    pre = [p.prerequisite for p in prereqs]
+    assert pre == ['BMED-2100', 'BMED-2300', 'BMED-2540']
+
+    coreqs = event_loop.run_until_complete(
+        CourseCorequisite.filter(department="BMED", level=4010))
+    assert coreqs[0].department == "BMED" and coreqs[0].level == 4010
+    co = [c.corequisite for c in coreqs]
+    assert co == ['BMED-4200']
 
 
 @pytest.mark.testclient
