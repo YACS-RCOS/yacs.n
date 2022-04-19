c04f468e3427960fbb5c4c44ceaf2ebd7dca3afc
ZHLOLin <linzhaolong.2010@gmail.com>
2022-04-19 16:40:19 -0400

    Update majorTemplates.py

--- src/api/db/majorTemplates.py
+++ src/api/db/majorTemplates.py
@@ -2,9 +2,16 @@ from datetime import date
 class MajorTemplates:
     def __init__(self, db_conn):
         self.db_conn = db_conn
-    
+    '''
+    Input:
+        major: student's major
+        year: Which year in templates{'First Year','Second Year','Third Year','Fourth Year'}
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        the offcial template of the given major, year ,and semester.
+    '''
     def find_major_template(self, major, year, semester):
-        sql =   '''
+        sql =   """
                 SELECT * 
                 FROM public."majorTemplates" 
                 WHERE
@@ -15,22 +22,36 @@ class MajorTemplates:
                 "Major" ASC,
                 array_position(array['First Year','Second Year','Third Year','Fourth Year'], "Year") ASC,
                 array_position(array['Summer', 'Fall', 'Spring'], "Semester") ASC;
-                '''
+                """
         args = (str(major), str(year), str(semester))
         return self.db.execute(sql, args, True)
-    
+    '''
+    Input:
+        major: student's major
+        year: Which year in templates{'First Year','Second Year','Third Year','Fourth Year'}
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        The list of courses in the offcial template of the given major, year ,and semester.
+    '''
     def find_courses(self, major, year, semester):
-        sql =   '''
+        sql =   """
                 SELECT unnest("Courses")
                 FROM public."majorTemplates" 
                 WHERE
                 "Major" = %s
                 and "Year" = %s
                 and "Semester" = %s;
-                '''
+                """
         args = (str(major), str(year), str(semester))
         return self.db.execute(sql, args, True)
-
+    '''
+    Input:
+        major: student's major
+        year: program start year
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        The list of courses a student should already taken.
+    '''
     def find_courses_already_taken(self, major, year, semester):
         todays_date = date.today()
         current_year = int(todays_date.year) - int(year)
@@ -55,7 +76,7 @@ class MajorTemplates:
             current_term = "Fifth Year"
 
 
-        sql =   '''
+        sql =   """
                 SELECT unnest("Courses")
                 FROM public."majorTemplates" 
                 WHERE
@@ -68,6 +89,6 @@ class MajorTemplates:
                 array_position(array['First Year','Second Year','Third Year','Fourth Year'], "Year") = array_position(array['First Year','Second Year','Third Year','Fourth Year'], %s)
                 and "Major" = %s
                 and array_position(array['Spring','Summer','Fall'], "Semester") <= array_position(array['Spring','Summer','Fall'], %s)
-                '''
+                """
         args = (str(current_term), str(major), str(current_term), str(major), str(semester))
         return self.db.execute(sql, args, True)
