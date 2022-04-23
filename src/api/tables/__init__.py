a98c72a28b5782d116bebf6f4d7dc8d1bd5a98f0
YuetingLiao <46305076+YuetingLiao@users.noreply.github.com>
2022-04-23 19:37:17 -0400

    Import model in model __init__ file

--- src/api/tables/__init__.py
+++ src/api/tables/__init__.py
@@ -1,8 +1,5 @@
 from .admin_settings import AdminSettings
 from .semester_date_range import SemesterDateRange
 from .semester_info import SemesterInfo
-from .user_event import UserEvent
-from .student_course_selection import StudentCourseSelection
-
 from .database import Base
 from .database_session import SessionLocal
