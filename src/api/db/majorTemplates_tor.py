3c8c3e1e9257b8bd20ffdf9219d03c57e18a4a28
ZHLOLin <linzhaolong.2010@gmail.com>
2022-04-26 16:14:47 -0400

    Create majorTemplates_tor.py
    Tortoise version update

---
+++ src/api/db/majorTemplates_tor.py
@@ -0,0 +1,99 @@
+
+from db.model import *
+from datetime import date
+class MajorTemplates(Model):
+    def __init__(self, db_conn):
+        super().__init__()
+    '''
+    Input:
+        major: student's major
+        year: Which year in templates{'First Year','Second Year','Third Year','Fourth Year'}
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        the offcial template of the given major, year ,and semester.
+    '''
+    async def find_major_template(self, major, year, semester):
+        sql =   """
+                SELECT * 
+                FROM public."majorTemplates" 
+                WHERE
+                "Major" = %s
+                and "Year" = %s
+                and "Semester" = %s
+                ORDER BY 
+                "Major" ASC,
+                array_position(array['First Year','Second Year','Third Year','Fourth Year'], "Year") ASC,
+                array_position(array['Summer', 'Fall', 'Spring'], "Semester") ASC;
+                """
+        args = (str(major), str(year), str(semester))
+        courses, error = await self.db.execute(sql, args, True)
+        return (courses, None) if not error else (False, error) 
+    '''
+    Input:
+        major: student's major
+        year: Which year in templates{'First Year','Second Year','Third Year','Fourth Year'}
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        The list of courses in the offcial template of the given major, year ,and semester.
+    '''
+    async def find_courses(self, major, year, semester):
+        sql =   """
+                SELECT unnest("Courses")
+                FROM public."majorTemplates" 
+                WHERE
+                "Major" = %s
+                and "Year" = %s
+                and "Semester" = %s;
+                """
+        args = (str(major), str(year), str(semester))
+        courses, error = await self.db.execute(sql, args, True)
+        return (courses, None) if not error else (False, error) 
+    '''
+    Input:
+        major: student's major
+        year: program start year
+        semester: {'Summer', 'Fall', 'Spring'}
+    Output:
+        The list of courses a student should already taken.
+    '''
+    async def find_courses_already_taken(self, major, year, semester):
+        todays_date = date.today()
+        current_year = int(todays_date.year) - int(year)
+        current_term = "First Year"
+        if current_year == 0:
+            current_term = "First Year"
+        elif current_year == 1 and semester == "Spring":
+            current_term = "First Year"
+        elif current_year == 1 and semester == "Fall":
+            current_year = "Second Year"
+        elif current_year == 2 and semester == "Spring":
+            current_term = "Second Year"
+        elif current_year == 2 and (semester == "Fall" or semester == "Summer"):
+            current_term = "Third Year"
+        elif current_year == 3 and (semester == "Spring" or semester == "Summer"):
+            current_term = "Third Year"
+        elif current_year == 3 and semester == "Fall":
+            current_year = "Fourth Year"
+        elif current_year == 4 and semester == "Spring":
+            current_term = "Fourth Year"
+        else:
+            current_term = "Fifth Year"
+
+
+        sql =   """
+                SELECT unnest("Courses")
+                FROM public."majorTemplates" 
+                WHERE
+                array_position(array['First Year','Second Year','Third Year','Fourth Year'], "Year") < array_position(array['First Year','Second Year','Third Year','Fourth Year'], %s)
+                and "Major" = %s
+                UNION
+                SELECT unnest("Courses")
+                FROM public."majorTemplates" 
+                WHERE
+                array_position(array['First Year','Second Year','Third Year','Fourth Year'], "Year") = array_position(array['First Year','Second Year','Third Year','Fourth Year'], %s)
+                and "Major" = %s
+                and array_position(array['Spring','Summer','Fall'], "Semester") <= array_position(array['Spring','Summer','Fall'], %s)
+                """
+        args = (str(current_term), str(major), str(current_term), str(major), str(semester))
+        courses, error = await self.db.execute(sql, args, True)
+        return (courses, None) if not error else (False, error) 
