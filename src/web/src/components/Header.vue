8b2c066cd6bd94be9642269fe1e7459adb37405a
Arnav Pawar <arnav.ap.pawar@gmail.com>
2022-11-11 16:43:29 -0500

    Creating GPA calculator
    GPA calculator initial addition

--- src/web/src/components/Header.vue
+++ src/web/src/components/Header.vue
@@ -50,6 +50,10 @@
           <font-awesome-icon icon="list" />
           Pathways
         </b-nav-item>
+        <b-nav-item :to="{ name: 'GPACalculator' }">
+          <font-awesome-icon icon="list" />
+          GPACalculator
+        </b-nav-item>
       </b-navbar-nav>
       <!-- If user has logged in -->
       <b-navbar-nav class="ml-auto">
---
+++ src/web/src/pages/GPACalculator.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>hi welcome to degree templates</p>
+  </template>
+  
+  <script>
+  export default {
+    name: "GPACalculator",
+  };
+  </script>
+  
+  <style></style>
+  
