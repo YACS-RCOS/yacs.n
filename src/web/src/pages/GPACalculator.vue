f6ebfb30b384225ac03237792bfa882379b88e99
Arnav Pawar <arnav.ap.pawar@gmail.com>
2022-11-11 17:15:17 -0500

    Updated GPAcalc
    created a vue file with functions that I will need to implement will take in user inputs with everything so no backend needed

--- src/web/src/pages/GPACalculator.vue
+++ src/web/src/pages/GPACalculator.vue
@@ -5,8 +5,40 @@
 <script>
 export default {
   name: "GPACalculator",
+  data() {
+    return {
+      breadcrumbNav: [
+        {
+          text: "YACS",
+          to: "/",
+        },
+        {
+          text: "GPACalculator",
+        },
+      ],
+    };
+  },
+  computed: {
+    getAverageForSem(creditsPerclass,gradeList) {
+        //list of all credits in order of what they imputed
+        //list of all grades in in order of inputed
+        //will list gpa for the given semester
+    },
+    getTotalAvg(creditHours, GPA, creditsPerclass, gradeList){
+        //number of credit hours
+        //GPA from thoes credit hours
+        //current semeser grade and creditsper class
+        //will return what new gpa will be and how its affected
+    }
+  }
 };
 </script>
 
-<style></style>
-  
+<style>
+.gridContainer {
+  display: inline-grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  align-content: center;
+}
+</style>
