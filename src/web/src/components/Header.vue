0b18eebd11f8b641e71ad6516dc8c4e28ca49c4c
Arnav Pawar <arnav.ap.pawar@gmail.com>
2022-11-11 16:48:24 -0500

    Update Header.vue

--- src/web/src/components/Header.vue
+++ src/web/src/components/Header.vue
@@ -54,6 +54,10 @@
           <font-awesome-icon icon="list" />
           GPACalculator
         </b-nav-item>
+        <b-nav-item :to="{ name: 'DegreeTemplates' }">
+          <font-awesome-icon icon="calendar" />
+          DegreeTemplates
+        </b-nav-item>
       </b-navbar-nav>
       <!-- If user has logged in -->
       <b-navbar-nav class="ml-auto">
