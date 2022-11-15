fc87e0b87b1371e2a6560674dc4addf9c3fb2f79
jeman <jemanp96@gmail.com>
2022-11-15 17:59:25 -0500

    Update CourseList.vue
    changed the filter

--- src/web/src/components/CourseList.vue
+++ src/web/src/components/CourseList.vue
@@ -174,6 +174,9 @@ export default {
           ? this.courseList
           : this.$store.getters.courses;
 
+
+      //filter finds out the departmemt first 
+
       // filter by selected department
       const filtered = courses.filter(
         (course) =>
@@ -187,20 +190,21 @@ export default {
       );
 
       // returns exact match, if not found, then department filtered list
-      const find = filtered.find(
+      const find = filtered.filter(      
+        //course:
+        //full_title: names of classes 
         (course) =>
-          (course.full_title &&
-<<<<<<< Updated upstream
+          (course.full_title &&  (
             course.full_title.toUpperCase().includes(this.textSearch.toUpperCase())) ||
-            course.title.toUpperCase().includes(this.textSearch.toUpperCase())
+            course.title.toUpperCase().includes(this.textSearch.toUpperCase()) ||
+            course.department.toUpperCase().includes(this.textSearch.toUpperCase())
+          )
+          
       );
-=======
-            course.full_title.toUpperCase() ===
-              this.textSearch.toUpperCase()) ||
-          course.title.toUpperCase() === this.textSearch.toUpperCase());
->>>>>>> Stashed changes
 
-      if (find) return [find];
+      console.log(find);
+
+      if (find) return find;
       else return filtered;
     },
   },
