b334e28e1b296dfcbe4c2bbfa83daf11e875a018
Arnav Pawar <arnav.ap.pawar@gmail.com>
2022-11-18 17:22:12 -0500

    added 2 options for GPA calc
    updated and created 2 sections one for gpa for semester and one for total gpa from semester

--- src/web/src/pages/GPACalculator.vue
+++ src/web/src/pages/GPACalculator.vue
@@ -1,5 +1,46 @@
 <template>
-  <p>hi welcome to degree templates</p>
+  <b-tabs card class="h-100 d-flex flex-column flex-grow-1">
+            <b-tab class="flex-grow-1" data-cy="course-search-tab">
+              <template v-slot:title>
+                <div class="text-center" data-cy="course-search-tab-header">   
+                  GPA calculator for semester               
+                </div>
+              </template>
+              <b-card-text class="w-100 d-flex flex-grow-1 flex-column">
+
+                <label for="usr">Name:</label>
+                <input type="text" class="form-control">
+
+
+                <input v-model="classes" placeholder="Number of classes this semester">
+                <p>Number of Classes: {{ classes }}</p>
+
+                
+
+
+
+
+              </b-card-text>
+            </b-tab>
+
+            <b-tab class="flex-grow-1" data-cy="selected-courses-tab">
+              <template v-slot:title>
+                <div class="text-center" data-cy="selected-courses-tab-header">
+                  GPA calculator for total time  
+                </div>
+              </template>
+              <b-card-text class="w-100 d-flex flex-grow-1 flex-column">
+
+
+
+                <p>Enter GPA and number of Credits</p>
+
+
+
+
+              </b-card-text>
+            </b-tab>
+   </b-tabs>
 </template>
   
 <script>
@@ -19,17 +60,18 @@ export default {
     };
   },
   computed: {
-    getAverageForSem(creditsPerclass,gradeList) {
+    //getAverageForSem(creditsPerclass,gradeList) {
         //list of all credits in order of what they imputed
         //list of all grades in in order of inputed
         //will list gpa for the given semester
-    },
-    getTotalAvg(creditHours, GPA, creditsPerclass, gradeList){
+        
+    //},
+    //getTotalAvg(creditHours, GPA, creditsPerclass, gradeList){
         //number of credit hours
         //GPA from thoes credit hours
         //current semeser grade and creditsper class
         //will return what new gpa will be and how its affected
-    }
+    //}
   }
 };
 </script>
