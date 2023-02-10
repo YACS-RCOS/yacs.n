ecf3c3b81259eb15b985e1d5b279f7f8c4683b8e
dorian451 <112524240+dorian451@users.noreply.github.com>
2023-02-10 16:43:53 -0500

    fix navbar only being highlighted on top-level pages

--- src/web/src/components/Header.vue
+++ src/web/src/components/Header.vue
@@ -38,7 +38,7 @@
     </b-navbar-toggle>
     <b-collapse id="header-navbar-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'CourseScheduler' }">
+        <b-nav-item :to="{ name: 'CourseScheduler' }" class="first">
           <font-awesome-icon icon="calendar" />
           Schedule
         </b-nav-item>
@@ -217,7 +217,13 @@ export default {
   }
 }
 //highlight current page in the navbar using class built into vue router
-#header .nav-item .router-link-exact-active{
+.nav-item:not(.first) .router-link-active{
+  border-radius: 5px;
+  padding: calc(8px - 0.2em);
+  border: 0.2em solid var(--dark-blue-secondary);
+}
+
+.nav-item.first .router-link-exact-active{
   border-radius: 5px;
   padding: calc(8px - 0.2em);
   border: 0.2em solid var(--dark-blue-secondary);
