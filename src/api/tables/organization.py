bcca79a77afeb0f49c39e429002c7e327dbf9818
Charles Chae <charleschae12@gmail.com>
2023-02-10 16:25:33 -0500

    organization database
    Finished the organization database.
    org_id is the unique number we give to each of the organization.

---
+++ src/api/tables/organization.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, PrimaryKeyConstraint
+from sqlalchemy.dialects.postgresql import INTEGER, VARCHAR, TIME
+
+from .database import Base
+
+class ClubInfo(Base):
+    __tablename__ = "Organization Info"
+
+    org_id = Column(VARCHAR(length=7))
+    name = Column(VARCHAR(length=255))
+    location = Column(VARCHAR(length=255))
+    size = Column(VARCHAR(length=255))
+    operation = Column(VARCHAR(length=255))
+    day_of_week = Column(INTEGER)
+    meeting_time = Column(VARCHAR(length=255))
+    contact = Column(VARCHAR(length=255))
+    leader = Column(VARCHAR(length=255))
+    focus_type = Column(VARCHAR(length=255))
+    organization_type = Column(VARCHAR(length=255))
+    requirement = Column(VARCHAR(length=255))
+    discription = Column(VARCHAR(length=255))  
+
+    __table_args__ = (
+        PrimaryKeyConstraint('org_id'),
+    )
