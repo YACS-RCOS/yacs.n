5749bd1ce5b7c0103334434b481f7a1a2bd2f93e
kimdan002 <kimdan7108@gmail.com>
2023-02-17 17:36:59 -0500

    Minor requirements

---
+++ rpi_data/Degree_Tamplates/minorscrap.py
@@ -0,0 +1,25 @@
+import logging
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrapFromURL(webLink):
+    URL = webLink
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    outFile = open("minorData.txt", "a")
+    outFile.truncate(0)
+
+    title_element = soup.find("h1", id="acalog-content")
+    outFile.write(minorelement.text)
+    outFile.write(":\n")
+
+    pathWayName = soup.find_all(id="acalog-content")
+
+    info = soup.find_all(class_="acalog-core")
+    for course in info:
+        outFile.write(minorcourse.text)
+    outFile.close()
+    cleaning1()
