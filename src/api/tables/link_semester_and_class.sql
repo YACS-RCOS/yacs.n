4b9bc995714eee915fa846d0c8abd1d8876b906d
jonha1 <jonnyha888@gmail.com>
2023-02-19 23:36:38 -0500

    draft of table linking semester and class
    draft of table that links each semester and degreeClass and uses them as foreign keys with comments
    - needs to be edited with variables names and uses of keywords

---
+++ src/api/tables/link_semester_and_class.sql
@@ -0,0 +1,13 @@
+CREATE TABLE semester_degree_class (
+  semester_id INT NOT NULL, /*The semester_id column is a foreign key column that references the id column in the semesters table.*/
+  degree_class_id INT NOT NULL,
+  PRIMARY KEY (semester_id, degree_class_id),
+  FOREIGN KEY (semester_id) REFERENCES semesters(id),
+  FOREIGN KEY (degree_class_id) REFERENCES degree_classes(id)
+);
+/*https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-create-table/
+#set the primary key to be a combination of semester_id and degree_class_id, 
+which ensures that each combination of semester and degree class is unique.
+# you can now link each semester to its corresponding degree class, allowing you
+ to easily retrieve information about which degree classes were offered during a
+  specific semester or which semesters a particular degree class was available.
