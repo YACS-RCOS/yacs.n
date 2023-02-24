2afda7695925c128242667f960d58eea341edf2f
kimdan002 <kimdan7108@gmail.com>
2023-02-24 17:34:18 -0500

    Update minorscrap.py

--- rpi_data/Degree_Tamplates/minorscrap.py
+++ rpi_data/Degree_Tamplates/minorscrap.py
@@ -1,24 +1,30 @@
-import logging
-from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 
+outFile = open("MinorData.txt", "a") #append mode
+outFile.truncate(0) #resizes the outfile to have 0 bytes effectively emptying it
 
-def scrapFromURL(webLink):
+def scrapFromURL(webLink, minor_db):
+    
     URL = webLink
-    page = requests.get(URL)
+    page = requests.get(URL) 
 
-    soup = BeautifulSoup(page.content, "html.parser")
-    outFile = open("minorData.txt", "a")
-    outFile.truncate(0)
-
+    #gather the html tree as the soup object
+    soup = BeautifulSoup(page.content, "html.parser") 
+    #print(soup)
+    
+    #find the first h1 which is the major name
     title_element = soup.find("h1", id="acalog-content")
     outFile.write(minor_element.text)
+    minor = minor_element.text
     outFile.write(":\n")
-
-    pathWayName = soup.find_all(id="acalog-content")
-
-    info = soup.find_all(class_="acalog-core")
-    for course in info:
-        outFile.write(minorcourse.text)
-    outFile.close()
+    startScrap = False
+    #the entire class template has a custom leftpad of 20 consistently, so gather that data
+    clp20 = soup.find_all(class_ = "custom_leftpad_20")
+    cur_entry = ("","")
+    startScrap = False #set to true if we have reached the "Program requirements" information
+    for items in clp20:
+        state = 'newMajor'
+        for div in items.find_all("div", recursive = False):
+            if (div.text == "Program Requirements" ):
+                startScrap = True
