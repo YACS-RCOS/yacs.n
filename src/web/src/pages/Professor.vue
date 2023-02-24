6f1c599741aa3c769367d72597db10b08a1c8f9c
Charles Chae <charleschae12@gmail.com>
2023-02-24 17:38:07 -0500

    Update Professor.vue

--- src/web/src/pages/Professor.vue
+++ src/web/src/pages/Professor.vue
@@ -1,50 +1,103 @@
 <template>
-  <div class="search-bar">
-    <input type="text" v-model="searchQuery" placeholder="Search...">
-    <button @click="search">Search</button>
-  </div>
+  <b-container fluid>
+    <b-breadcrumb :items="breadcrumbNav"></b-breadcrumb>
+
+    <!-- button to switch between alphabet order and category order -->
+    <div style="float: left;" class="w-10">
+      <b-button
+        @click="listAlphabet()"
+        style="
+          margin-top: 10px;
+          color: #007bff;
+          border: solid #007bff;
+          background-color: transparent;
+        "
+      >
+        List by Alphabet
+      </b-button>
+      <br />
+      <b-button
+        @click="listCate()"
+        style="
+          margin-top: 10px;
+          color: #007bff;
+          border: solid #007bff;
+          background-color: transparent;
+        "
+      >
+        List by Department
+      </b-button>
+
+    </div>
+    <CenterSpinner
+      v-else
+      :height="80"
+      :fontSize="1.3"
+      loadingMessage="Pathways"
+      :topSpacing="30"
+    />
+  </b-container>
 </template>
 
 <script>
+import CenterSpinnerComponent from "../components/CenterSpinner";
+
 export default {
+  name: "Pathway",
+  components: {
+    CenterSpinner: CenterSpinnerComponent,
+  },
   data() {
     return {
-      searchQuery: '',
+      breadcrumbNav: [
+        {
+          text: "YACS",
+          to: "/",
+        },
+        {
+          text: "Pathways",
+        },
+      ],
     };
-  },
-  methods: {
-    search() {
-      // Perform search operation based on searchQuery
-      console.log(`Searching for ${this.searchQuery}`);
-    },
   },
 };
 </script>
 
-<style scoped>
-.search-bar {
-  display: flex;
-  align-items: center;
+<style>
+.gridContainer {
+  display: inline-grid;
+  grid-template-columns: auto auto;
   justify-content: center;
-  padding: 10px;
-  background-color: #f1f1f1;
+  align-content: center;
 }
 
-input[type="text"] {
-  padding: 5px;
-  margin-right: 5px;
-  border: none;
-  border-radius: 3px;
-  font-size: 16px;
+.categoryBox {
+  text-align: center;
 }
 
-button {
-  padding: 5px 10px;
-  border: none;
-  border-radius: 3px;
-  background-color: #4CAF50;
-  color: white;
-  font-size: 16px;
+.category-title {
+  color: hsl(211, 100%, 60%);
+  background: rgba(108, 90, 90, 0.15);
+  border-bottom: rgba(108, 90, 90, 0.1), solid, 1px;
+}
+
+.pathway-button {
+  display: inline-block;
+  background: white;
+  border-style: none;
+  text-align: justify;
+  width: 95%;
+}
+
+.pathway-button:hover {
+  background: rgba(108, 90, 90, 0.15) !important;
+}
+
+.courseInPath {
   cursor: pointer;
+}
+
+.courseInPath:hover {
+  background-color: rgba(39, 130, 230, 0.5);
 }
 </style>
