82650ac843e8669bc82b30e9a4521f09b439ed92
Charles Chae <charleschae12@gmail.com>
2023-02-24 17:07:12 -0500

    Update Professor.vue

--- src/web/src/pages/Professor.vue
+++ src/web/src/pages/Professor.vue
@@ -1,29 +1,50 @@
 <template>
-  <b-container fluid>
-    <b-breadcrumb :items="breadcrumbNav"></b-breadcrumb>
+  <div class="search-bar">
+    <input type="text" v-model="searchQuery" placeholder="Search...">
+    <button @click="search">Search</button>
+  </div>
+</template>
 
-    <!-- button to switch between alphabet order and category order -->
-    <div style="float: left;" class="w-10">
-      <b-button
-        @click="listAlphabet()"
-        style="
-          margin-top: 10px;
-          color: #007bff;
-          border: solid #007bff;
-          background-color: transparent;
-        "
-      >
-        List by Alphabet
-      </b-button>
-      <br />
-      <b-button
-        @click="listCate()"
-        style="
-          margin-top: 10px;
-          color: #007bff;
-          border: solid #007bff;
-          background-color: transparent;
-        "
-      >
-        List by Category
-      </b-button>
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
+  methods: {
+    search() {
+      // Perform search operation based on searchQuery
+      console.log(`Searching for ${this.searchQuery}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.search-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  background-color: #f1f1f1;
+}
+
+input[type="text"] {
+  padding: 5px;
+  margin-right: 5px;
+  border: none;
+  border-radius: 3px;
+  font-size: 16px;
+}
+
+button {
+  padding: 5px 10px;
+  border: none;
+  border-radius: 3px;
+  background-color: #4CAF50;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
