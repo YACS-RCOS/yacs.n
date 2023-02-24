020fdbcff487d932220cfb8589c74171e7e1de4f
JS <67136819+skyjune0824@users.noreply.github.com>
2023-02-24 17:45:39 -0500

    Update Sidebar.vue

---
+++ src/web/src/pages/Sidebar.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="sidebar">
+		<div class="sidebar-backdrop" @click="closeSidebarPanel" v-if="isPanelOpen">
+		<Transition name = "slide">
+			<div v-if = "isPanelOpen"
+				class = "sidebar-panel">
+				<slot></slot>
+			</div>
+		</Transition>
+	</div>
+</template>
+<script>
+	export default {
+		data: () => ({
+			isPanelOpen: true
+		}),
+		methods: {
+			closeSidebarPanel(){
+				this.isPanelOpen = false
+			}
+		}
+	}
+</script>
+<style>
+	.slide-enter-active,
+	.slide-enter-active
+	{
+		transition: transform 0.2s ease;
+	}
+
+	.slide-enter,
+	.slide-leave-to{
+		transform: translateX(-100%);
+		translation: all 0.15s ease-in 0s;
+	}
+
+	.sidebar-backdrop {
+        background-color: rgba(0,0,0,.5);
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        cursor: pointer;
+    }
+
+    .sidebar-panel {
+        overflow-y: auto;
+        background-color: white;
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        z-index: 999;
+        padding: 3rem 20px 2rem 20px;
+        width: 300px;
+    }
+</style>
