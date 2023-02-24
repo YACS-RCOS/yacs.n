f27888c79cb9b68d9aa812d1ac5ea121b58b0365
ajwadcs <ajwadislam@gmail.com>
2023-02-24 16:43:32 -0500

    Used tabs instead of collapsing buttons

--- src/web/src/components/CourseInfo.vue
+++ src/web/src/components/CourseInfo.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
-    <!-- Using modifiers -->
-    <b-button v-b-toggle.collapse-2 class="m-1">Toggle Collapse</b-button>
-
-    <!-- Using value -->
-    <b-button v-b-toggle="'collapse-2'" class="m-1">Toggle Collapse</b-button>
-
-    <!-- Element to collapse -->
-    <b-collapse id="collapse-2">
-        <b-card>I am collapsible content!</b-card>
-    </b-collapse>
-    </div>
+<div>
+  <b-tabs
+    active-nav-item-class="font-weight-bold text-uppercase text-danger"
+    active-tab-class="font-weight-bold text-success"
+    content-class="mt-3"
+    fill
+  >
+    <b-tab title="Example Coursework" active><p>Hello</p></b-tab>
+    <b-tab title="Syllabus"><p>I'm</p></b-tab>
+    <b-tab title="Custom Fields"><p>the</p></b-tab>
+    <b-tab title="Course FAQ"><p>file</p></b-tab>
+    <b-tab title="Sections"><p>yes</p></b-tab>
+  </b-tabs>
+</div>
 </template>
