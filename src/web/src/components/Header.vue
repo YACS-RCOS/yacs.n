664038b907b79ba67c3bf73dfbbc988695fab95c
I'm a Github Action <beepboop@github.com>
2023-03-02 15:24:01 +0000

    beep boop Github Action Master Branch CI Prettier hard at work

--- src/web/src/components/Header.vue
+++ src/web/src/components/Header.vue
@@ -217,13 +217,13 @@ export default {
   }
 }
 //highlight current page in the navbar using class built into vue router
-.nav-item:not(.first) .router-link-active{
+.nav-item:not(.first) .router-link-active {
   border-radius: 5px;
   padding: calc(8px - 0.2em);
   border: 0.2em solid var(--dark-blue-secondary);
 }
 
-.nav-item.first .router-link-exact-active{
+.nav-item.first .router-link-exact-active {
   border-radius: 5px;
   padding: calc(8px - 0.2em);
   border: 0.2em solid var(--dark-blue-secondary);
