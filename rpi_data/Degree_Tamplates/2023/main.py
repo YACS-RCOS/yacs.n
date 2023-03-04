fc99017296ebda7871231d3e1240f39bc41b3343
xingsixue123 <99207361+xingsixue123@users.noreply.github.com>
2023-03-04 02:55:24 -0500

    final bug fix of edge cases

--- rpi_data/Degree_Tamplates/2023/main.py
+++ rpi_data/Degree_Tamplates/2023/main.py
@@ -1,22 +1,22 @@
 from subprocess import call
-#call main parser, main scraper, main to_json function
+# call main parser, main scraper, main to_json function
 call(["python", "html_parser.py"])
 call(["python", "major_scrap.py"])
 call(["python", "to_json_2022.py"])
-#call edge cases functions
+# call edge cases functions
 call(["python", "ienv.py"])
 call(["python", "psysci.py"])
 call(["python", "chembio.py"])
 call(["python", "engcocu.py"])
-#main json
+# main json
 fout = open("jsons/majorData.json", "w", encoding='UTF-8')
 fmain = open("jsons/majorDataTemp.json", "r", encoding='UTF-8')
 lines = fmain.readlines()
 for line in lines:
     print(line)
     fout.write(line)
-#edge cases combination
-jsons = ["ienv","psysci","chembio","engcocu"]
+# edge cases added
+jsons = ["ienv","psysci","chembio","engcocu","gamesimu","mechengcu"]
 for i in range(len(jsons)):
     fjson = open("jsons/"+jsons[i]+".json", "r", encoding='UTF-8')
     lines = fjson.readlines()
--- rpi_data/Degree_Tamplates/readme.md
+++ rpi_data/Degree_Tamplates/readme.md
@@ -2,10 +2,6 @@ This folder contains the degree templates' scraper and json converter.
     Degree_Tamplates\2023\jsons : temp json and main json:  <majorData.json>
     Degree_Tamplates\2023\temp : temp txt files from scaper
 
-main.py calls minor functions and generates  Degree_Tamplates\2023\jsons\majorData.json
+main.py calls minor functions and generates:  Degree_Tamplates\2023\jsons\majorData.json
 
-3 glitches in majorData.json:
-line 378
-line 399
-line 502
-needed to be fixed
+
