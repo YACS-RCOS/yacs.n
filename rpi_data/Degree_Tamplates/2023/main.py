1e1f56116a5e8c1487afce7dea5edfd4c256f017
xingsixue123 <99207361+xingsixue123@users.noreply.github.com>
2023-03-05 02:41:09 -0500

    main.py edgecase function update

--- rpi_data/Degree_Tamplates/2023/main.py
+++ rpi_data/Degree_Tamplates/2023/main.py
@@ -1,13 +1,15 @@
 from subprocess import call
+
 # call main parser, main scraper, main to_json function
 call(["python", "html_parser.py"])
 call(["python", "major_scrap.py"])
 call(["python", "to_json_2022.py"])
+
 # call edge cases functions
-call(["python", "ienv.py"])
-call(["python", "psysci.py"])
-call(["python", "chembio.py"])
-call(["python", "engcocu.py"])
+jsons = ["ienv","psysci","chembio","engcocu","gamesimu","mechengcu"]
+for edgecase in jsons:
+    call(["python", edgecase+".py"])
+    
 # main json
 fout = open("jsons/majorData.json", "w", encoding='UTF-8')
 fmain = open("jsons/majorDataTemp.json", "r", encoding='UTF-8')
@@ -15,8 +17,8 @@ lines = fmain.readlines()
 for line in lines:
     print(line)
     fout.write(line)
-# edge cases added
-jsons = ["ienv","psysci","chembio","engcocu","gamesimu","mechengcu"]
+    
+# edge cases json added
 for i in range(len(jsons)):
     fjson = open("jsons/"+jsons[i]+".json", "r", encoding='UTF-8')
     lines = fjson.readlines()
