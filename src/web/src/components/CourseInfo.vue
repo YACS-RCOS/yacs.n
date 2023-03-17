ac11b2e719727241da8c78e842bd62758cc44326
ajwadcs <ajwadislam@gmail.com>
2023-03-17 16:37:49 -0400

    Update CourseInfo.vue
    Changed text and centered it

--- src/web/src/components/CourseInfo.vue
+++ src/web/src/components/CourseInfo.vue
@@ -1,13 +1,12 @@
 <template>
 <div>
   <b-tabs
-    active-nav-item-class="font-weight-bold text-uppercase text-danger"
-    active-tab-class="font-weight-bold text-success"
+    active-nav-item-class="font-weight-bold text-uppercase text-danger"  
     content-class="mt-3"
     fill
   >
-    <b-tab title="Syllabus"><p>Please contact the YACS team if you have the syllabus associated with this class</p></b-tab>
-    <b-tab title="Course FAQ"><p>TBD</p></b-tab>
+    <b-tab title="Syllabus"><p align="center">Please contact the YACS team if you have the syllabus associated with this class</p></b-tab>
+    <b-tab title="Course FAQ"><p align="center">Coming Soon</p></b-tab>
     <b-tab title="Sections">
       <p><CourseListing :course="course" :showAddButton = "false"/></p>
     </b-tab>
