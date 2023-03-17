3063b7583376f9c80014813187c577016e88f46a
jonha1 <jonnyha888@gmail.com>
2023-03-17 17:47:28 -0400

    FIxed functions for professor
    - gave %s values

--- src/api/db/professor.py
+++ src/api/db/professor.py
@@ -7,42 +7,48 @@ class Professor:
         sql = 	"""
                     INSERT INTO
                         professor (first_name, last_name, phone_numbber, email,
-                        department, office_room, classes, office_hours_time)
+                        department, office_room, classes, office_hours_time, rcsid)
                     VALUES
-                        (%s, %s, %s, %s, %s, %s, %s, %s)
+                        (%(First_name)s, %(Last_name)s, %(Phone_number)s, %(Email)s, %(Dep)s, %(Office_room)s, %(Classes)s, %(Office_time)s)
                     ON CONFLICT DO NOTHING;
                     """
-        resp, error = self.db_conn.execute(sql, [first_name, last_name, phone, #find out what resp does
+        {
+            "First_name": first_name,
+            "Last_name": last_name,
+            "Phone_number": phone,
+            "Email": email,
+            "Dep": dep,
+            "Office_room": office,
+            "Classes": classes, 
+            "Office_time": office_time
+        }, 
+        resp, error = self.db_conn.execute(sql, [first_name, last_name, phone, 
         email, dep, office, classes, office_time], False)
         return (True, None) if not error else (False, error)
 
-    def remove_professor(self, first_name, last_name, phone, email, dep, office, 
-        classes, office_time):
+    def remove_professor_by_email(self, email):
         if email is None:
             sql =   """
                     DELETE FROM 
                         professor
                     WHERE
-                        email = %s 
+                        email = %(Email)s 
                     """
-            error = self.db_conn.execute(sql, [first_name, last_name, phone, 
-            email, dep, office, classes, office_time], False)
+            {
+                "Email": email
+            }
+            error = self.db_conn.execute(sql, [email], False)
         else:
             sql =   """
                     DELETE FROM 
                         professor
                     WHERE
-                        first_name = %s AND 
-                        last_name = %s AND 
-                        phone_number = %s AND 
-                        email = %s AND
-                        department = %s AND
-                        office_room = %s AND
-                        classes = %s AND 
-                        office_hours_time = %s
+                        email = %(Email)s
                     """
-            error = self.db_conn.execute(sql, [first_name, last_name, phone, 
-            email, dep, office, classes, office_time], False)
+            {
+                "Email": email
+            }
+            error = self.db_conn.execute(sql, [email], False)
         return (True, None) if not error else (False, error)
 
     def get_professor_info_by_email(self,email):
@@ -53,12 +59,13 @@ class Professor:
                         from
                             professor
                         where
-                            email = %s
+                            email = %(Email)s
                     """
-        #add the vriables in semester_info
+            {
+                "Email": email
+            }
         info, error = self.db_conn.execute(sql, [email], True)
         return (info, None) if not error else (False, error)
-
 
     def get_professor_name_by_email(self,email):
         if email is None:
@@ -69,16 +76,29 @@ class Professor:
                         from
                             professor
                         where
-                            email = %s
+                            email = %(Email)s
                     """
+            {
+                "Email": email
+            }
         name, error = self.db_conn.execute(sql, [email], True)
         return (name, None) if not error else (False, error)
-        
     
     def get_professor_info_by_rcs(self,rcs):
         return self.get_professor_name_by_email(self, rcs+"rpi.edu")
 
-    #gets classes, name(both first and last --> return json file), phone_nu,.... office horus time
-    # return all professrs in a dtabase (json)
-    # return all the parofesors in a certain department (json)
+
+    def get_professors_by_department(self): #return as a json
+        return
+
+    def get_all_professors(self):  #return as a json
+        return
+
     # return all professrs that teach a certain class throughout curretn sesmter (json)
+    def get_professors_classes_by_semester(self):  #return as a json
+        return
+
+    def get_professor_phone_number(self):
+        return
+    #gets classes, name(both first and last --> return json file), phone_nu,.... office horus time
+
