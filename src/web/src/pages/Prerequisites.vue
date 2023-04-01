7f73638002252bc07ba7b9cde0a9d77f6232de94
YCK03 <55895494+YCK03@users.noreply.github.com>
2023-04-01 11:57:07 -0400

    Update Prerequisites.vue
    Restructuring prerequisites to have the same department list as the course scheduler.

--- src/web/src/pages/Prerequisites.vue
+++ src/web/src/pages/Prerequisites.vue
@@ -1,26 +1,53 @@
+<!--
+  look at newCourse Scheduler
+-->
+
+
+
+
+
+
+
+
+
+
+
+<template>
+  <p class = "Prerequisites"> Prerequisites {{ Prerequisites }}</p>
+  <b-tab
+    title = "Department"
+    active
+  >
+  <CourseList
+                  v-if="!loading"
+                  @addCourse="addCourse"
+                  @removeCourse="removeCourse"
+                  @showCourseInfo="showCourseInfo"
+                  class="w-100"
+                />
+</template>
+
 <script>
+import CourseListComponent from "@/components/CourseList";
 export default {
     name:"Prerequisites",
+    components : {
+      CourseList: CourseListComponent,
+    },
     data() {
     return {
       Prerequisites: 'Select your classes from the menu below'
-      
     }
   }
 };
 </script>
 
-<template>
-    <p class = "Prerequisites">{{ Prerequisites }}</p>
-    
-</template>
-
 <style>
 .Prerequisites{
     color: blue;
-  font-weight: bold;
+  
 }
 
-</style>
+</style> 
 
-import CourseListComponent from "@/components/CourseList";
+
