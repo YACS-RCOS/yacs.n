08b4cbc50bc0ff9921ec9cdf7ba4103524c87fe0
YCK03 <55895494+YCK03@users.noreply.github.com>
2023-04-07 16:12:50 -0400

    Update to prerequisites page

--- src/web/src/pages/Prerequisites.vue
+++ src/web/src/pages/Prerequisites.vue
@@ -1,17 +1,54 @@
 <!--
   look at newCourse Scheduler
 -->
+<template>
+  <b-container fluid class="py-3 h-100 main-body">
+    <b-row class="h-100">
+      <b-col md="4" class="d-flex flex-column">
+        <b-tabs card class="h-100 d-flex flex-column flex-grow-1">
+          <b-tab
+            title = Prerequisites
+            active
+            class="flex-grow-1 w-100"
+            data-cy="course-search-tab"
+          >
+
+            <b-card-text class="d-flex flex-grow-1 w-100">
+              <CenterSpinner
+                v-if="loading"
+                class="d-flex flex-grow-1 flex-column w-100 justify-content-center align-items-center"
+                :height="60"
+                :fontSize="1"
+                loadingMessage="Prerequisites"
+                :topSpacing="0"
+              />
+
+                <CourseList
+                  v-if="!loading"
+                  @addCourse="addCourse"
+                  @removeCourse="removeCourse"
+                  @showCourseInfo="showCourseInfo"
+                  class="w-100"
+                />
+            </b-card-text>
+        </b-tab>
+        </b-tabs>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+
+<script>
+
+</script>
 
 
 
 
 
 
-
-
-
-
-
+<!-- 
 <template>
   <p class = "Prerequisites"> Prerequisites {{ Prerequisites }}</p>
   <b-tab
@@ -50,4 +87,4 @@ export default {
 
 </style> 
 
-
+ -->
