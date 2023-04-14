918c6c8b40eb0fef5b43a2c53cb5825a73d57a64
Yixuan Shen <sheny22@rpi.edu>
2023-04-14 17:10:46 -0400

    Update course data for 2023, also used chmod 755 ./*, which is why all file seems to be changed

--- rpi_data/get-summer-2023.sh
+++ rpi_data/get-summer-2023.sh
@@ -5,3 +5,5 @@ SOURCE_URL=https://sis.rpi.edu/reg/zs20230502.htm DEST=p1.csv HEADERS=False pyth
 SOURCE_URL=https://sis.rpi.edu/reg/zs20230503.htm DEST=p2.csv HEADERS=False python ./modules/rpi-parse.py
 cat p* > summer-2023.csv
 rm -rf p*.csv
+
+./update-summer-2023.sh
