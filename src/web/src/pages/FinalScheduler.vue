269524a8df4df8870471b061e889cdc44451770c
xiaxy803 <xiaxy13577010167@outlook.com>
2023-04-21 17:30:39 -0400

    Change the input column reading department to selection department
    I change the input reading form on department column to the selection, so user can select it easier, also eliminate the wrong possible input
    
    Co-Authored-By: xingsixue123 <99207361+xingsixue123@users.noreply.github.com>

--- src/web/src/pages/FinalScheduler.vue
+++ src/web/src/pages/FinalScheduler.vue
@@ -63,19 +63,55 @@
                 <section class="d-flex flex-row justify-content-center">
                   <div class="d-flex flex-column justify-content-center align-items-center w-30">
                     <p class="text-center">Dept</p>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
-                    <hr>
-                    <input type="text" class="w-100 me-3">
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
+                    <b-form-group for="department">
+                      <b-form-select
+                        v-model="selectedDepartment"
+                        :options="departmentOptions"
+                        class="h-120"
+                      ></b-form-select>
+                    </b-form-group>
                   </div>
                   <div class="d-flex flex-column justify-content-center align-items-center w-30">
                     <p class="text-center">Number</p>
