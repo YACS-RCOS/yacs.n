5e2798229cd96841b526265fc7ca0b494a78fe48
ajwadcs <ajwadislam@gmail.com>
2023-04-21 16:25:37 -0400

    Rearranged tabs
    put the sections tab first

--- src/web/src/components/CourseInfo.vue
+++ src/web/src/components/CourseInfo.vue
@@ -5,11 +5,11 @@
     content-class="mt-3"
     fill
   >
-    <b-tab title="Syllabus"><p align="center">Please contact the YACS team if you have the syllabus associated with this class</p></b-tab>
-    <b-tab title="Course FAQ"><p align="center">Coming Soon</p></b-tab>
     <b-tab title="Sections">
       <p><CourseListing :course="course" :showAddButton = "false"/></p>
     </b-tab>
+    <b-tab title="Syllabus"><p align="center">Please contact the YACS team if you have the syllabus associated with this class</p></b-tab>
+    <b-tab title="Course FAQ"><p align="center">Coming Soon</p></b-tab>
   </b-tabs>
 </div>
 </template>
