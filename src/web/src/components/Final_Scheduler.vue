dfe67daea954997bfbbfd706ff20f0b0f7517c94
xingsixue123 <99207361+xingsixue123@users.noreply.github.com>
2023-04-26 22:25:07 -0400

    small modification

--- src/web/src/components/Final_Scheduler.vue
+++ src/web/src/components/Final_Scheduler.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="calendar-container">
+  <div class="calendar">
     <table>
       <thead>
         <tr>
-          <th v-for="day in daysOfWeek">{{ day }}</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(week, index) in weeks" :key="index">
-          <td v-for="(day, index) in week" :key="index" :class="{ event: isEvent(day), [getEventClass(day)]: isEvent(day) }">{{ day }}</td>
+        <tr v-for="week in weeks" :key="week">
+          <td v-for="day in week" :key="day.date" :class="{'prev-month': day.prevMonth, 'next-month': day.nextMonth, 'today': day.today}">
+            {{ day.date.getDate() }}
+            <div v-for="event in eventsOnDay(day.date)" :key="event.title" class="event">{{ event.title }}</div>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -17,81 +26,159 @@
 
 <script>
 export default {
-data() {
-  return {
-  daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  weeks: [],
-  events: [
-      { date: 1, title: 'Intro to Computer Science' },
-      { date: 2, title: 'Data Structure' },
-      { date: 3, title: 'Foundation to Computer Science' },
-      { date: 4, title: 'Computer Organization' },
-      { date: 5, title: 'Intro to Algorithm' }
-  ]
-  }
-},
-mounted() {
-  this.buildCalendar()
-},
-methods: {
-  buildCalendar() {
-  const date = new Date(2023, 4, 1)
-  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
-  const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-  const daysInMonth = lastDayOfMonth.getDate()
-  const firstDayOfWeek = firstDayOfMonth.getDay()
-  const weeks = []
-
-  let week = []
-  for (let i = 0; i < firstDayOfWeek; i++) {
-      week.push('')
-  }
-
-  for (let i = 1; i <= daysInMonth; i++) {
-      week.push(i)
-      if (week.length === 7) {
-      weeks.push(week)
-      week = []
-      }
-  }
-
-  if (week.length > 0) {
-      for (let i = week.length; i < 7; i++) {
-      week.push('')
-      }
-      weeks.push(week)
-  }
-
-  this.weeks = weeks
+  data() {
+    return {
+      daysInWeek: 7,
+      weeks: [],
+      events: [
+        {
+          date: new Date(2023, 4, 3),
+          title: "Math exam"
+        },
+        {
+          date: new Date(2023, 4, 8),
+          title: "History essay due"
+        },
+        {
+          date: new Date(2023, 4, 10),
+          title: "Science lab report due"
+        },
+        {
+          date: new Date(2023, 4, 16),
+          title: "English presentation"
+        },
+        {
+          date: new Date(2023, 4, 20),
+          title: "Art project due"
+        },
+        {
+          date: new Date(2023, 4, 25),
+          title: "Physical education final exam"
+        },
+        {
+          date: new Date(2023, 4, 28),
+          title: "Music recital"
+        }
+      ]
+    };
   },
-  isEvent(day) {
-  return this.events.some(event => event.date === day)
+  mounted() {
+    this.generateCalendar();
   },
-  getEventClass(day) {
-  const event = this.events.find(event => event.date === day)
-  return event ? event.title.toLowerCase().replace(' ', '-') : ''
+  methods: {
+    generateCalendar() {
+      const date = new Date();
+      date.setFullYear(2023); // Set the year to 2023
+      date.setMonth(4); // Set the month to May
+      date.setDate(1); // Set the date to the 1st of the month
+
+      const month = date.getMonth();
+      const year = date.getFullYear();
+
+      let dayOfWeek = date.getDay(); // Get the day of the week of the 1st
+
+      // Add days from the previous month to fill the first week
+      while (dayOfWeek > 0) {
+        this.weeks.push({
+          date: new Date(year, month, -dayOfWeek + 1),
+          prevMonth: true
+        });
+        dayOfWeek--;
+      }
+
+      // Add days for the current month
+      while (date.getMonth() === month) {
+        this.weeks.push({
+          date: new Date(date),
+          today: this.isToday(date)
+        });
+        date.setDate(date.getDate() + 1);
+        dayOfWeek++;
+
+        if (dayOfWeek === this.daysInWeek) {
+          // Start a new week
+          dayOfWeek = 0;
+        }
+      }
+
+      // Add days from the next month to fill the last week
+      while (dayOfWeek > 0 && dayOfWeek < this.daysInWeek) {
+        this.weeks.push({
+          date: new Date(year, month + 1, this.daysInWeek - dayOfWeek + 1),
+          nextMonth: true
+        });
+          dayOfWeek++;
+        }
+        },
+          eventsOnDay(date) {
+          return this.events.filter(event => {
+          return event.date.getDate() === date.getDate() &&
+          event.date.getMonth() === date.getMonth() &&
+          event.date.getFullYear() === date.getFullYear();
+          });
+          },
+          isToday(date) {
+          const today = new Date();
+        return (
+          date.getDate() === today.getDate() &&
+          date.getMonth() === today.getMonth() &&
+          date.getFullYear() === today.getFullYear()
+      );
+    }
   }
-}
-}
+};
 </script>
 
 <style>
-.calendar-container {
-float: right;
-width: 70%;
+.calendar {
+  font-family: sans-serif;
+  border-collapse: collapse;
 }
 
-table {
-border-collapse: collapse;
+.calendar td {
+  border: 1px solid #ddd;
+  padding: 5px;
+  text-align: center;
+  height: 80px;
+  position: relative;
 }
 
-th, td {
-border: 1px solid black;
-padding: 5px;
+.calendar td.prev-month:before {
+  content: attr(data-date);
+  display: block;
+  color: #bbb;
+  font-size: 12px;
+  position: absolute;
+  top: 5px;
+  left: 5px;
 }
 
-.event {
-background-color: yellow;
+.calendar td.next-month:before {
+  content: attr(data-date);
+  display: block;
+  color: #bbb;
+  font-size: 12px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
 }
 
+.calendar td.today {
+  background-color: #f2f2f2;
+}
+
+.calendar .event {
+  background-color: #4CAF50;
+  color: white;
+  font-size: 12px;
+  padding: 2px;
+  border-radius: 3px;
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+  right: 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 </style>
