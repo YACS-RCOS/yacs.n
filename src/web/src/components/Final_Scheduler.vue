664cfae3e9aa1ad7fa3b0f723e71535610183acd
xingsixue123 <99207361+xingsixue123@users.noreply.github.com>
2023-04-26 20:07:33 -0400

    final scheduler revise

--- src/web/src/components/Final_Scheduler.vue
+++ src/web/src/components/Final_Scheduler.vue
@@ -1,80 +1,80 @@
 <template>
-    <div class="calendar-container">
-      <table>
-        <thead>
-          <tr>
-            <th v-for="day in daysOfWeek">{{ day }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(week, index) in weeks" :key="index">
-            <td v-for="(day, index) in week" :key="index" :class="{ event: isEvent(day), [getEventClass(day)]: isEvent(day) }">{{ day }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
+  <div class="calendar-container">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="day in daysOfWeek">{{ day }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(week, index) in weeks" :key="index">
+          <td v-for="(day, index) in week" :key="index" :class="{ event: isEvent(day), [getEventClass(day)]: isEvent(day) }">{{ day }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
 <script>
 export default {
 data() {
-    return {
-    daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    weeks: [],
-    events: [
-        { date: 1, title: 'Intro to Computer Science' },
-        { date: 2, title: 'Data Structure' },
-        { date: 3, title: 'Foundation to Computer Science' },
-        { date: 4, title: 'Computer Organization' },
-        { date: 5, title: 'Intro to Algorithm' }
-    ]
-    }
+  return {
+  daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  weeks: [],
+  events: [
+      { date: 1, title: 'Intro to Computer Science' },
+      { date: 2, title: 'Data Structure' },
+      { date: 3, title: 'Foundation to Computer Science' },
+      { date: 4, title: 'Computer Organization' },
+      { date: 5, title: 'Intro to Algorithm' }
+  ]
+  }
 },
 mounted() {
-    this.buildCalendar()
+  this.buildCalendar()
 },
 methods: {
-    buildCalendar() {
-    const date = new Date(2023, 4, 1)
-    const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
-    const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-    const daysInMonth = lastDayOfMonth.getDate()
-    const firstDayOfWeek = firstDayOfMonth.getDay()
-    const weeks = []
+  buildCalendar() {
+  const date = new Date(2023, 4, 1)
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
+  const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+  const daysInMonth = lastDayOfMonth.getDate()
+  const firstDayOfWeek = firstDayOfMonth.getDay()
+  const weeks = []
 
-    let week = []
-    for (let i = 0; i < firstDayOfWeek; i++) {
-        week.push('')
-    }
+  let week = []
+  for (let i = 0; i < firstDayOfWeek; i++) {
+      week.push('')
+  }
 
-    for (let i = 1; i <= daysInMonth; i++) {
-        week.push(i)
-        if (week.length === 7) {
-        weeks.push(week)
-        week = []
-        }
-    }
+  for (let i = 1; i <= daysInMonth; i++) {
+      week.push(i)
+      if (week.length === 7) {
+      weeks.push(week)
+      week = []
+      }
+  }
 
-    if (week.length > 0) {
-        for (let i = week.length; i < 7; i++) {
-        week.push('')
-        }
-        weeks.push(week)
-    }
+  if (week.length > 0) {
+      for (let i = week.length; i < 7; i++) {
+      week.push('')
+      }
+      weeks.push(week)
+  }
 
-    this.weeks = weeks
-    },
-    isEvent(day) {
-    return this.events.some(event => event.date === day)
-    },
-    getEventClass(day) {
-    const event = this.events.find(event => event.date === day)
-    return event ? event.title.toLowerCase().replace(' ', '-') : ''
-    }
+  this.weeks = weeks
+  },
+  isEvent(day) {
+  return this.events.some(event => event.date === day)
+  },
+  getEventClass(day) {
+  const event = this.events.find(event => event.date === day)
+  return event ? event.title.toLowerCase().replace(' ', '-') : ''
+  }
 }
 }
 </script>
-  
+
 <style>
 .calendar-container {
 float: right;
@@ -95,4 +95,3 @@ background-color: yellow;
 }
 
 </style>
-  
