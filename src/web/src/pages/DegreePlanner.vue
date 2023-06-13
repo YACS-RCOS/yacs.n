bdd41c00d970d0676c7a26bc3ef0aeb48c6c2a06
EggyRepublic <Eggyrepublic@gmail.com>
2023-06-13 14:08:59 -0400

    beautified display

--- src/web/src/pages/DegreePlanner.vue
+++ src/web/src/pages/DegreePlanner.vue
@@ -1,19 +1,28 @@
 <template>
     <b-container fluid>
-        <h1>Degree Planner</h1>
+        <div class="heading">
+            <h1>Degree Planner</h1>
+            <h2>Requirements</h2>
+        </div>
         <div class="container">
-            <div class="column">
-                <h1>Requirements</h1>
-                <div class="text-block" v-for="(item, index) in requirements" :key="index">
-                    <h3>{{ item.name }}</h3>
-                    <h6>specifications: {{ item.specifications }}</h6>
-                    <h6>{{ item.actual_count }} / {{ item.required_count }}</h6>
-                    <li v-for="course in item.fulfillment_set" :key="course">
-                        {{ course }}
-                    </li>
-                    <br>
+            <div class="text-block" v-for="(item, index) in requirements" :key="index">
+                <h3>{{ item.name }}</h3>
+                <h6>specifications: {{ item.specifications }}</h6>
+                <div v-bind:class="{fulfillment:item.actual_count >= item.required_count, unfulfilled_fulfillment:item.actual_count < item.required_count}">
+                    <h4>Fulfillment:</h4>
+                    <div v-bind:class="{req_fulfilled:item.actual_count >= item.required_count, req_unfulfilled:item.actual_count < item.required_count}">
+                        <h5>{{ item.actual_count }} / {{ item.required_count }}</h5>
+                    </div>
+                    <div class="fulfilled-list">
+                        <li v-for="course in item.fulfillment_set" :key="course">
+                            {{ course }}
+                        </li>
+                    </div>
+                </div>
+                <br>
+                <div class="recommendations" v-if="recommendations[item.name].length > 0 && recommendations[item.name][0].fulfillment_set.length > 0">
                     <h4>Recommendations:</h4>
-                    <div class="recommendations" v-for="recommendation in recommendations[item.name]" :key="recommendation">
+                    <div class="recommendation-list" v-for="recommendation in recommendations[item.name]" :key="recommendation">
                         <h6>specifications: {{ recommendation.specifications }}</h6>
                         <li v-for="course in recommendation.fulfillment_set" :key="course">
                             {{ course }}
@@ -57,35 +66,67 @@
   
 <style scoped>
   .container {
-    display: flex;
-    justify-content: space-around;
-    padding: 30px;
-    justify-content: center;
-    align-items: flex-start;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
   }
   .column {
     flex: 0;
-    margin: 30px;
+    margin: 20px;
   }
   .text-block {
-    border: 10px solid #43494f;
-    border-radius: 20px;
-    padding: 20px;
-    margin: 12px;
-    min-width:400px;
+    border: 8px solid #43494f;
+    border-radius: 16px;
+    padding: 15px;
+    margin: 10px;
+    min-width:200px;
     width: 50vw;
-    max-width: 800px;
-    min-height: 100px;
+    max-width: 500px;
+    min-height: 80px;
     align-items: center;
     font-size: 1em;
+    background-color: #21242b;
   }
   .text-block h3 {
     color:cornflowerblue;
+    font-size: 1.7em;
   }
   .text-block h6 {
-    color:lightslategray;
+    color:rgb(129, 145, 161);
+    font-size: 0.8em;
   }
-  .column h1 {
+  .fulfillment {
+    padding: 15px;
+    margin: 2px;
+    border-radius: 8px;
+    color: #e3e8e4;
+    background-color: #434f41;
+  }
+  .unfulfilled_fulfillment {
+    padding: 15px;
+    margin: 2px;
+    border-radius: 8px;
+    color: #e3e8e4;
+    background-color: #4f433e;
+  }
+  .recommendations {
+    padding: 15px;
+    margin: 2px;
+    border-radius: 8px;
+    background-color: #393b40;
+  }
+  .recommendations-list {
+    color: #e3e8e4;
+  }
+  .req_fulfilled {
+    color: greenyellow;
+    background-color: #434f41;
+  }
+  .req_unfulfilled {
+    color: orangered;
+    background-color: #4f433e;
+  }
+  .heading {
     text-align: center;
   }
 </style>
