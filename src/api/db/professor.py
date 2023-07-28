5ee103a8d7fe0237201a589d760303953e6a776f
Jun Song <40209659+Acewvrs@users.noreply.github.com>
2023-07-28 14:13:07 -0400

    attempt at fixing warning

--- src/api/db/professor.py
+++ src/api/db/professor.py
@@ -166,15 +166,15 @@ class Professor:
     def get_professor_info_by_email(self, email):
         if email is not None:
             sql = """
-                    select
+                    SELECT 
                         *
-                    from
+                    FROM 
                         professor
-                    where
-                        email = '{email}'
-                    """
-            info, error = self.db_conn.execute(sql, None, True)
-            return (info, None) if not error else (False, error)
+                    WHERE 
+                        email = %s
+                """
+            info, error = self.db_conn.execute(sql, email, True)
+        return (info, None) if not error else (False, error)
 
     def get_professor_phone_number_by_email(self, email):    
         if email is not None:
