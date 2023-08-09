82763eb38015a3c8f78e5d19664fe0410af50972
jonha1 <jonnyha888@gmail.com>
2023-08-08 21:07:10 -0400

    got rid of INQR

--- src/web/src/utils.js
+++ src/web/src/utils.js
@@ -392,7 +392,6 @@ export const getLongName = (department) => {
     ECON: "Economics",
     GSAS: "Games and Simulations Arts and Sciences",
     IHSS: "Interdisciplinary HASS",
-    INQR: "",
     LANG: "Languages",
     LITR: "Literature",
     PHIL: "Philosophy",
