7e6db1d4f145fb34f667e51c706ebe57fb89fe2d
jonha1 <jonnyha888@gmail.com>
2023-08-08 20:34:43 -0400

    Revert "718 finals schedule days don't line up (#737)"
    This reverts commit cde1586f1bc7fa57f3deb00a3a256a64014569cc.

--- src/web/src/pages/Calendar.vue
+++ src/web/src/pages/Calendar.vue
@@ -109,7 +109,6 @@ export default {
         STU: "Studio",
         null: "No Type",
       },
-      
     };
   },
   methods: {
@@ -154,9 +153,6 @@ export default {
     mapSessionType(type) {
       return this.sessionTypes[type] == null ? type : this.sessionTypes[type];
     },
-
-   
-    
   },
   computed: {
     hasConflict() {
@@ -275,7 +271,6 @@ export default {
     hourHeight() {
       return (60 * 100) / this.numMinutes;
     },
-
   },
   watch: {
     possibility(val) {
@@ -284,7 +279,6 @@ export default {
     },
   },
 };
-
 </script>
 
 <style scoped lang="scss">
@@ -326,7 +320,7 @@ $hourFontSize: 0.5em;
   margin: 0 auto;
   margin-bottom: 3px;
   text-align: center;
-  font-size: 0.85vw;
+  font-size: 0.8em;
   font-variant: small-caps;
 }
 
