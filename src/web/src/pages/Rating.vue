cbcf129c59767c611212c2caa0a400e09fdb53e6
Dong Lin <lind6@rpi.edu>
2023-08-16 20:46:33 -0400

    Rating Modification

--- src/web/src/pages/Rating.vue
+++ src/web/src/pages/Rating.vue
@@ -1,59 +1,73 @@
 <template>
-    <div class="rating">
-      <div
-        v-for="star in 5"
-        :key="star"
-        @click="rate(star)"
-        @mouseover="hoverRating(star)"
-        @mouseleave="resetHover"
-        :class="{ 'selected': star <= currentRating, 'hover': star <= hoverRatingValue }"
-      >
-        &#9733;
-      </div>
+  <div class="rating">
+    <div
+      v-for="star in totalStars"
+      :key="star"
+      @click="rate(star)"
+      @mouseover="hoverRating(star)"
+      @mouseleave="resetHover"
+      :class="{
+        'selected': star <= currentRating,
+        'hover': star <= hoverRatingValue
+      }"
+    >
+      {{ getStarSymbol(star) }}
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      initialRating: {
-        type: Number,
-        default: 0
-      }
+    <p v-if="showRating">Your Rating: {{ currentRating }}/5</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialRating: {
+      type: Number,
+      default: 0,
     },
-    data() {
-      return {
-        currentRating: this.initialRating,
-        hoverRatingValue: 0
-      };
+    totalStars: {
+      type: Number,
+      default: 5,
     },
-    methods: {
-      rate(rating) {
-        this.currentRating = rating;
-        this.$emit('rate', rating);
-      },
-      hoverRating(rating) {
-        this.hoverRatingValue = rating;
-      },
-      resetHover() {
-        this.hoverRatingValue = 0;
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .rating {
-    display: inline-block;
-  }
-  
-  .hover {
-    color: gold;
-    cursor: pointer;
-  }
-  
-  .selected {
-    color: gold;
-  }
-  </style>
-  
+    showRating: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      currentRating: this.initialRating,
+      hoverRatingValue: 0,
+    };
+  },
+  methods: {
+    rate(rating) {
+      this.currentRating = rating;
+      this.$emit('rate', rating);
+    },
+    hoverRating(rating) {
+      this.hoverRatingValue = rating;
+    },
+    resetHover() {
+      this.hoverRatingValue = 0;
+    },
+    getStarSymbol(star) {
+      return star <= this.currentRating ? '★' : '☆';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.rating {
+  display: inline-block;
+}
+
+.hover {
+  color: gold;
+  cursor: pointer;
+}
+
+.selected {
+  color: gold;
+}
+</style>
