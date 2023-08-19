b876914e4ba7d7e15db31888ae68f9bfdef06e92
EggyRepublic <Eggyrepublic@gmail.com>
2023-08-19 01:43:01 -0400

    moved celery and redis to main docker-compose
    should be deployment ready

--- src/api/celery_app.py
+++ src/api/celery_app.py
@@ -4,15 +4,15 @@ from degree_planner.dp.recommend import recommend
 from degree_planner.dp.fulfill import get_optimized_fulfillment, get_group_fulfillment, get_fulfillment_details
 from degree_planner.dp.requirement import Requirement
 
-celery_broker = 'redis://redis-celery:6379/0'
-celery_backend = 'redis://redis-celery:6379/1'
+celery_broker = environ.get('CELERY_BROKER', 'redis://redis-celery:6379/0')
+celery_backend = environ.get('CELERY_BACKEND', 'redis://redis-celery:6379/1')
 celery_app = Celery("celery_app", broker=celery_broker, backend=celery_backend)
 
 celery_app.conf.update(
     task_serializer=environ.get('CELERY_TASK_SERIALIZER', 'pickle'),
     result_serializer=environ.get('CELERY_RESULT_SERIALIZER', 'pickle'),
     accept_content=environ.get('CELERY_ACCEPT_CONTENT', 'pickle').split(','),
-    result_expires=60,
+    result_expires=int(environ.get('CELERY_RESULT_EXPIRES', '60'))
 )
 
 @celery_app.task()
