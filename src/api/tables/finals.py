58db88636a8ef70004a991b1e2472c3409f9aac9
samhpark1 <samhpark1@gmail.com>
2023-09-22 17:26:38 -0400

    Setting up finals.py
    created columns for table and set respective characterstics to primary keys

---
+++ src/api/tables/finals.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import VARCHAR
+
+from .database import Base
+
+class Finals(Base):
+    __tablename__ = "finals"
+
+    courseCode = Column(VARCHAR(length=255), primary_key=True, nullable=False)
+    section = Column(VARCHAR(legnth=255), primary_key=True, nullable=False)
+    department = Column(VARCHAR(length=255), nullable=False)
+    room = Column(VARCHAR(length=255), nullable=False)
+    dayOfWeek = Column(VARCHAR(length=255), nullable=False)
+    day = Column(VARCHAR(legnth=255), nullable=False)
+    hour = Column(VARCHAR(length=255), nullable=False)
+
+
+    
+
+    # rcs = Column(VARCHAR(length=255), nullable = True)
