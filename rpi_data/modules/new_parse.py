787ee7abb68646625861f4003c5d6bfca53f72ca
martig7 <martig7@rpi.edu>
2023-09-29 17:38:11 -0400

    Feature Request — Rewrite webscraper to use SIS login Fixes #801

--- rpi_data/modules/new_parse.py
+++ rpi_data/modules/new_parse.py
@@ -3,3 +3,11 @@ from bs4 import BeautifulSoup as bs
 
 URL = "https://sis.rpi.edu"
 
+login = input("Input Your User: ")
+password = input("Input Your Password: ")
+
+with requests.session() as s:
+    req = s.get(URL).text
+    html = bs(req, "html.parser")
+    token = html.find("input")
+    
