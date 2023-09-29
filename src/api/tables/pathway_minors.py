1f207ac555cce9de30214fe70413a0bc1b48e659
walshm7 <95325262+walshm7@users.noreply.github.com>
2023-09-29 16:59:06 -0400

    update pathways_minors
    fix typo in pathway minors

--- src/api/tables/pathway_minors.py
+++ src/api/tables/pathway_minors.py
@@ -4,7 +4,7 @@ from sqlalchemy.dialects.postgresql import VARCHAR
 from .database import Base
 
 class pathway_minors(Base):
-    __tablename__ = "pathway_minors(Base):"
+    __tablename__ = "pathway_minors"
 
     pathway = Column(VARCHAR(length=255), primary_key = True, nullable=False)
     minor  = Column(VARCHAR(length=255), primary_key = True, nullable=False)
