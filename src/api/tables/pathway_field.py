b7ade24c4ef9a875f2daa272d545f29968bd5ba0
anthony-basnight <anthonyhbasnight@gmail.com>
2023-09-29 16:55:52 -0400

    changed the primary keys and nullable attributes with Mikayla and Matt

--- src/api/tables/pathway_field.py
+++ src/api/tables/pathway_field.py
@@ -6,11 +6,11 @@ from .database import Base
 class pathway_field(Base):
     __tablename__ = "pathway_field"
 
-    pathway_name = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    course_name = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    field_number = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    occurance = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    course_credits = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    course_level = Column(VARCHAR(length=255), primary_key = True, nullable=False)
-    desc_credit_level = Column(VARCHAR(length=255), primary_key = True, nullable=False)
+    pathway_name = Column(VARCHAR(length=255), primary_key=True, nullable=False)
+    course_name = Column(VARCHAR(length=255), primary_key= True, nullable=False)
+    field_number = Column(VARCHAR(length=255), primary_key= False, nullable=False)
+    occurrence = Column(VARCHAR(length=255), primary_key= False, nullable=False)
+    course_credits = Column(VARCHAR(length=255), primary_key=False, nullable=False)
+    desc_credit_level = Column(VARCHAR(length=255), primary_key=False, nullable=True)  # Ex: 12 credits at the...
+    desc_course_level = Column(VARCHAR(length=255), primary_key=False, nullable=True)  # Ex: ... 4000 level
 
