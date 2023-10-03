865d9e9d97ceaf557a45aca7b81e0f9bf2e5675a
martig7 <martig7@rpi.edu>
2023-10-03 16:13:21 -0400

    Feature Request — Rewrite webscraper to use SIS login Fixes #801

--- rpi_data/modules/new_parse.py
+++ rpi_data/modules/new_parse.py
@@ -9,5 +9,14 @@ password = input("Input Your Password: ")
 with requests.session() as s:
     req = s.get(URL).text
     html = bs(req, "html.parser")
-    token = html.find("input")
+    token = html.find("input", {"name" : "csrf_token"}).attrs["value"]
+    payload = {
+        "csrf_token" : token,
+        "j_username" : login,
+        "j_password" : password,
+        "_eventId_proceed" : ""
+    }
+    
+    res = s.post(URL, data = payload)
+    new_req = s.get(s.url).text
     
