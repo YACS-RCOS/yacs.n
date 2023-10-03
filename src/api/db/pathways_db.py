7064725570cadad03d94585697a7c0782f08e3bc
Matt Lammon <mlammon11@gmail.com>
2023-10-03 17:38:45 -0400

    created functions in pathway_db
    made add_pathway and add_bulk_profesors that parses pathwayV2 json file to grab category and pathway names

--- src/api/db/pathways_db.py
+++ src/api/db/pathways_db.py
@@ -5,13 +5,13 @@ import asyncio
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# from your_module import Professor, Base
+#from your_module import Professor, Base
 
 # https://stackoverflow.com/questions/54839933/importerror-with-from-import-x-on-simple-python-files
-# if __name__ == "__main__":
-#     import connection
-# else:
-#     from . import connection
+if __name__ == "__main__":
+    import connection
+else:
+    from . import connection
 
 
 class Pathway:
@@ -19,74 +19,44 @@ class Pathway:
         self.db_conn = db_conn
         self.cache = cache
 
-    # def add_professor(self, first_name, last_name, email, phone, dep, office,
-    #     classes, office_time, rcs):
-    #         if email is not None:
-    #             print(email)
-    #             return self.db_conn.execute("""
-    #         INSERT INTO
-    #             professor (first_name, last_name, email, phone_number,
-    #             department, office_room, classes, office_hours_time, rcs)
-    #         VALUES
-    #                (%(First_name)s, %(Last_name)s, %(Phone_number)s, %(Email)s,
-    #                %(Dep)s, %(Office_room)s, %(Classes)s, %(Office_time)s, %(Rcs_id)s)
-    #         ON CONFLICT DO NOTHING
-    #         ;
-    #     """, {
-    #             "First_name": first_name,
-    #             "Last_name": last_name,
-    #             "Email": email,
-    #             "Phone_number": phone,
-    #             "Dep": dep,
-    #             "Office_room": office,
-    #             "Classes": classes,
-    #             "Office_time": office_time,
-    #             "Rcs_id": rcs
-    #         }
-    #     , False)
-    #         else:
-    #             return (False, "email cant be none")
+    def add_pathway(self, pathway_name, category_name):
+            if pathway_name is not None:
+                print(pathway_name)
+                return self.db_conn.execute("""
+            INSERT INTO
+                pathway (pathway_name, category_name)
+            VALUES
+                   (%(Pathway_name)s, %(Category_name)s)
+            ON CONFLICT DO NOTHING
+            ;
+        """, {
+                "Pathway_name": pathway_name,
+                "Category_name": category_name
+            }
+        , False)
+            else:
+                return (False, "pathway_name cannot be none")
 
 
 
-def add_bulk_professor():
+def add_bulk_pathways(self):
     # Load the JSON data from a file
-    with open('../../../src/web/src/pages/pathwayV2.json') as file:
+    with open('../../web/src/pages/pathwayV2.json') as file:
         data = json.load(file)
 
     # Connect to the SQL database
-    #conn = self.db.get_connection()
+    conn = self.db.get_connection()
 
-    # Loop through each professor record in the JSON data
+    # Loop through each pathways in json file
     for category in data:
-        category_name = category["Category Name"][0]
-        print(f"Category Name: {category_name}")
-
-        for pathway in category["Pathways"]:
-            pathway_name = pathway["Name"][0]
-            print(f"Pathway Name: {pathway_name}")
-
-            choose_one = pathway["Choose one of the following"]
-            choose_remaining = pathway["Choose remaining credits from the following"]
-
-            print("Choose one of the following:")
-            for course in choose_one:
-                print(f"- {course}")
-
-            print("Choose remaining credits from the following:")
-            for course in choose_remaining:
-                print(f"- {course}")
-
-            compatible_minors = pathway.get("Compatible minor(s)")
-            if compatible_minors:
-                print("Compatible minor(s):", ", ".join(compatible_minors))
-
-            print("\n")
-        #conn.add(professor)
+        for pathway in category['Pathways']:
+            pathway = Pathway(pathway_category = category["Category Name"][0],
+                              pathway_name = pathway["Name"][0])
+            conn.add(pathway)
 
     # Commit the changes to the database
-    #onn.commit()
-    #self.clear_cache()
+    conn.commit()
+    self.clear_cache()
     return (True,None)
 
 
@@ -96,4 +66,4 @@ def add_bulk_professor():
 #     pathways = Pathway(connection.db)
 #     pathways.populate_from_csv(csv_text)
 
-add_bulk_professor()
+add_bulk_pathways()
