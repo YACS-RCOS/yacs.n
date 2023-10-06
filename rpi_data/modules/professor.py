c689e1e7604903546b9a95f4877684bfde571cc4
RC <91489366+RonglinC@users.noreply.github.com>
2023-10-06 17:18:41 -0400

    first_edition_scrape

---
+++ rpi_data/modules/professor.py
@@ -0,0 +1,41 @@
+import json
+from bs4 import BeautifulSoup
+import requests
+with open('Professors.json') as f:
+    data =json.load(f)
+
+def scrape_info(url):
+    response =requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+    link=soup.find('a',class_='nav-link',text='Teaching')
+
+    if link:
+        name=link.text
+        print(name)
+    else:
+        print("not found")
+    
+    
+for professor in data:
+    name = professor['Name']
+    print(name)
+    title = professor['Title']
+    email = professor['Email']
+    phone =professor['Phone']
+    department = professor['Department']
+    portfolio  = professor['Portfolio']
+    profile = professor['Profile Page']
+
+    if profile:
+        classes = scrape_info(profile)
+        professor['Teaching']=classes
+    else:
+        professor['Teaching']='Not available'
+
+'''
+save the updated data to new json file
+'''
+with open('Professors.json','w')as f:
+    json.dump(data,f,indent=4)
+
+
