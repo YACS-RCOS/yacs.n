0123ac6d02fd4cff2be654ec0532e4e12358c0e3
RC <91489366+RonglinC@users.noreply.github.com>
2023-10-13 17:24:06 -0400

    course scrape work but has bug
    scrape the couse but also scrape some unnecessary information beyond course name

--- rpi_data/teaching.py
+++ rpi_data/teaching.py
@@ -5,62 +5,36 @@ import requests
 with open('Professors.json') as f:
     data = json.load(f)
 
+#testing html: https://faculty.rpi.edu/kathleen-galloway
+#testing html: https://faculty.rpi.edu/keith-fraser
 
-def extract_courses(soup):
-    teachings = {}
+def extract_courses_from_html(url):
+    response=requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
     
-    teaching_header = soup.find('h2', class_='red')
+    # Find the teaching header
+    teaching_header = soup.find('h2', class_='red sans-alt text-uppercase', string='Teaching')
     
     if not teaching_header:
-        return teachings  # return empty dictionary if the header is not found
-    
-    course_info_div = teaching_header.find_next_sibling('div', class_='field--name-field-teaching-summary')
-    if not course_info_div:
-        return teachings  # return empty dictionary if the courses div is not found
+        return []
 
-    categories = course_info_div.select('strong')
-    
-    for category in categories:
-        cat_name = category.text.strip()
-        teachings[cat_name] = []
-
-        for sibling in category.find_next_siblings():
-            if sibling.name == 'strong':
-                break
-            if sibling.name == 'p' and sibling.text.strip() != '':
-                teachings[cat_name].append(sibling.text.strip())
-
-    return teachings
+    courses = []
+    for sibling in teaching_header.find_all_next():
+        # If you hit another header, stop
+        if sibling.name and sibling.name.startswith('h'):
+            break
+        # If it's a paragraph, extract the course name
+        if sibling.name == 'p':
+            text = sibling.text.strip()
+            if text:  # To ensure non-empty strings
+                courses.append(text)
+    print(courses)
+    return courses
 
 
-def scrape_info(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extracting titles
-    titles_list = soup.select('.doc-title.serif a')
-    titles = [title.text.strip() for title in titles_list]
-    print(titles)  
-    
-    # Extracting teaching info
-    teachings = extract_courses(soup)
-    
-    return {
-        'titles': titles,
-        'teachings': teachings
-    }
-
-    
-for professor in data:
-    profile = professor['Profile Page']
-
-    if profile:
-        extracted_data = scrape_info(profile)
-        professor['Titles'] = extracted_data['titles']
-        professor['Teaching'] = extracted_data['teachings']
-    else:
-        professor['Teaching'] = 'Not available'
-
-# Save the updated data to new json file
-with open('Updated_Professors.json', 'w') as f:
-    json.dump(data, f, indent=4)
+# testing 
+# it work but also scrape the thing beside the course name 
+url ="https://faculty.rpi.edu/keith-fraser"
+course = extract_courses_from_html(url)
+print(course)
