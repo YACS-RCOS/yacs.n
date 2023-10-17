f22065310a867e5f86e54a8e6c5dfdaeb703c48c
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2023-10-17 16:59:17 -0400

    Implemented Login Features for backend

--- src/web/src/components/Login.vue
+++ src/web/src/components/Login.vue
@@ -20,9 +20,7 @@
       ></b-form-input>
     </b-form-group>
     <b-button type="submit" variant="primary">Submit</b-button>
-    <b-button @click="initiateGoogleOneTap" variant="primary">
-    Login with Google
-</b-button>
+    <b-button @click="initiateGoogleOneTap" variant="primary" style="margin-left: 10px;">Login with Google</b-button>
     <div>
       <b-button-group size="md">
         <button
@@ -60,37 +58,55 @@ export default {
     SignUpForm: SignUpComponent,
   },
   data() {
-    return {
-      form: {
-        email: "",
-        password: "",
-      },
-      showForm: true,
-    };
+      return {
+        form: {
+          email: "",
+          password: "",
+          token: null // Store the Google ID token here
+        },
+        showForm: true,
+      };
   },
   methods: {
     async onSubmit(evt) {
-      evt.preventDefault();
+        evt.preventDefault();
 
-      try {
-        await this.$store.dispatch(userTypes.actions.LOGIN, this.form);
+        let loginData;
+        if (this.form.token) { 
+            // If logging in with Google
+            loginData = {
+                token: this.form.token,
+                type: "google"
+            };
+        } else {
+            // Standard email/password login
+            loginData = {
+                email: this.form.email,
+                password: this.form.password,
+                type: "standard"
+            };
+        }
 
-        this.$bvToast.toast(`You are now logged in!`, {
-          title: `Welcome ${this.user.name}`,
-          variant: "success",
-        });
+        try {
+            await this.$store.dispatch(userTypes.actions.LOGIN, loginData);
 
-        this.$emit("submit");
-      } catch (err) {
-        this.$bvToast.toast(err, {
-          title: "Login failed!",
-          variant: "danger",
-        });
-      }
+            this.$bvToast.toast(`You are now logged in!`, {
+              title: `Welcome ${this.user.name}`,
+              variant: "success",
+            });
+
+            this.$emit("submit");
+        } catch (err) {
+            this.$bvToast.toast(err, {
+              title: "Login failed!",
+              variant: "danger",
+            });
+        }
     },
+
     initiateGoogleOneTap() {
         window.google.accounts.id.initialize({
-            client_id: "747784477249-pkqhk4sj2s6hhe1i3pa74k57d8c1mspv.apps.googleusercontent.com",
+            client_id: "833663758121-ff8hq6a8ibujhv969laf6h9edc000ad2.apps.googleusercontent.com",
             callback: this.handleGoogleResponse
         });
 
@@ -98,14 +114,17 @@ export default {
     },
 
     handleGoogleResponse(response) {
-        // This function will be triggered after the user selects an account.
-        // You can extract user information from the `response` object.
-        
+        // Get the ID token from the Google response
+        const idToken = response.credential;
+
+        // Update the form data
         this.form.email = response.getBasicProfile().getEmail();
+        this.form.token = idToken; // Use a new property to store the token
 
         // Then, proceed with your login or authentication process.
         this.onSubmit();
     },
+
     onSignUp() {
       this.$refs["signup-modal"].hide();
     },
