68f312b41a6a589fa948c2c1d9e38d5d6120bc09
jonha1 <jonnyha888@gmail.com>
2023-10-31 17:27:34 -0400

    fix syntax error

--- src/web/src/pages/UploadJson.vue
+++ src/web/src/pages/UploadJson.vue
@@ -30,41 +30,7 @@
       <b-spinner v-show="loading" />
     </b-container>
   </template>
-  
-<script>
-  <b-container>
-    <section id="import-json">
-      <h2>Import Data</h2>
-      <div class="well well-sm">
-        Input professor data as JSON
-      </div>
-      <form @submit.prevent="onSubmit" class="form-group">
-        <div class="input-group">
-          <label>
-            Upload JSON file
-            <input type="file" name="file" class="form-control-file" />
-          </label>
-          <br />
-        </div>
-        <div class="input-group">
-          <label>
-            <input type="checkbox" name="isPubliclyVisible" checked />
-            Make Public
-          </label>
-        </div>
-        <input
-          type="Submit"
-          label="submit"
-          class="btn btn-success btn-sm"
-          value="Submit"
-        />
-      </form>
-    </section>
-    <b-spinner v-show="loading" />
-  </b-container>
-</template>
 
-<script>
 import { uploadJSON } from "@/services/AdminService";
 
 export default {
