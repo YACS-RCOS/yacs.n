686eede1fcc5d5234888848854bc313e87b27b4d
snoopy0328 <144743638+snoopy0328@users.noreply.github.com>
2023-11-03 22:19:40 -0400

    fixed the drop down indication and overall format of the faq page

--- src/web/src/pages/FAQs.vue
+++ src/web/src/pages/FAQs.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="faq-page">
-      <h1>Frequently Asked Questions</h1>
-  
-      <div v-for="(question, index) in faqData" :key="index" class="faq-item">
-        <div class="question-wrapper" @click="toggleAnswer(index)">
-          <h3 :class="{ 'active': question.isOpen }">{{ question.title }}</h3>
-          <div class="dropdown-indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
-              <polyline points="6 9 12 15 18 9"></polyline>
-            </svg>
-          </div>
-        </div>
-        <div class="divider"></div>
-        <div class="answer-wrapper" :class="{ 'open': question.isOpen }" :style="{'max-height': question.maxheight }">
-          <p v-if="question.isOpen">{{ question.answer}}</p>
-          <p v-if="question.isOpen">{{ question.answer1}}</p>
-          <p v-if="question.isOpen">{{ question.answer2,}}</p>
-          <p v-if="question.isOpen">{{ question.answer3}}</p>
+  <div class="faq-page">
+    <h1>Frequently Asked Questions</h1>
+    <div v-for="(faq, index) in faqData" :key="index" class="faq-item" :class="{ open: faq.isOpen }" @click="toggleAnswer(index)">
+      <div class="question-wrapper">
+        <h3 :class="{ active: faq.isOpen }">{{ faq.title }}</h3>
+        <div class="dropdown-indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
         </div>
       </div>
+      <div class="divider"></div>
+      <div class="answer-wrapper" :class="{ open: faq.isOpen }">
+        <p>{{ faq.answer }}</p>
+        <p>{{ faq.answer1 }}</p>
+        <p>{{ faq.answer2 }}</p>
+        <p>{{ faq.answer3 }}</p>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
 <script>
 export default {
@@ -30,13 +29,11 @@ export default {
         faqData: [
             {
                 title: 'What is YACS and what is its mission?',
-                answer_symbol: 'A:',
                 answer:' YACS (Yet Another Course Scheduler) is a web-based application designed to assist students in scheduling and planning their courses at academic institutions. It provides features such as course catalog browsing, schedule building, and course availability information. YACS aims to simplify the course selection process, allowing students to efficiently organize their academic schedules.',
                 isOpen: false
             },
             {
                 title: 'How can I use the Schedule feature?',
-                answer_symbol: 'A:',
                 answer:'1. Click the Schedule navbar on the top of the page.',
                 answer1:'2. Add courses to your schedule by searching for them in the course catalog or using other available options.',
                 answer2:'3. Arrange the courses in your preferred time slots or based on your schedule requirements.',
@@ -45,7 +42,6 @@ export default {
             },
             {
                 title: 'What is the Explore feature?',
-                answer_symbol: 'A:',
                 answer:'1. Click the Explore navbar at the top of the page.' ,
                 answer1:'2. Choose the major you want to explore from the available options. ',
                 answer2:'3. YACS will display the courses associated with the selected major for the specified semester.',
@@ -54,7 +50,6 @@ export default {
             },
             {
                 title: 'How can I use the Pathways Navigation feature?',
-                answer_symbol: 'A:',
                 answer:'1. Click the Pathways button at the top of the page.',
                 answer1:'2. Select the pathway you are interested in or that aligns with your academic goals.',
                 answer2:'3. YACS will display the pathway requirements and the courses available to fulfill each requirement.',
@@ -63,7 +58,6 @@ export default {
             },
             {
                 title: 'How can I use the Finals feature?',
-                answer_symbol: 'A:',
                 answer:'1. Click the Finals button on the top of the page.',
                 answer1:'2. Enter the courses you are enrolled in for the semester. Once you finish adding your courses, hit Search.',
                 answer2:'3. YACS will generate your final exam schedule, displaying the date, time, and location of each exam.',
@@ -96,20 +90,23 @@ h1 {
 }
 
 .faq-item {
-  margin-bottom: 20px;
-  border: 3px solid #ccc;
-  border-radius: 4px;
+  /* margin-bottom: 20px; */
+  /* border: 3px solid #ccc; */
+  /* border-radius: 4px; */
   padding: 10px;
   cursor: pointer;
 }
 
+/* .faq-item:not(:first-child){
+  border-top: 3px solid #ccc;
+} */
 .faq-item:hover {
   border-color: hsl(211, 100%, 60%);
 }
 
-.question-wrapper {
+/* .question-wrapper {
   margin-bottom: 10px;
-}
+} */
 
 .faq-item h3 {
   font-size: 18px;
@@ -151,20 +148,19 @@ h1 {
 }
 
 .dropdown-indicator {
-  width: 20px;
-  height: 20px;
-  margin-left: auto;
-  display: inline-flex;
-  align-items: center;
-  transition: transform 0.3s ease;
-}
-.open .dropdown-indicator {
-  transform: rotate(180deg);
-  width: 20px;
-  height: 20px;
-  margin-left: auto;
-  display: inline-flex;
-  align-items: center;
-  transition: transform 0.3s ease;
-}
+    width: 20px;
+    margin-left: auto;
+    display: inline-flex;
+    align-items: center;
+    transition: transform 0.3s ease;
+  }
+  .open .dropdown-indicator {
+    transform: rotate(180deg);
+    width: 20px;
+    height: 20px;
+    margin-left: auto;
+    display: inline-flex;
+    align-items: center;
+    transition: transform 0.3s ease;
+  }
 </style>
