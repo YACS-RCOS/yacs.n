6dd6fdeca1fb4fc214b9945562b300752c5cc4a3
lForger <navajaydan@gmail.com>
2023-12-08 23:31:42 -0500

    Completed getCataId

--- rpi_data/modules/prereq_parse.py
+++ rpi_data/modules/prereq_parse.py
@@ -1,5 +1,5 @@
 #Ok so this file should take in some inputs (what though) and output a dictionary.
-#This dictionary has all of the courses' crn as the keys and the value as the relevant information
+#This dictionary has all of the courses' course code as the keys and the value as the relevant information
 #See fetch_catalog_course_info.py for the source of a lot of the code
 #It's just that I will be rewriting it so that it is (hopefully) more understandable and better documented
 import requests as req
@@ -20,7 +20,7 @@ import pdb
 class PrereqParse:
     def __init__(self, api_key):
         self.key = api_key
-        self.search_endpoint = "http://rpi.apis.acalog.com/v2/search/courses"
+        self.search_endpoint = "http://rpi.apis.acalog.com/v2/search/courses?"
         self.course_detail_endpoint = "http://rpi.apis.acalog.com/v2/content?options[full]=1&method=getItems&type=courses"
         self.catalogIdEndpoint = "http://rpi.apis.acalog.com/v2/content?key=3eef8a28f26fb2bcc514e6f1938929a1f9317628&format=xml&method=getCatalogs"
         self.resFormat = "xml"
@@ -37,19 +37,18 @@ class PrereqParse:
         tree = etree.parse(StringIO(res.content.decode()))
         root = tree.getroot()
         try:
-            pdb.set_trace()
-            #currentCatalog = root.xpath("//*[local-name() = 'archived'][contains(text(), 'No')]")
-            #currentCatalog = root.xpath("//*[local-name() = 'archived']")
-            #tree.xpath("//*[local-name() = 'archived']")[0].getparent().getparent().attrib['id']
-            #TODO: Rewrite in regex
-            #TODO: get the catalog id from this
-            retCata = root.xpath("//*[local-name() = 'archived']")[0].getparent().getparent().attrib['id']
-            return 0
+            #TODO: Test this
+            key = "Rennselaer Catalog " + year + "-" + (year + 1)
+            for paths in root.xpath("//text()"):
+                if path == key:
+                    return re.search("(?P<id>\d+)$", path.getparent().getparent().attrib['id'])
         except Exception as exception:
             print("Failed to get the catalog id")
     #Return the endpoint of the course ids
     def getCourseIds(self):
         return 0
+        idXML = req.get("{self.search_endpoint}?key={self.key}&format={self.resFormat}&method=listing&catalog={self.cataId}&options[limit]=0").content
+
     #Return a dictionary of every course in the catalog
     def getAllCourses(self):
         return 0
