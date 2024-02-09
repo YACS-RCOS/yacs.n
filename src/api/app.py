3aab465beae59e9a8124e78e80db0b9a8586a598
Tevetron <friedrich.jonathan4@gmail.com>
2024-02-09 16:41:10 -0500

    Update app.py

--- src/api/app.py
+++ src/api/app.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, HTTPException, Request, Response, UploadFile, Form, File, Depends
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi_cache import FastAPICache
+from fastapi.security import OAuth2PasswordBearer
 from fastapi_cache.backends.inmemory import InMemoryBackend
 from fastapi_cache.decorator import cache
 from fastapi_cache.coder import PickleCoder
@@ -38,6 +39,7 @@ app.add_middleware(SessionMiddleware,
                    secret_key=os.environ.get("API_SIGN_KEY", "localTestingKey"))
 
 FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # - init interfaces to db
 db_conn = connection.db
@@ -50,19 +52,29 @@ semester_info = SemesterInfo.semester_info(db_conn)
 professor_info = All_professors.Professor(db_conn, FastAPICache)
 users = UserModel.User()
 
+
 def is_admin_user(session):
     if 'user' in session and (session['user']['admin'] or session['user']['super_admin']):
         return True
     return False
+
 
 @app.get('/')
 @cache(expire=Constants.HOUR_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
 async def root(request: Request):
     return Response(content='YACS API is Up!',)
 
+
+@app.get("/auth")
+@cache(expire=Constants.HOUR_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
+async def authenticate(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+
 @app.get('/api')
 def apiroot():
     return Response(content='wow')
+
 
 @app.get('/api/class')
 @cache(expire=Constants.HOUR_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
@@ -83,18 +95,20 @@ async def get_classes(request: Request, semester: str or None = None, search: st
             classes, error = class_info.get_classes_full(semester)
         return classes if not error else Response(error, status_code=500)
     return Response(content="missing semester option", status_code=400)
-    
+
+
 @app.get('/api/department')
 @cache(expire=Constants.HOUR_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
 async def get_departments():
     """
     GET /api/department
     Cached: 1 Hour
- 
+
     List of departments i.e. COGS, CIVL, CSCI, BIOL
     """
     departments, error = class_info.get_departments()
     return departments if not error else Response(content=error, status_code=500)
+
 
 @app.get('/api/subsemester')
 @cache(expire=Constants.HOUR_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
@@ -115,6 +129,7 @@ async def get_subsemesters(subsemester: SubsemesterPydantic = Depends(Subsemeste
     db_list = [dict(r) for r in subsemesters]
     return db_list if not error else Response(error, status_code=500)
 
+
 @app.get('/api/semester')
 @cache(expire=Constants.DAY_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
 async def get_semesters():
@@ -126,10 +141,12 @@ async def get_semesters():
     db_list = [dict(r) for r in semesters]
     return db_list if not error else Response(error, status_code=500)
 
+
 @app.get('/api/semesterInfo')
 def get_all_semester_info():
     all_semester_info, error = class_info.get_all_semester_info()
     return all_semester_info if not error else Response(error, status=500)
+
 
 @app.get('/api/defaultsemester')
 def get_defaultSemester():
@@ -147,7 +164,9 @@ def set_defaultSemester(semester_set: DefaultSemesterSetPydantic):
         print(error)
         return Response(error.__str__(), status_code=500)
 
-#Parses the data from the .csv data files
+# Parses the data from the .csv data files
+
+
 @app.post('/api/bulkCourseUpload')
 async def uploadHandler(
         isPubliclyVisible: str = Form(...),
@@ -180,24 +199,25 @@ async def uploadHandler(
         print(error)
         return Response(error.__str__(), status_code=500)
 
+
 @app.post('/api/bulkProfessorUpload')
 async def uploadJSON(
         isPubliclyVisible: str = Form(...),
-        file: UploadFile = File(...)):  
+        file: UploadFile = File(...)):
     # Check to make sure the user has sent a file
     if not file:
         return Response("No file received", 400)
-    
+
     # Check that we receive a JSON file
     if file.filename.find('.') == -1 or file.filename.rsplit('.', 1)[1].lower() != 'json':
         return Response("File must have JSON extension", 400)
-    
+
     # Get file contents
     contents = await file.read()
-    
+
     # Load JSON data
     try:
-        #convert string to python dict
+        # convert string to python dict
         json_data = json.loads(contents.decode('utf-8'))
         # print(json_data)
     except json.JSONDecodeError as e:
@@ -216,34 +236,37 @@ async def uploadJSON(
 
 @app.post('/api/mapDateRangeToSemesterPart')
 async def map_date_range_to_semester_part_handler(request: Request):
-     # This depends on date_start, date_end, and semester_part_name being
-     # ordered since each field has multiple entries. They should be ordered
-     # as each dict entry has the value of list. But if it doesn't work,
-     # look into parameter_storage_class which will change the default
-     # ImmutableMultiDict class that form uses. https://flask.palletsprojects.com/en/1.0.x/patterns/subclassing/
-     form = await request.form()
-     if (form):
-         # If checkbox is false, then, by design, it is not included in the form data.
-         is_publicly_visible = form.get('isPubliclyVisible', default=False)
-         semester_title = form.get('semesterTitle')
-         semester_part_names = form.getlist('semester_part_name')
-         start_dates = form.getlist('date_start')
-         end_dates = form.getlist('date_end')
-         if (start_dates and end_dates and semester_part_names and is_publicly_visible is not None and semester_title):
-             _, error = date_range_map.insert_all(start_dates, end_dates, semester_part_names)
-             semester_info.upsert(semester_title, is_publicly_visible)
-             if (not error):
-                 return Response(status_code=200)
-             else:
-                 return Response(error, status_code=500)
-     return Response("Did not receive proper form data", status_code=500)
+    # This depends on date_start, date_end, and semester_part_name being
+    # ordered since each field has multiple entries. They should be ordered
+    # as each dict entry has the value of list. But if it doesn't work,
+    # look into parameter_storage_class which will change the default
+    # ImmutableMultiDict class that form uses. https://flask.palletsprojects.com/en/1.0.x/patterns/subclassing/
+    form = await request.form()
+    if (form):
+        # If checkbox is false, then, by design, it is not included in the form data.
+        is_publicly_visible = form.get('isPubliclyVisible', default=False)
+        semester_title = form.get('semesterTitle')
+        semester_part_names = form.getlist('semester_part_name')
+        start_dates = form.getlist('date_start')
+        end_dates = form.getlist('date_end')
+        if (start_dates and end_dates and semester_part_names and is_publicly_visible is not None and semester_title):
+            _, error = date_range_map.insert_all(
+                start_dates, end_dates, semester_part_names)
+            semester_info.upsert(semester_title, is_publicly_visible)
+            if (not error):
+                return Response(status_code=200)
+            else:
+                return Response(error, status_code=500)
+    return Response("Did not receive proper form data", status_code=500)
+
 
 @app.get('/api/user/course')
 async def get_student_courses(request: Request):
     if 'user' not in request.session:
         return Response("Not authorized", status_code=403)
 
-    courses, error = course_select.get_selection(request.session['user']['user_id'])
+    courses, error = course_select.get_selection(
+        request.session['user']['user_id'])
     return courses if not error else Response(error, status_code=500)
 
 
@@ -254,9 +277,11 @@ async def get_user_info(request: Request, session_id):
 
     return user_controller.get_user_info(session_id)
 
+
 @app.post('/api/user')
 async def add_user(user: UserPydantic):
     return user_controller.add_user(user.dict())
+
 
 @app.delete('/api/user')
 async def delete_user(request: Request, session: UserDeletePydantic):
@@ -266,12 +291,14 @@ async def delete_user(request: Request, session: UserDeletePydantic):
 
     return user_controller.delete_user(session.dict())
 
+
 @app.put('/api/user')
-async def update_user_info(request:Request, user:updateUser):
+async def update_user_info(request: Request, user: updateUser):
     if 'user' not in request.session:
         return Response("Not authorized", status_code=403)
 
     return user_controller.update_user(user)
+
 
 @app.post('/api/session')
 async def log_in(request: Request, credentials: SessionPydantic):
@@ -283,6 +310,7 @@ async def log_in(request: Request, credentials: SessionPydantic):
         request.session['user'] = user
     return session_res
 
+
 @app.delete('/api/session')
 def log_out(request: Request, session: SessionDeletePydantic):
     response = session_controller.delete_session(session.dict())
@@ -292,24 +320,29 @@ def log_out(request: Request, session: SessionDeletePydantic):
 
     return response
 
+
 @app.post('/api/event')
 def add_user_event(request: Request, credentials: SessionPydantic):
     return Response(status_code=501)
+
 
 @app.post('/api/user/course')
 async def add_student_course(request: Request, credentials: UserCoursePydantic):
     if 'user' not in request.session:
         return Response("Not authorized", status_code=403)
-    resp, error = course_select.add_selection(credentials.name, credentials.semester, request.session['user']['user_id'], credentials.cid)
+    resp, error = course_select.add_selection(
+        credentials.name, credentials.semester, request.session['user']['user_id'], credentials.cid)
     return Response(status_code=200) if not error else Response(error, status_code=500)
 
 
 @app.delete('/api/user/course')
-async def remove_student_course(request: Request, courseDelete:CourseDeletePydantic):
+async def remove_student_course(request: Request, courseDelete: CourseDeletePydantic):
     if 'user' not in request.session:
         return Response("Not authorized", status_code=403)
-    resp,error = course_select.remove_selection(courseDelete.name, courseDelete.semester, request.session['user']['user_id'], courseDelete.cid)
+    resp, error = course_select.remove_selection(
+        courseDelete.name, courseDelete.semester, request.session['user']['user_id'], courseDelete.cid)
     return Response(status_code=200) if not error else Response(error, status_code=500)
+
 
 @app.get('/api/professor/name/{email}')
 async def get_professor_name_by_email(email: str):
@@ -318,10 +351,12 @@ async def get_professor_name_by_email(email: str):
     # Return the data as a JSON response
     return professorName if not error else Response(content=error, status_code=500)
 
+
 @app.get('/api/professor/department/{department}')
 async def get_professor_from_department(department: str):
     professors, error = professor_info.get_professors_by_department(department)
     return professors if not error else Response(content=error, status_code=500)
+
 
 @app.get('/api/professor')
 @cache(expire=Constants.DAY_IN_SECONDS, coder=PickleCoder, namespace="API_CACHE")
@@ -330,33 +365,37 @@ async def get_all_professors():
     GET /api/professor
     Cached: 24 Hours
     """
-    professors, error = professor_info.get_all_professors()  # replace professor_info with db_manager
+    professors, error = professor_info.get_all_professors(
+    )  # replace professor_info with db_manager
     db_list = [dict(prof) for prof in professors] if professors else []
-    return db_list if not error else Response(error, status_code = 500)
+    return db_list if not error else Response(error, status_code=500)
+
 
 @app.get('/api/professor/phone_number/{email}')
 async def get_professor_phone_number_by_email(email: str):
-    
-    phone_number, error = professor_info.get_professor_phone_number_by_email(email)
+
+    phone_number, error = professor_info.get_professor_phone_number_by_email(
+        email)
     return phone_number if not error else Response(content=error, status_code=500)
 
 
 @app.get('/api/professor/email/{email}')
-async def get_professor_info_by_email(email:str):
+async def get_professor_info_by_email(email: str):
     professor_email, error = professor_info.get_professor_info_by_email(email)
     return professor_email if not error else Response(content=error, status_code=500)
 
-#@app.delete('/api/user/course')
+# @app.delete('/api/user/course')
 # async def remove_student_course(request: Request, courseDelete:CourseDeletePydantic):
 #     if 'user' not in request.session:
 #         return Response("Not authorized", status_code=403)
 #     resp,error = course_select.remove_selection(courseDelete.name, courseDelete.semester, request.session['user']['user_id'], courseDelete.cid)
 #     return Response(status_code=200) if not error else Response(error, status_code=500)
 
+
 @app.post('/api/professor/add/{msg}')
-async def add_professor(msg:str):
+async def add_professor(msg: str):
     info = msg.split(",")
-    #msg should be name, title , email ,phone number, dep, portfolio page, rcs
+    # msg should be name, title , email ,phone number, dep, portfolio page, rcs
     # rcs = info[2].split("@")
     # id = rcs[0]
     # print("name", info[0])
@@ -366,19 +405,28 @@ async def add_professor(msg:str):
     # print("dep", info[4])
     # print("portfolio_page", info[5])
     # print("rcs", id)
-    
-    professor, error = professor_info.add_professor(info[0], info[1], info[2], info[3] , info[4],
-    info[5], info[6], info[7], info[8])
+
+    professor, error = professor_info.add_professor(info[0], info[1], info[2], info[3], info[4],
+                                                    info[5], info[6], info[7], info[8])
     return professor if not error else Response(error, status_code=500)
+
 
 @app.post('/api/professor/add/test')
 async def add_test_professor():
-    professor, error = professor_info.add_professor("random", "person", "number", "test?@rpi.edu", "CSCI", 
-        "lally 300", "52995")
-    return professor if not error else Response(content = error, status_code = 500)
+    professor, error = professor_info.add_professor("random", "person", "number", "test?@rpi.edu", "CSCI",
+                                                    "lally 300", "52995")
+    return professor if not error else Response(content=error, status_code=500)
+
 
 @app.delete('/api/professor/remove/{email}')
-async def remove_professor(email:str):
+async def remove_professor(email: str):
     print(email)
     professor, error = professor_info.remove_professor(email)
     return professor if not error else Response(str(error), status_code=500)
+
+
+@app.delete('/api/semester/{id}')
+async def remove_semester(id: str):
+    print(id)
+    semester, error = courses.delete_by_semester(semester=id)
+    return semester if not error else Response(str(error), status_code=500)
