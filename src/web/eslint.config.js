9b8f04595623e84aaba25bb9af5a2272110d8810
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-03-18 22:11:45 -0400

    add linting for tag closing, disable rule for vue views

--- src/web/eslint.config.js
+++ src/web/eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from "typescript-eslint";
 // @ts-expect-error Vue Eslint plugin does not support TS yet
 import vuelint from "eslint-plugin-vue";
 
-/** @type {Record<string, import("typescript-eslint").configs.base.rules>} */
+/** @type {Record<string, typeof import("typescript-eslint").config>} */
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
 const vuelint_rules = Object.fromEntries(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
@@ -13,6 +13,7 @@ const vuelint_rules = Object.fromEntries(
 );
 
 export default tseslint.config(
+  // standard configs
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   tseslint.configs.eslintRecommended,
@@ -21,8 +22,13 @@ export default tseslint.config(
     rules: {
       ...vuelint_rules["vue3-essential"],
       ...vuelint_rules["vue3-strongly-recommended"],
-      ...vuelint_rules["vue3-recommended"],
-
+      ...vuelint_rules["vue3-recommended"]
+    }
+  },
+  prettier,
+  // additional rules we want to use
+  {
+    rules: {
       "vue/block-lang": [
         "error",
         {
@@ -35,7 +41,7 @@ export default tseslint.config(
       "vue/block-order": [
         "error",
         {
-          order: ["script[setup]", "script:not([setup])", "template"]
+          order: ["script:not([setup])", "script[setup]", "template"]
         }
       ],
 
@@ -51,6 +57,16 @@ export default tseslint.config(
       ],
 
       "vue/define-props-declaration": ["error", "type-based"],
+
+      "vue/html-self-closing": [
+        "error",
+        {
+          html: {
+            void: "any"
+          }
+        }
+      ],
+
       "vue/no-boolean-default": ["error", "no-default"],
       "vue/no-ref-object-reactivity-loss": ["warn"],
 
@@ -61,7 +77,19 @@ export default tseslint.config(
         }
       ],
 
-      "vue/no-restricted-block": ["error", "style"],
+      "vue/no-restricted-block": [
+        "error",
+        {
+          element: "style",
+          message:
+            "Do not use <style> block in this project.\n We use TailwindCSS instead; look here: https://tailwindcss.com/docs/utility-first"
+        },
+        {
+          element: "route",
+          message:
+            "Do not use <route> block in this project.\n Use definePage() instead; look here: https://uvr.esm.is/guide/extending-routes.html#definepage"
+        }
+      ],
       "vue/no-useless-mustaches": ["error"],
       "vue/prefer-separate-static-class": ["error"],
       "vue/require-typed-object-prop": ["error"],
@@ -91,10 +119,16 @@ export default tseslint.config(
     }
   },
   {
+    files: ["src/views/**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": ["off"]
+    }
+  },
+  { ignores: ["dist/**/*.*"] },
+  {
     plugins: {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       vue: vuelint
     }
-  },
-  prettier
+  }
 );
