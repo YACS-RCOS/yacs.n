7cfa957acf48bf94aac5f07f0eeff80143c9321d
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-03-18 22:12:18 -0400

    add file-based routing

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,11 +1,15 @@
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import vueRouter from "unplugin-vue-router/vite";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vueRouter({
+      routesFolder: "src/views"
+    }),
     vue(),
     checker({
       vueTsc: {
