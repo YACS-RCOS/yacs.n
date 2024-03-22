76e7a09ed77326fb8b361e2fe6515e22d3bacc3c
kyogrekube <alexblitchfield@gmail.com>
2024-03-22 17:31:27 -0400

    Added dark mode functionality to entire page

--- src/web/src/pages/About.vue
+++ src/web/src/pages/About.vue
@@ -7,25 +7,29 @@
     <!-- New content with named columns and grey section -->
     <div class="centered-boxes">
       <div class="column-section">
-        <!-- Content for the first column -->
-        <h3 class="category-title">What is Yacs?</h3>
-        <!-- Additional content for column 1 -->
-        <p>Yet Another Course Scheduler, or better known as Yacs, is a course scheduler designed to help RPI students plan what courses they will take during each semester.</p>
+        <div class="categoryBox border m-2 mb-4">
+          <!-- Content for the first column -->
+          <h3 class="category-title">What is Yacs?</h3>
+          <!-- Additional content for column 1 -->
+          <p>Yet Another Course Scheduler, or better known as Yacs, is a course scheduler designed to help RPI students plan what courses they will take during each semester.</p>
+        </div>
       </div>
     </div>
     <div class="centered-boxes">
       <div class="column-section">
-        <!-- Content for the second column -->
-        <h3 class="category-title">What can Yacs do?</h3>
-        <!-- Additional content for column 2 -->
-        <p>Yacs has a variety of features that include but are not limited to:</p>
-        <ul>
-          <li>Allow students to view courses from the current and previous semesters</li>
-          <li>Allow students to create a mock schedule using their desired courses</li>
-          <li>Allow students to view information and requirements of current HASS pathways</li>
-          <li>Allow students to view information on RPI professors</li>
-          <li>Allow students to make a mock schedule for their final exams</li>
-        </ul>
+        <div class="categoryBox border m-2 mb-4">
+          <!-- Content for the second column -->
+          <h3 class="category-title">What can Yacs do?</h3>
+          <!-- Additional content for column 2 -->
+          <p>Yacs has a variety of features that include but are not limited to:</p>
+          <ul>
+            <li>Allow students to view courses from the current and previous semesters</li>
+            <li>Allow students to create a mock schedule using their desired courses</li>
+            <li>Allow students to view information and requirements of current HASS pathways</li>
+            <li>Allow students to view information on RPI professors</li>
+            <li>Allow students to make a mock schedule for their final exams</li>
+          </ul>
+        </div>
       </div>
     </div>
 
@@ -71,10 +75,6 @@ data() {
 
 .column-section {
   width: 70%;
-  background-color: #f8f9fa; /* Grey background */
-  border: 1px solid #ced4da; /* Border color */
-  border-radius: 5px; /* Rounded corners */
-  padding: 20px; /* Increase padding to add space between border and content */
 }
 
 p {
