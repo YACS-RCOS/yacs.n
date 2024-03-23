0780f3b241afbbb402e5ce56bff1049af44142b3
martig7 <martig7@rpi.edu>
2024-03-22 20:57:20 -0400

    Wrote some formatting for the scraped data.

--- rpi_data/modules/no_login.py
+++ rpi_data/modules/no_login.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from concurrent.futures import ThreadPoolExecutor
 from course import Course
+from datetime import datetime
 
 
 def find_codes(term, subj):
@@ -31,10 +32,10 @@ def generate_links(term, codes):
         links.append(single_course)
     return links
 
-def scrape_all(links):
+def scrape_all(links, term):
     courses = []
     with ThreadPoolExecutor(max_workers=50) as pool:
-        pool.map(links, courses, link_scrape)
+        pool.map(link_scrape, term, links, courses)
     return courses
 
 # info[0] is crn, info[1] is major, 2 - course code, 3- section, 4 - if class is on campus (most are), 5 - credits, 6 - class name 
@@ -77,12 +78,14 @@ def link_scrape(term, link, major):
         slots = get_slots(term, split_title[1])
         for course in body_info:
             course.append(split_title[1]) # CRN
-            course.append(split_title[2]) # course code
+            course.append(split_title[2].split(" ")[1]) # course code
             course.append(split_title[3]) # section
             course.append(major) # major
             course.append(slots[0]) # capacity
             course.append(slots[1]) # current_enrolled
             course.append(slots[2]) # remaining
+            course.append(number_to_term(term))
+            course.append(split_title[0]) # NAME
             
         format_and_order(body_info)
     return
@@ -138,24 +141,94 @@ def table_scrape(table:bs) -> list[list[str]]:
             scraped_table.append(stripped_row)
     return scraped_table
 
-# Type, Time, Days, Where, Date Range, Schedule Type, Instructor, Credits, CRN, course code, section, major
-# missing: semester
-
 def number_to_term(term) -> str:
-    year = term[:4]
-    semester = term[4:]
-    print(year)
-    print(semester)
+    date = term[:4]
+    if term[4:] == "01":
+        date = "SPRING " + date
+    elif term[4:] == "09":
+        date = "FALL " + date
+    elif term[4:] == "05" or term[4:] == "06" or term[4:] == "07" or term[4:] == "08":
+        date = "SUMMER " + date
+    elif term[4:] == "12":
+        date = "WINTER " + date
+    elif term[4:] == "10":
+        date = "HARTFORD " + date
+    return date
+
+# current order:
+
+# 0: Type, 1: Time, 2: Days, 3: Where, 4: Date Range, 5: Schedule Type, 6: Instructor, 7: Credits, 8: CRN, 9: course code, 10: section, 11: major, 12: capacity, 13: current, 14: rem, 15: term, 16: NAME
+
+# desired order:
+
+#[crn, major, code, section, credits, name, days, stime, etime, max, curr, rem, profs, sdate, enddate, loc]
 
 def format_and_order(courses:list[list[str]]) -> list[list[str]]:
-    print(courses)
+    final_courses = []
+    for course in courses:
+        if (len(course) != 17):
+            raise RuntimeError("Too many things got parsed")
+        final = []
+        final.append(course[8]) # CRN
+        final.append(course[11]) # major
+        final.append(course[9]) # code
+        final.append(course[10]) # section
+        final.append(course[7]) # credits
+        final.append(course[16]) # name
+        final.append(course[2]) # days
+        stime, etime = time_split(course[1])
+        final.append(stime) # stime
+        final.append(etime) # etime
+        final.append(course[12]) # max
+        final.append(course[13]) # curr
+        final.append(course[14]) # rem
+        final.append(course[6]) # profs
+        sdate, edate = date_split(course[4])
+        final.append(sdate) # sdate
+        final.append(edate) # edate
+        final.append(course[3]) # loc
+        print(final)
+        new_course = Course(final)
+        new_course.addSemester(course[15])
+        new_course.print()
+        final_courses.append(new_course)
+    return final_courses
 
-#codes = find_codes(202409, "CSCI")
 
-#links = generate_links(202409, codes)
 
-#link_scrape(202309, "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse?term_in=202309&subj_in=CSCI&crse_in=1200&schd_in=L", "CSCI")
+def time_split(time) -> list[str]:
+    split = time.split(" - ")
+    if (len(split) != 2):
+        raise RuntimeError("Something has gone very wrong in time_split")
+    stime = "".join(split[0].split(" ")).upper()
+    etime = "".join(split[1].split(" ")).upper()
+    return stime, etime
+
+def date_split(date):
+    non_formatted = date.split(" - ")
+    non_formatted_start = non_formatted[0]
+    non_formatted_end = non_formatted[1]
+    dt_start = datetime.strptime(non_formatted_start, '%b %d, %Y')
+    dt_end = datetime.strptime(non_formatted_end, '%b %d, %Y')
+    sdate = "{0:%Y}-{0:%m}-{0:%d}".format(dt_start)
+    edate = "{0:%Y}-{0:%m}-{0:%d}".format(dt_end)
+    return sdate, edate
+
+    
+
+
+
+codes = find_codes(202409, "CSCI")
+
+links = generate_links(202409, codes)
+
+scrape_all(links, "202409")
+
+#link_scrape("202309", "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse?term_in=202309&subj_in=CSCI&crse_in=1200&schd_in=L", "CSCI")
 
 #get_slots(202409, "65029")
 
-print(number_to_term("202409"))
+#print(number_to_term("202407"))
+    
+#date_split("Aug 28, 2024 - Dec 20, 2024")
+
