190524b91d40ae5a83bc194e1d6698bedf676833
martig7 <martig7@rpi.edu>
2024-04-09 17:26:03 -0400

    Got all of the professor links.

--- rpi_data/modules/faculty_scrape.py
+++ rpi_data/modules/faculty_scrape.py
@@ -20,21 +20,49 @@ def access_department_links() -> list[str]:
     for link in links:
         get_professor_links(link)
 
-def get_professor_links(department_link: str):
+def get_professor_links(department_link: str) -> list[str]:
     r = requests.get(department_link)
     tree = etree.HTML(r.content)
     page = 0
-    found = tree.xpath("/html/body/div[2]/div/div/div[2]/div/div[2]/main/section/div[2]/div/div/div/div[2]")[0]
+    print(department_link)
+    found = tree.xpath("/html/body/div[2]/div/div/div[2]/div/div[2]/main/section/div[2]/div/div/div/div[2]")
+    if (len(found) == 0):
+        found = tree.xpath("/html/body/div[2]/div/div/div[2]/div/div[2]/main/section/div[2]/div/div/div/div/div[2]")[0]
+    else:
+        found = found[0]
+    
+                       
     unparsed_professor_branches = found
+    print(len(found))
     while len(unparsed_professor_branches) % 25 == 0:
         page += 1
         temp = requests.get(department_link + "?page="+ str(page))
         temp_tree = etree.HTML(temp.content)
         found = temp_tree.xpath("/html/body/div[2]/div/div/div[2]/div/div[2]/main/section/div[2]/div/div/div/div[2]")
-        if len(found) == 0:
+        if len(found) == 0 or "view-empty" in found[0].attrib["class"]:
             break
         found = found[0]
         [unparsed_professor_branches.append(i) for i in found]
+
+    professorlinks = set()    
+    
+    for element in unparsed_professor_branches:
+        stack = []
+
+        stack.append(element)
+        result = ""
+        
+        while len(stack) != 0:
+            element_checking = stack.pop()
+            if (element_checking.tag == "a"):
+                result = element_checking.attrib["href"]
+            [stack.append(i) for i in element_checking.getchildren()]
+        professorlinks.add(result)
+    
+    return list(professorlinks)
+    
+    
+    
 
 
 access_department_links()
