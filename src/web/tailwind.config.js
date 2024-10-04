9197b6c78868b7b0f23b5bc1d5b3c741a8ee2eb5
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-10-04 19:11:05 -0400

    move font family out of themer config, add secondary and action colors

--- src/web/tailwind.config.js
+++ src/web/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "selector",
   theme: {
-        extend: {
+    extend: {
       animation: {
         "fade-in": "fadein 0.25s linear",
         "fade-out": "fadeout 0.25s linear"
@@ -22,23 +22,31 @@ export default {
           to: { opacity: "0%" }
         }
       },
+      fontFamily: { sans: ['"Noto Sans"', "sans-serif"] },
+      backgroundImage: () => ({
+        "multiselect-caret": `url("${svgToTinyDataUri(
+          `<svg viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>`
+        )}")`,
+        "multiselect-spinner": `url("${svgToTinyDataUri(
+          `<svg viewBox="0 0 512 512" fill="${colors.green[500]}" xmlns="http://www.w3.org/2000/svg"><path d="M456.433 371.72l-27.79-16.045c-7.192-4.152-10.052-13.136-6.487-20.636 25.82-54.328 23.566-118.602-6.768-171.03-30.265-52.529-84.802-86.621-144.76-91.424C262.35 71.922 256 64.953 256 56.649V24.56c0-9.31 7.916-16.609 17.204-15.96 81.795 5.717 156.412 51.902 197.611 123.408 41.301 71.385 43.99 159.096 8.042 232.792-4.082 8.369-14.361 11.575-22.424 6.92z"></path></svg>`
+        )}")`,
+        "multiselect-remove": `url("${svgToTinyDataUri(
+          `<svg viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>`
+        )}")`
+      })
+    }
+  },
+  plugins: [
+    themer({
+      defaultTheme: {
+        extend: {
           colors: {
             primary: colors.black,
+            secondary: colors.neutral[200],
+            action: colors.pink[100],
             "on-primary": colors.slate[100],
-            "on-primary-dark": colors.slate[100],
-            link: colors.blue[500]
-          },
-          backgroundImage: () => ({
-            "multiselect-caret": `url("${svgToTinyDataUri(
-              `<svg viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>`
-            )}")`,
-            "multiselect-spinner": `url("${svgToTinyDataUri(
-              `<svg viewBox="0 0 512 512" fill="${colors.green[500]}" xmlns="http://www.w3.org/2000/svg"><path d="M456.433 371.72l-27.79-16.045c-7.192-4.152-10.052-13.136-6.487-20.636 25.82-54.328 23.566-118.602-6.768-171.03-30.265-52.529-84.802-86.621-144.76-91.424C262.35 71.922 256 64.953 256 56.649V24.56c0-9.31 7.916-16.609 17.204-15.96 81.795 5.717 156.412 51.902 197.611 123.408 41.301 71.385 43.99 159.096 8.042 232.792-4.082 8.369-14.361 11.575-22.424 6.92z"></path></svg>`
-            )}")`,
-            "multiselect-remove": `url("${svgToTinyDataUri(
-              `<svg viewBox="0 0 320 512" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>`
-            )}")`
-          })
+            "on-primary-dark": colors.slate[200]
+          }
         }
       },
       themes: [
@@ -47,6 +55,8 @@ export default {
           extend: {
             colors: {
               primary: colors.white,
+              secondary: colors.neutral[500],
+              action: colors.pink[500],
               "on-primary": colors.zinc[700],
               "on-primary-dark": colors.zinc[800]
             }
