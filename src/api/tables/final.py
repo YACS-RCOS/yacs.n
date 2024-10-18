b1a44930a9809f0f364e641d38ae9d929b2e4a93
Jonathan Friedrich <friedrich.jonathan4@gmail.com>
2024-10-18 17:31:37 -0400

    uploading finals schedule

--- src/api/tables/final.py
+++ src/api/tables/final.py
@@ -6,16 +6,13 @@ from .database import Base
 class Final(Base):
     __tablename__ = "final"
 
-    season = Column(VARCHAR(length=255))
-    year = Column(INTEGER)
-    major = Column(VARCHAR(length=255))
+    semester = Column(VARCHAR(length=255))
     course = Column(VARCHAR(length=255))
     section = Column(INTEGER)
     start = Column(TIME)
     end = Column(TIME)
-    building = Column(VARCHAR(length=255))
-    room = Column(VARCHAR(length=255))
+    room_assignment = Column(VARCHAR(length=255))
 
     __table_args__ = (
-        PrimaryKeyConstraint('major', 'course', 'section', 'season', 'year'),
+        PrimaryKeyConstraint('semester', 'course', 'section'),
     )
