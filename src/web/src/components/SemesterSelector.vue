7d495bebf608b7a0a667ca26a4b5a1d3eccd90b2
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-11-22 17:16:13 -0500

    make the semester selector actually work correctly

--- src/web/src/components/SemesterSelector.vue
+++ src/web/src/components/SemesterSelector.vue
@@ -2,51 +2,42 @@
 import { useClassInfoStore } from "@/stores/classinfo";
 import Multiselect from "@vueform/multiselect";
 import "@vueform/multiselect/themes/tailwind.css";
+import { until } from "@vueuse/core";
 import { storeToRefs } from "pinia";
 
-const selected_semeter = null;
+const { semesters, selectedSemester } = storeToRefs(useClassInfoStore());
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { semesters } = storeToRefs(useClassInfoStore());
+void until(semesters)
+  .toBeTruthy()
+  .then((v) => {
+    selectedSemester.value = v[0];
+  });
 </script>
 
 <template>
   <Multiselect
-    v-model="selected_semeter"
-    :options="['Batman', 'Robin', 'Joker']"
+    v-model="selectedSemester"
+    :options="semesters ?? []"
+    :loading="!semesters"
+    :disabled="!semesters"
     mode="single"
     :can-clear="false"
     :can-deselect="false"
     :classes="{
       container:
-        'multiselect !bg-on-primary-dark !border !border-link !rounded-md !text-link !w-28',
+        'multiselect !bg-on-primary-dark !border !border-primary !rounded-md !text-link !w-max',
+      containerActive: 'ring ring-opacity-30 ring-primary',
+      wrapper:
+        'relative mx-auto w-full h-full flex items-center justify-end box-border cursor-pointer outline-none',
       dropdown: 'multiselect-dropdown !bg-on-primary-dark',
-      singleLabel: 'multiselect-single-label !max-w-[initial]',
-      option: 'flex flex-col p-0 m-0 list-none hover:bg-gray-700',
-      optionSelected: 'dark:bg-blue-600 dark:text-white'
+      spinner:
+        ' mask-image-multiselect-spinner w-8 mx-8 aspect-square animate-spin bg-primary bg-center bg-no-repeat self-center ',
+      singleLabel: 'multiselect-single-label !min-w-max !static !px-2',
+      option: 'flex flex-col p-0 m-0 list-none',
+      optionSelected: 'bg-primary bg-opacity-15',
+      optionPointed: 'bg-primary bg-opacity-10',
+      optionSelectedPointed: 'bg-primary bg-opacity-20',
+      caret: 'multiselect-caret mask-image-multiselect-caret !bg-primary !bg-none'
     }"
-  >
-    <template #clear><div /></template>
-  </Multiselect>
-  <!-- <select class="text-lin rounded-sm border border-link bg-transparent p-1">
-    <template v-if="semesters.length > 0">
-      <option
-        v-for="sem of semesters"
-        :key="sem"
-        :value="sem.toLowerCase()"
-        class="bg-on-primary-dark hover:bg-primary"
-      >
-        {{ sem }}
-      </option>
-    </template>
-    <option
-      v-else
-      :value="null"
-      selected
-      disabled
-      class="bg-on-primary-dark"
-    >
-      Loading...
-    </option>
-  </select> -->
+  />
 </template>
