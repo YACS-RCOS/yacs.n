5a316ed67689129d856827f819da3e7b5533d713
dorian451 <112524240+dorian451@users.noreply.github.com>
2025-01-31 16:52:59 -0500

    remove "*.vue" types shim because it hallucinates files that aren't there

--- src/web/env.d.ts
+++ src/web/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import { DefineComponent } from "vue";
-  const Component: DefineComponent;
-  export default Component;
-}
+// declare module "*.vue" {
+//   import { DefineComponent } from "vue";
+//   const Component: DefineComponent;
+//   export default Component;
+// }
--- src/web/src/main.ts
+++ src/web/src/main.ts
@@ -6,7 +6,7 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
-const app = createApp(App);
+const app = createApp(App as Parameters<typeof createApp>[0]);
 
 app.use(createPinia());
 app.use(router);
