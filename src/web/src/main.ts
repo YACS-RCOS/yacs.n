9219562d54526dedb4bc7cf3d28ceacfe62e12c8
dorian451 <112524240+dorian451@users.noreply.github.com>
2025-02-04 16:52:43 -0500

    add vue3-toastify for toasts

--- src/web/src/main.ts
+++ src/web/src/main.ts
@@ -5,10 +5,19 @@ import { createApp } from "vue";
 
 import App from "@/App.vue";
 import router from "@/router";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
 const app = createApp(App as Parameters<typeof createApp>[0]);
 
 app.use(createPinia());
 app.use(router);
+app.use(Vue3Toastify, {
+  toastStyle: {
+    color: "rgb(var(--colors-primary))",
+    background: "rgb(var(--colors-on-primary))",
+    zIndex: "100000"
+  }
+} as ToastContainerOptions);
 
 app.mount("#app");
