50f2d6479bc9c1a1ffad5f2890ba170828e93f94
dorian451 <112524240+dorian451@users.noreply.github.com>
2025-02-04 16:54:32 -0500

    use toasts to alert user to upload success or failure

--- src/web/src/views/admin/upload_courses.vue
+++ src/web/src/views/admin/upload_courses.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import * as api from "@/api/admin";
 import UploadModal from "@/components/modals/UploadModal.vue";
+import { toast } from "vue3-toastify";
 </script>
 
 <template>
@@ -12,6 +13,20 @@ import UploadModal from "@/components/modals/UploadModal.vue";
         api.upload_course_csv(v).catch((e) => {
           throw [v.name, e];
         })
+    "
+    @upload-success="
+      () => {
+        toast.success(`Upload Success!`);
+      }
+    "
+    @upload-error="
+      (e) => {
+        for (const error of e) {
+          toast.error(
+            `Error uploading ${error.reason[0]}: ${error.reason[1][1].response?.data ?? error.reason[1][1].message}`
+          );
+        }
+      }
     "
   />
 </template>
