53f433f3c16617f8448e12d0327dc24e3b7ffe20
dorian451 <112524240+dorian451@users.noreply.github.com>
2025-02-14 16:31:01 -0500

    add vueuse's usememoize to our frontend api calls

--- src/web/src/api/semester.ts
+++ src/web/src/api/semester.ts
@@ -1,18 +1,17 @@
+import { useMemoize } from "@vueuse/core";
 import { app } from "./common";
 
 export type SemesterResponse = { semester: string }[];
 export type SemesterInfoResponse = { public: boolean; semester: string }[];
 
-/** Gets a list of available semesters
- */
-export async function getSemesters() {
+/** Gets a list of available semesters */
+export const getSemesters = useMemoize(async () => {
   const resp = await app.get<SemesterResponse>("/semester");
   return resp.data.map((v) => v.semester);
-}
+});
 
-/** Gets semester info
- */
-export async function getSemesterInfo() {
+/** Gets semester info */
+export const getSemesterInfo = useMemoize(async () => {
   const resp = await app.get<SemesterInfoResponse>("/semesterInfo");
   return resp.data;
-}
+});
