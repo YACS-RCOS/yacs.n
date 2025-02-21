ab9ca3dabdbef91293021d65752c0a590fe50ef6
bryantran24 <158430748+bryantran24@users.noreply.github.com>
2025-02-21 16:34:10 -0500

    Added upload prof json
    Added it to admin.ts and a new file upload_prof.vue which should mimic upload_courses.vue but for json instead and accepts single files

--- src/web/src/views/admin/upload_prof.vue
+++ src/web/src/views/admin/upload_prof.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
-import GenericModal from "@/components/modals/GenericModal.vue";
-import { useRouter } from "vue-router/auto";
-
-const router = useRouter();
+import * as api from "@/api/admin";
+import UploadModal from "@/components/modals/UploadModal.vue";
+import { toast } from "vue3-toastify";
 </script>
+
 <template>
-  <GenericModal
-    :open="true"
-    @close="router.back()"
+  <UploadModal
+    accept-file-types="application/json"
+    title="Upload Professors JSON"
+    :action="
+      (v) =>
+        api.upload_prof_json(v).catch((error) => {
+          throw [v.name, error];
+        })
+    "
+    @upload-success="
+      (f) => {
+        toast.success(`Successfully uploaded ${f}!`);
+      }
+    "
+    @upload-error="
+      (error) => {
+        toast.error(
+          `Error uploading ${error[0]}: ${error[1].response?.data ?? error[1].message}`,
+          { autoClose: 60000 }
+        );
+      }
+    "
   />
 </template>
