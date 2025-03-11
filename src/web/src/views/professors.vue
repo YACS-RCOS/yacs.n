78c8c5942a75fc3ac668ae5671d65881ce288463
bryantran24 <158430748+bryantran24@users.noreply.github.com>
2025-03-11 16:41:57 -0400

    Made a search box to search for professor

--- src/web/src/views/professors.vue
+++ src/web/src/views/professors.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { getProfessors, type Professor } from "@/api/professor";
 import ProfessorModal from "@/components/modals/ProfessorModal.vue";
 
 const professors = ref<Professor[]>([]);
 const selectedProfessor = ref<Professor | null>(null);
 const isModalOpen = ref(false);
+const searchTerm = ref("");
 
 onMounted(async () => {
   try {
     professors.value = await getProfessors();
-    // professors.value.sort((a, b) => a.name.localeCompare(b.name));
+    professors.value.sort((a, b) => a.name.localeCompare(b.name));
     console.log(professors);
   } catch (error) {
     console.error("Error fetching professors:", error);
   }
+});
+
+const filteredProfessors = computed(() => {
+  if (!searchTerm.value) return professors.value;
+  return professors.value.filter((prof) =>
+    prof.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+  );
 });
 
 function showProfessor(prof: Professor) {
@@ -31,9 +39,19 @@ function closeModal() {
 <template>
   <div class="flex flex-col text-primary gap-4 p-4">
     <h2 class="text-2xl font-bold">Professors</h2>
-    <div class="flex flex-col gap-2">
+
+    <div class="mb-4">
+      <input
+        v-model="searchTerm"
+        type="text"
+        placeholder="Search by name"
+        class="px-2 py-1 border border-gray-300 rounded text-black"
+      />
+    </div>
+
+    <div class="grid grid-cols-3 gap-4">
       <button
-        v-for="prof in professors"
+        v-for="prof in filteredProfessors"
         :key="prof.email"
         class="text-500 hover:underline text-left"
         @click="showProfessor(prof)"
@@ -41,6 +59,7 @@ function closeModal() {
         {{ prof.name }}
       </button>
     </div>
+
 
     <ProfessorModal
       v-if="selectedProfessor"
