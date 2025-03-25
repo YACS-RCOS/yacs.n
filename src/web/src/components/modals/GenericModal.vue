7e93e1bceb61e467338567ef79e092fff38f6493
dorian451 <112524240+dorian451@users.noreply.github.com>
2025-03-25 16:26:08 -0400

    fix bg-opacity for modal scrim

--- src/web/src/components/modals/GenericModal.vue
+++ src/web/src/components/modals/GenericModal.vue
@@ -50,7 +50,7 @@ watch(
     <div
       v-if="open"
       ref="popup"
-      class="absolute left-0 top-0 flex h-full w-full flex-col items-center bg-black bg-opacity-50 p-24 motion-safe:[&.v-enter-active]:animate-fade-in motion-safe:[&.v-leave-active]:animate-fade-out"
+      class="absolute left-0 top-0 flex h-full w-full flex-col items-center bg-black/50 p-24 motion-safe:[&.v-enter-active]:animate-fade-in motion-safe:[&.v-leave-active]:animate-fade-out"
       :class="
         align == 'top' ? 'justify-start' : align == 'bottom' ? 'justify-end' : 'justify-center'
       "
