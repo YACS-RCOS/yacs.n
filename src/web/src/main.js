deabe830da4ce2637a5a920eb9cc53d8830da0fc
Jonathan Friedrich <friedrich.jonathan4@gmail.com>
2025-04-01 16:41:19 -0400

    add tracking code to frontend
    provides support for our matomo tracking analytics without breaking any existing frontend features. this should leave us ready for getting our analytics up and running without interrupting our current services.

--- src/web/src/main.js
+++ src/web/src/main.js
@@ -11,17 +11,24 @@ import VueRouter from "vue-router";
 import router from "./routes";
 import VueCookies from "vue-cookies";
 import Meta from "vue-meta";
+import VueMatomo from 'vue-matomo'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 Vue.use(VueCookies);
 Vue.use(Meta);
-
 Vue.$cookies.config("7d");
+Vue.use(VueMatomo, {
+  host: 'https://analytics.yacs.cs.rpi.edu/',
+  siteId: 1,
+});
 
 new Vue({
   render: (h) => h(App),
   router,
+  components: { App },
   store,
 }).$mount("#app");
+
+window._paq.push(['trackPageView']);
