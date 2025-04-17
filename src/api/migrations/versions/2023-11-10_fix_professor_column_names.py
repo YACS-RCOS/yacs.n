0f2e39dae7ccba978ae13b31e5922d0767de9b62
Jonathan Friedrich <friedrich.jonathan4@gmail.com>
2025-04-17 16:57:02 -0400

    remove irrelevant code
    general cleanup
    
    matomo.conf has been moved to exist inside "nginx.development.conf"
    
    comment some changes that were necessary due to architecture

--- src/api/migrations/versions/2023-11-10_fix_professor_column_names.py
+++ src/api/migrations/versions/2023-11-10_fix_professor_column_names.py
@@ -1,6 +1,6 @@
 """fix professor column names
 
-Revision ID: f62dd5e95906
+Revision ID: c959c263997f
 Revises: c0289fddf6dd
 Create Date: 2023-11-10 22:18:36.674812
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f62dd5e95906'
+revision = 'c959c263997f'
 down_revision = 'c0289fddf6dd'
 branch_labels = None
 depends_on = None
